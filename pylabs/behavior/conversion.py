242189f9b2cd6c4f71327a17193062aab8646d7d
Jasper van den Bosch <jasperb@uw.edu>
2015-06-29 18:14:35 -0700

    csv2fslmat implementation

--- pylabs/behavior/conversion.py
+++ pylabs/behavior/conversion.py
@@ -1,9 +1,36 @@
-import os
+import os, numpy
 
 
-def csv2fslmat(csvfile):
+def csv2fslmat(csvfile, selectSubjects=None):
+    # Create FSL matrix files for correlation from behavioral data in a csv file
+
+    skipCols = 4
+    with open(csvfile) as csv:
+        lines = csv.readlines()
+    data = numpy.loadtxt(csvfile, skiprows=2)
+    measures = lines[1].split()[skipCols:]
+
+    #import pdb
+    #pdb.set_trace()
+    if selectSubjects is not None:
+        allsubjects = data[:,0].astype(int)
+        selection = numpy.array([s in selectSubjects for s in allsubjects])
+        data = data[selection,:]
+    nsubjects = data.shape[0]
+
+    sdata = data[data[:,0].argsort()] #sort order of subjects
+    means = sdata.mean(axis=0) #per-measure average
+    dmdata = (sdata-means) # demeaned data
+
     os.makedirs('matfiles')
-    with open('matfiles/cardsorttotal.mat', 'w') as dummyfile:
-        dummyfile.write('bla')
-    #csvfile
-    #out: .mat
+    for m, measure in enumerate(measures):
+        c = skipCols+m # column for this measure
+        matfname = '{0}.mat'.format(measure)
+        with open('matfiles/'+matfname, 'w') as matfile:
+            matfile.write('/ContrastName1\t{0}\n'.format(measure))
+            matfile.write('/NumWaves\t2\n/NumPoints\t{0}\n'.format(nsubjects))
+            matfile.write('/PPheights\t\t1.000000e+00 1.000000e+00\n')
+            matfile.write('\n/Matrix\n')
+            for s in range(nsubjects):
+                matfile.write('1.000000e+00	{0}\t\n'.format(dmdata[s, c]))
+
--- pylabs/behavior/tests/test_csv2fslmat.py
+++ pylabs/behavior/tests/test_csv2fslmat.py
@@ -1,11 +1,11 @@
 from nose.tools import (assert_raises, assert_equal, assert_not_equal,
                         assert_true)
-import os, shutil
+import os, shutil, unittest
 from pylabs.behavior.conversion import csv2fslmat
 
 sampledir = 'pylabs/behavior/tests/sampledata/'
 
-def test_x():
+def test_basic():
     if os.path.isdir('matfiles'):
         shutil.rmtree('matfiles') # clean up
     csvfile = sampledir+'behavior.csv'
@@ -13,4 +13,14 @@ def test_x():
     assert_true(os.path.isfile('matfiles/cardsorttotal.mat'))
     with open('matfiles/cardsorttotal.mat') as createdfile:
         with open(sampledir+'cardsorttotal.mat') as correctfile:
-            assert_equal(createdfile.read(), correctfile.read())
+            assert_equal(createdfile.readlines(), correctfile.readlines())
+
+def test_select_two_subjects():
+    if os.path.isdir('matfiles'):
+        shutil.rmtree('matfiles') # clean up
+    csvfile = sampledir+'behavior.csv'
+    csv2fslmat(csvfile=csvfile, selectSubjects=[317, 396])
+    assert_true(os.path.isfile('matfiles/cardsorttotal.mat'))
+    with open('matfiles/cardsorttotal.mat') as createdfile:
+        with open(sampledir+'cardsorttotal_only2.mat') as correctfile:
+            assert_equal(createdfile.readlines(), correctfile.readlines())
