3eda43d2d53d80cf85d6754487acea2c4d25bc3c
Jasper van den Bosch <jasperb@uw.edu>
2015-07-01 18:05:56 -0700

    multirandpar specifies output dir

--- pylabs/correlation/randpar.py
+++ pylabs/correlation/randpar.py
@@ -5,7 +5,9 @@ from pylabs.utils import Shell
 def multirandpar(images, shell=Shell()):
     # randomise_parallel on multiple images and/or multiple predictors
     for image in images:
-        shell.run('randomize_parallel -i '+image)
+        cmd = 'randomize_parallel -i {0} '.format(image)        #input image
+        cmd = cmd+'-o randpar_{0}/x'.format(image.split('.')[0]) #output dir
+        shell.run(cmd)
 
 
 
--- pylabs/correlation/tests/test_multirandpar.py
+++ pylabs/correlation/tests/test_multirandpar.py
@@ -8,11 +8,23 @@ class MultiRandParTests(TestCase):
     def setUp(self):
         self.shell = MockShell()
 
-    def test_runs_randpar_on_each_image(self):
+    def test_runs_randpar_on_each_image_with_separate_output_dir(self):
         imgs = ['one.img','two.img']
         multirandpar(imgs, shell=self.shell)
-        self.shell.assert_ran_command_matching('randomize_parallel *-i '+imgs[0])
-        self.shell.assert_ran_command_matching('randomize_parallel *-i '+imgs[1])
+        self.shell.assert_ran_command_matching(
+            'randomize_parallel *-i one.img*')
+        self.shell.assert_ran_command_matching(
+            'randomize_parallel *-i two.img*')
+
+    def test_uses_separate_output_dir_per_image(self):
+        imgs = ['one.img','two.img']
+        multirandpar(imgs, shell=self.shell)
+        self.shell.assert_ran_command_matching(
+            'randomize_parallel *-i one.img *-o randpar_one*/*')
+
+#one output dir per image
+
+
 
 
 
