e533704fd08dfa9c837f569dbfafcf9a1feb2479
Jasper van den Bosch <jasperb@uw.edu>
2015-07-06 15:03:04 -0700

    Started on a 'pipeline' script for VBM.

---
+++ pylabs/vbm/pipeline.py
@@ -0,0 +1,17 @@
+## Raw file conversion
+# nibabel
+## RMS / Concat
+# MRIConcat
+## Skullstripping
+# fslvbm_2_proc_-n
+## Segmentation
+## Normalizing
+
+## From here there is two paths; filter out covariate or include it in model.
+
+## Covariate Filtering
+# create gender covariate mat file from csv
+# reg_filt script
+## Randomize
+# create behavior variable regressor mat files from csv
+# multirandpar
