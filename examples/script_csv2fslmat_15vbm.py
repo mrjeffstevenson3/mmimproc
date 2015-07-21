b3fe98abf8d48aca02545b195518db2ce7fda6ca
todd98028 <toddr@uw.edu>
2015-07-20 20:32:25 -0700

    Reality check and resulting todo list for VBM pipeline. Added scotty data path

---
+++ examples/script_csv2fslmat_15vbm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+from pylabs.correlation.behavior import csv2fslmat
+csvfile = '/media/DiskArray/shared_data/js/self_control/behavioral_data/behav_from_andy_march27_2015/EF_and_Brain_july08_2015_Meq0_delta.csv'
+subjects= [
+317,
+328,
+332,
+334,
+335,
+347,
+353,
+364,
+371,
+376,
+379,
+381,
+384,
+385,
+396,
+]
+csv2fslmat(csvfile, selectSubjects=subjects,groupcol=True,covarcols=[42])
--- pylabs/utils/paths.py
+++ pylabs/utils/paths.py
@@ -3,9 +3,12 @@ import socket
 
 def getlocaldataroot():
     hostname = socket.gethostname()
+
     if hostname == 'gram':
         return '/home/jasper/mirror/'
     elif hostname == 'JVDB':
         return '/diskArray/mirror/'
+    elif hostname == 'scotty':
+        return '/media/DiskArray/shared_data/'
     else:
         raise ValueError('Don''t know where data root is on this computer.')
