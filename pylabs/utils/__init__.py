7eb4f9273c5fbcf894c3285f0d67c1a3ff02145a
Jasper van den Bosch <jasperb@uw.edu>
2015-07-21 19:05:23 -0700

    #noqa comment for Binaries import in utils package

--- pylabs/utils/__init__.py
+++ pylabs/utils/__init__.py
@@ -5,4 +5,4 @@ from ._dirs import InDir, InTempDir  # noqa
 from ._mocks import MockShell  # noqa
 from ._filesys import Filesystem  # noqa
 from ._options import PylabsOptions  # noqa
-from ._binaries import Binaries
+from ._binaries import Binaries  # noqa
