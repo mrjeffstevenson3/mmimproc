3e5bcf108447b618ba215d8846581add53ea8b09
Jasper van den Bosch <jasperb@uw.edu>
2015-07-24 16:16:59 -0700

    [vbm] Adapted VBM pipeline to new directory arguments for csv2fslmat and multirandpar.

--- pylabs/vbm/pipeline.py
+++ pylabs/vbm/pipeline.py
@@ -5,6 +5,7 @@
 ## Normalizing
 ## From here there is two paths; filter out covariate or include it in model.
 import os, glob
+from os.path import join as pathjoin
 import niprov
 from pylabs.correlation.behavior import csv2fslmat
 from pylabs.correlation.regfilt import multiregfilt
@@ -15,12 +16,16 @@ opts = NiprovOptions()
 opts.dryrun = True
 opts.verbose = True
 
+exptag='gender_filter'
+
 subjects = [
 317, 328, 332, 334, 335, 347, 353, 364, 371, 376, 379, 381, 384, 385, 396]
 
 fs = getlocaldataroot()
 vbmdir = fs+'js/self_control/hbm_group_data/vbm_15subj/workdir_v1/stats/'
-qsubdir = vbmdir+'qsubs'
+matfiledir = pathjoin(vbmdir,'matfiles',exptag)
+resultdir = pathjoin(vbmdir,'randpar',exptag)
+qsubdir = pathjoin(vbmdir,'qsubs_defunctcommands')
 behavdir = fs+'js/self_control/behavioral_data/behav_from_andy_march27_2015/'
 csvfile = behavdir+'EF_and_Brain_july08_2015_Meq0_delta.csv'
 niprov.add(csvfile)
@@ -28,17 +33,15 @@ images = glob.glob(vbmdir+'GM_mod_merg_s2.nii.gz')
 [niprov.add(img) for img in images]
 
 ## Covariate Filtering
-filterargs = {'selectSubjects':subjects, 'groupcol':False, 'demean':False, 
-    'workdir':vbmdir, 'opts':opts} # preset csv2fslmat arguments
-matfiles = csv2fslmat(csvfile, tag='filt_gender', cols=[2], **filterargs)
+matfiles = csv2fslmat(csvfile, cols=[2], selectSubjects=subjects, 
+    groupcol=False, demean=False, outdir=matfiledir, opts=opts)
 images = multiregfilt(images, matfiles[0], opts=opts)
 
 ## Randomize
 designfile = vbmdir+'scs_design2col.con'
 assert os.path.isfile(designfile)
-corrargs = {'selectSubjects':subjects, 'groupcol':True, 'workdir':vbmdir, 
-    'opts':opts}
-matfiles = csv2fslmat(csvfile, tag='vars', cols=[5, 21], **corrargs)
+matfiles = csv2fslmat(csvfile, cols=[5, 21], selectSubjects=subjects, 
+    groupcol=True, outdir=matfiledir, opts=opts)
 multirandpar(images, matfiles, designfile, niterations=100, workdir=qsubdir, 
-    opts=opts)
+    outdir=resultdir, opts=opts)
 
