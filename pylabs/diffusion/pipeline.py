bc14105f97678d189895594511f5c21a6afa2317
Jasper van den Bosch <jasperb@uw.edu>
2015-07-24 16:09:12 -0700

    csv2fslmat now places files directly in outdir (formerly workdir).

--- pylabs/diffusion/pipeline.py
+++ pylabs/diffusion/pipeline.py
@@ -1,4 +1,5 @@
 import os, glob
+from os.path import join as pathjoin
 import niprov
 from pylabs.correlation.behavior import csv2fslmat
 from pylabs.correlation.regfilt import multiregfilt
@@ -18,8 +19,8 @@ measures = ['F1', 'F2', 'FA', 'L1', 'MD', 'MO', 'RA', 'AD', 'L2', 'L3']
 skellist = [imgtemplate.format(m) for m in measures]
 fs = getlocaldataroot()
 tbssdir = fs+'js/self_control/hbm_group_data/tbss_19subj/workdir_thr1p5_v3/stats/'
-matfilerootdir = tbssdir+'matfiles/'
-resultdir = tbssdir+'randpar/{0}/'.format(exptag)
+matfiledir = pathjoin(tbssdir,'matfiles',exptag)
+resultdir = pathjoin(tbssdir,'randpar',exptag)
 qsubdir = resultdir+'qsubdir_defunctcommands'
 behavdir = fs+'js/self_control/behavioral_data/behav_from_andy_march27_2015/'
 csvfile = behavdir+'EF_and_Brain_july08_2015_Meq0_delta.csv'
@@ -33,9 +34,8 @@ niprov.add(csvfile)
 ## Randomize
 designfile = tbssdir+'scs_design4col.con'
 assert os.path.isfile(designfile)
-matfiles = csv2fslmat(csvfile, tag=exptag, cols=range(5, 39), 
-    covarcols=[2, 41], selectSubjects=subjects, groupcol=True, 
-    workdir=matfilerootdir, opts=opts)
+matfiles = csv2fslmat(csvfile, cols=range(5, 39), covarcols=[2, 41],
+    selectSubjects=subjects, groupcol=True, outdir=matfiledir, opts=opts)
 multirandpar(images, matfiles, designfile, masks=masks, niterations=500, 
     tbss=True, workdir=qsubdir, outdir=resultdir, opts=opts)
 
