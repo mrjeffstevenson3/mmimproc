5ecc54e21cba01f03df22861f1cfe30c70caedb1
Jasper van den Bosch <jasperb@uw.edu>
2015-07-24 12:40:36 -0700

    Cleaned up diffusion pipeline a bit.

--- pylabs/diffusion/pipeline.py
+++ pylabs/diffusion/pipeline.py
@@ -1,9 +1,3 @@
-## Raw file conversion:  nibabel
-## RMS / Concat: MRIConcat
-## Skullstripping: fslvbm_2_proc_-n
-## Segmentation
-## Normalizing
-## From here there is two paths; filter out covariate or include it in model.
 import os, glob
 import niprov
 from pylabs.correlation.behavior import csv2fslmat
@@ -15,31 +9,25 @@ opts = NiprovOptions()
 opts.dryrun = False
 opts.verbose = True
 
-subjects= [ 317, 322, 324, 328, 332, 334, 335, 341, 347, 353, 364, 370, 371, 376, 379, 381, 384, 385, 396 ]
-skellist = [ 'all_F1_skeletonised.nii.gz', 'all_F2_skeletonised.nii.gz', 'all_FA_skeletonised.nii.gz', 'all_L1_skeletonised.nii.gz', 'all_MD_skeletonised.nii.gz', 'all_MO_skeletonised.nii.gz', 'all_RA_skeletonised.nii.gz', 'all_AD_skeletonised.nii.gz', 'all_L2_skeletonised.nii.gz', 'all_L3_skeletonised.nii.gz' ]
+subjects= [ 317, 322, 324, 328, 332, 334, 335, 341, 347, 353, 364, 370, 371, 
+    376, 379, 381, 384, 385, 396 ]
+imgtemplate = 'all_{0}_skeletonised.nii.gz'
+measures = ['F1', 'F2', 'FA', 'L1', 'MD', 'MO', 'RA', 'AD', 'L2', 'L3']
+skellist = [imgtemplate.format(m) for m in measures]
 fs = getlocaldataroot()
 tbssdir = fs+'js/self_control/hbm_group_data/tbss_19subj/workdir_thr1p5_v3/stats/'
 qsubdir = tbssdir+'qsubdir'
 behavdir = fs+'js/self_control/behavioral_data/behav_from_andy_march27_2015/'
 csvfile = behavdir+'EF_and_Brain_july08_2015_Meq0_delta.csv'
 niprov.add(csvfile)
-images = [ tbssdir+i for i in skellist ]
-
-
+images = [tbssdir+i for i in skellist]
 [niprov.add(img) for img in images]
-
-## Covariate Filtering
-# filterargs = {'selectSubjects':subjects, 'groupcol':False, 'demean':False,
-#     'workdir':tbssdir, 'opts':opts} # preset csv2fslmat arguments
-# matfiles = csv2fslmat(csvfile, tag='filt_gender', cols=[2], **filterargs)
-# images = multiregfilt(images, matfiles[0], opts=opts)
 
 ## Randomize
 designfile = tbssdir+'scs_design4col.con'
 assert os.path.isfile(designfile)
-corrargs = {'selectSubjects':subjects, 'groupcol':True, 'workdir':tbssdir,
-    'opts':opts}
-matfiles = csv2fslmat(csvfile, tag='gender_and_dti_delta_cov', cols=range(5, 39), covarcols=[2, 41], **corrargs)
+matfiles = csv2fslmat(csvfile, tag='gender_and_dti_delta_cov', cols=range(5, 39), 
+    covarcols=[2, 41], selectSubjects=subjects, groupcol=True, workdir=tbssdir, opts=opts)
 multirandpar(images, matfiles, designfile, niterations=500, workdir=qsubdir,
     opts=opts)
 
