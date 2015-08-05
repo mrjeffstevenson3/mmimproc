6e629165d2bee17c28d92525754e56ac1d192af9
Jasper van den Bosch <jasperb@uw.edu>
2015-08-04 17:35:41 -0700

    rsync command for atlases

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,12 @@
-rsync -e ssh -avz toddr@scotty.ilabs.uw.edu:/media/DiskArray/shared_data/js/self_control/hbm_group_data/vbm_17subj/workdir_v1/stats/ js/self_control/hbm_group_data/vbm_17subj/workdir_v1/stats/
+rsync -e ssh -avz toddr@scotty.ilabs.uw.edu:/media/DiskArray/shared_data/js/self_control/hbm_group_data/vbm_15subj/workdir_v1/stats/ js/self_control/hbm_group_data/vbm_15subj/workdir_v1/stats/
 
 rsync -e ssh -avz toddr@scotty.ilabs.uw.edu:/media/DiskArray/shared_data/js/self_control/behavioral_data/behav_from_andy_march27_2015/ js/self_control/behavioral_data/behav_from_andy_march27_2015/
+
+rsync -e ssh -avz toddr@scotty.ilabs.uw.edu:/media/DiskArray/shared_data/js/self_control/hbm_group_data/tbss_19subj/workdir_thr1p5_v3/stats/ js/self_control/hbm_group_data/tbss_19subj/workdir_thr1p5_v3/stats/
+
+
+rsync -e ssh -avz toddr@scotty.ilabs.uw.edu:/home/toddr/Software/jasper_atlases/ jasper_atlases 
+
 
 
 
