f46c3e1936b5634242b5b5a5909b67e37254a06d
Jasper van den Bosch <jasperb@uw.edu>
2015-08-04 17:34:23 -0700

    [WIP] tests for file finding, selection, etc.

---
+++ pylabs/utils/tests/test_filefinding.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from mock import patch, call, Mock, MagicMock
+
+
+class FilefinderTests(TestCase):
+
+
+    def test_corep_from_randpar_base_result(self):
+        pass
