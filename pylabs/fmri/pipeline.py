89058a8ac0aea648f634eeba8c841b67fe01df46
Jasper van den Bosch <jasperb@uw.edu>
2015-08-10 14:54:12 -0700

    [fmri] Standardizing contrast images using nipype/FLIRT.

--- pylabs/fmri/pipeline.py
+++ pylabs/fmri/pipeline.py
@@ -4,9 +4,9 @@ import niprov
 import nibabel, numpy
 from pylabs.utils.paths import getlocaldataroot
 from niprov.options import NiprovOptions
-from dipy.align.reslice import reslice
+from pylabs.transformations.standard import standardize2mni
 opts = NiprovOptions()
-opts.dryrun = True
+opts.dryrun = False
 opts.verbose = True
 
 fs = getlocaldataroot()
@@ -15,48 +15,40 @@ fs = getlocaldataroot()
 contrasts = ['Congruent','Incongruent',
     'Congruent_gt_Incongruent','Incongruent_gt_Congruent']
 fmridir = join(fs,'self_control/hbm_group_data/fmri')
+mnidir = join(fmridir,'nifti')
+mnidims = [182, 218, 182]
 
-#fmrisubjects = sorted([int(os.path.basename(i)[:3]) for i in fmriimages])
-# remap. http://brainmap.wustl.edu/help/mapper.html
-
-# http://nipy.org/nibabel/coordinate_systems.html
 
 
 def getContrast(filename):
     return '_'.join(os.path.basename(filename).split('.')[0].split('_')[1:])
 
-origdims = (79, 95, 68)
-dims1mm = [d*2 for d in origdims]
 hdrfilter = join(fmridir, 'analyze', '*.hdr')
 hdrimages = sorted(glob.glob(hdrfilter))
+[niprov.add(i) for i in hdrimages]
+images = standardize2mni(hdrimages, mnidir, opts=opts)
+
+
 for contrast in contrasts:
-    print('Working on contrast '+contrast)
-    contrastimages = [i for i in hdrimages if getContrast(i)==contrast]
+    print('Grouping contrast '+contrast)
+
+    contrastimages = [i for i in images if getContrast(i)==contrast]
     nsubjects = len(contrastimages)
-    data = numpy.full(dims1mm+[nsubjects], numpy.nan)
+    data = numpy.full(mnidims+[nsubjects], numpy.nan)
     for s, image in enumerate(contrastimages):
         imagebasename = os.path.basename(image)
-        print('Rescaling image '+image)
         img = nibabel.load(image)
-        data1 = img.get_data()
-        affine1 = img.get_affine()
-        zooms = img.get_header().get_zooms()[:3]
-        new_zooms = (1., 1., 1.)
-        data2, affine2 = reslice(data1, affine1, zooms, new_zooms)
-        data[:,:,:,s] = data2
-    groupimg = nibabel.Nifti1Image(data, affine2)
-    groupimage = join(fmridir, '{0}_1mm.nii.gz'.format(contrast))
-    print('Saving '+groupimage)
-    nibabel.save(groupimg, groupimage)
-    niprov.log(groupimage, 'grouped and upsampled to 1mm', contrastimages, 
+        data[:,:,:,s] = img.get_data()
+        affine = img.get_affine()
+    groupimg = nibabel.Nifti1Image(data, affine)
+    groupfilename = join(fmridir, '{0}.nii.gz'.format(contrast))
+    print('Saving '+groupfilename)
+    nibabel.save(groupimg, groupfilename)
+    niprov.log(groupfilename, 'grouped', contrastimages, 
         script=__file__, opts=opts)
 
-#from nipype.interfaces import fsl
-#from nipype.testing import example_data
-#flt = fsl.FLIRT(bins=640, cost_func='mutualinfo')
-#flt.inputs.in_file = 'myInput.img'
-#flt.inputs.reference = 'myReference.img'
-#flt.inputs.out_file = 'moved_subject.nii'
-#flt.inputs.out_matrix_file = 'subject_to_template.mat'
-#res = flt.run() 
+
+# remap. http://brainmap.wustl.edu/help/mapper.html
+# http://nipy.org/nibabel/coordinate_systems.html
+
 
---
+++ pylabs/transformations/standard.py
@@ -0,0 +1,25 @@
+import os
+from os.path import join
+import niprov
+from nipype.interfaces import fsl
+from pylabs.utils import PylabsOptions
+
+
+def standardize2mni(images, outdir, opts=PylabsOptions()):
+    standardizedImages = []
+    for image in images:
+        imagebasename = os.path.basename(image)
+        print('Standardizing '+imagebasename)
+        outfile = join(outdir, imagebasename.split('.')[0]+'.nii.gz')
+
+        template = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
+        flt = fsl.FLIRT()
+        flt.inputs.in_file = image
+        flt.inputs.reference = template
+        flt.inputs.out_file = outfile
+        result = flt.run() 
+        niprov.log(outfile, 'standardized with FLIRT', image, code=flt.cmdline,
+            script=__file__, logtext=result.outputs.out_log, opts=opts)
+        standardizedImages.append(outfile)
+    return standardizedImages
+
