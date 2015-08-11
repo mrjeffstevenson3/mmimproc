7539e1c61e1594c755ac51f03abe4afdda5bcf7c
Jeff Stevenson <mrjeffs@redshirt.ilabs.uw.edu>
2015-08-11 12:55:10 -0700

    VBM pipeline updates and fix to atlas labels

--- pylabs/vbm/pipeline.py
+++ pylabs/vbm/pipeline.py
@@ -15,8 +15,7 @@ opts = NiprovOptions()
 opts.dryrun = True
 opts.verbose = True
 
-subjects = [
-317, 328, 332, 334, 335, 347, 353, 364, 371, 376, 379, 381, 384, 385, 396]
+subjects = [317, 328, 332, 334, 335, 347, 353, 364, 371, 376, 379, 381, 384, 385, 396]
 
 fs = getlocaldataroot()
 statsdir = fs+'self_control/hbm_group_data/vbm_15subj/workdir_v1/stats/'
@@ -31,27 +30,26 @@ niprov.add(csvfile)
 images = glob.glob(statsdir+'?M_mod_merg_s4.nii.gz')
 [niprov.add(img) for img in images]
 
-exptag='randpar_n500_gender_and_vbm_delta_cov_4col_vbm'
+exptag='filter_gender_and_vbm_delta_n500'
 matfiledir = pathjoin(statsdir,'matfiles','matfiles_'+exptag)
 resultdir = pathjoin(statsdir,'randomise_runs',exptag)
 qsubdir = pathjoin(resultdir, 'qsubdir_defunctcommands')
 
 ## Covariate Filtering
-# matfiles = csv2fslmat(csvfile, cols=[2], covarcols=[43] selectSubjects=subjects,
-#     groupcol=False, demean=False, outdir=matfiledir, opts=opts)
-# images = multiregfilt(images, matfiles[0], opts=opts)
+matfiles = csv2fslmat(csvfile, cols=[2], covarcols=[43], selectSubjects=subjects,
+    groupcol=False, demean=False, outdir=matfiledir, opts=opts)
+images = multiregfilt(images, matfiles[0], opts=opts)
 
 ## Randomize n500 test run
-designfile = statsdir+'scs_design4col.con'
+designfile = statsdir+'scs_design2col.con'
 assert os.path.isfile(designfile)
 collist = range(5, 8)+range(18, 32)
-matfiles = csv2fslmat(csvfile, cols=collist, covarcols=[2, 43], selectSubjects=subjects,
+matfiles = csv2fslmat(csvfile, cols=collist, selectSubjects=subjects,
     groupcol=True, outdir=matfiledir, opts=opts)
-
 combs = {img:matfiles for img in images}
 randparfiles = multirandpar(combs, designfile, niterations=500,
     workdir=qsubdir, outdir=resultdir, opts=opts)
-
+#opts.dryrun = False
 corrPfiles = [f+'_tfce_corrp_tstat1.nii.gz' for f in randparfiles]
 corrPfiles += [f+'_tfce_corrp_tstat2.nii.gz' for f in randparfiles]
 waitForFiles(corrPfiles, interval=5) # every 5 seconds check if files done.
@@ -66,13 +64,27 @@ report(selectedCorrPfiles1mm, atlas, atlaslabels(atlasfile),
 
 combs = deconstructRandparFiles(selectedCorrPfiles, matdir=matfiledir, imgdir=statsdir)
 
-opts.dryrun = True
-exptag='randpar_n5000_gender_and_vbm_delta_cov_fm_sig_n5000_4col_vbm'
+#opts.dryrun = False
+exptag='filtered_gender_and_vbm_delta_2col_n5000_select'
 resultdir = pathjoin(statsdir,'randomise_runs',exptag)
 
 randparfiles = multirandpar(combs, designfile, niterations=5000,
     tbss=False, workdir=qsubdir, outdir=resultdir, opts=opts)
+corrPfiles = [f+'_tfce_corrp_tstat1.nii.gz' for f in randparfiles]
+corrPfiles += [f+'_tfce_corrp_tstat2.nii.gz' for f in randparfiles]
+waitForFiles(corrPfiles, interval=5) # every 5 seconds check if files done.
 
+selectedCorrPfiles = select(corrPfiles, withVoxelsOverThresholdOf(.95))
+
+selectedCorrPfiles1mm = upsample1mm(selectedCorrPfiles, opts=opts)
+atlasfile = 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz'
+atlas = pathjoin('data','atlases',atlasfile)
+report(selectedCorrPfiles1mm, atlas, atlaslabels(atlasfile), 
+    relevantImageFilenameSegment=-5)
+atlasfile = 'JHU-ICBM-tracts-maxprob-thr0-1mm_newATR1_newCC21_LR_cerebellum.nii.gz'
+atlas = pathjoin('data','atlases',atlasfile)
+report(selectedCorrPfiles1mm, atlas, atlaslabels(atlasfile), 
+    relevantImageFilenameSegment=-5)
 
 
 
