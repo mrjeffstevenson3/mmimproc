0b88a3ac200216978da9650a4da1e5f92c8631a9
Jasper van den Bosch <jasperb@uw.edu>
2015-10-07 14:48:45 -0700

    Phantom pipeline plotting command

--- pylabs/phantomT1/pipeline.py
+++ pylabs/phantomT1/pipeline.py
@@ -1,5 +1,6 @@
 from nipype.interfaces import fsl
 from pylabs.regional import statsByRegion
+import matplotlib.pyplot as plt
 
 vialAtlas = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
 
@@ -10,7 +11,10 @@ vialAtlas = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
 # get region/vial-wise data for each timepoint
 vialTimeseries = numpy.zeros(ntimepoints, nvials)
 for t, phantom in enumerate(scanTimeseries):
+    print('Examining scan {0} of {1}'.format(t,ntimepoints))
     regionalStats = statsByRegion(phantom, vialAtlas)
     vialTimeseries[t, :] = regionalStats['average'])
 
 # plot development over time for each vial
+plt.plot(numpy.arange(ntimepoints), vialTimeseries) 
+plt.show()
