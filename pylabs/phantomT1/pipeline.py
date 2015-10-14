0af0e65121bb53c640cb9c156e6f55a3fefbead3
Jasper van den Bosch <jasperb@uw.edu>
2015-10-14 16:06:52 -0700

    Adapted phantom pipeline to data directory structure.

--- pylabs/phantomT1/pipeline.py
+++ pylabs/phantomT1/pipeline.py
@@ -1,19 +1,40 @@
+from __future__ import print_function
+import os, fnmatch, glob, collections
+from os.path import join
+import numpy
 from nipype.interfaces import fsl
-from pylabs.regional import statsByRegion
 import matplotlib.pyplot as plt
+from pylabs.regional import statsByRegion
+from pylabs.correlation.atlas import atlaslabels
+from pylabs.utils.paths import getlocaldataroot
 
-vialAtlas = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
+#loop through timepoints, pick a method and parameter,
+sessions = glob.glob(join(getlocaldataroot(),'phantom_qT1_disc','phantom*'))
+scansByType = collections.defaultdict(list)
+for session in sessions:
+    for root, dirnames, filenames in os.walk(session):
+        for filename in fnmatch.filter(filenames, '*.nii'):
+            filepath = join(root, filename)
+            scantype = filename.split('.')[0]
+            scansByType[scantype].append(filepath)
+for scantype in scansByType.keys():
+    print('{0} {1}'.format(scantype.ljust(25), len(scansByType[scantype])))
 
-# find phantom raw files
-# process raw files
-    # coregister to atlas file
+atlasfile = 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz'
+vialAtlas = join('data','atlases',atlasfile)
+labels = atlaslabels(atlasfile)
+nvials = len(labels)
+# loop over scantypes here..
+sampleScantype = 'orig_seir_ti_3000_1'
+scans = scansByType[sampleScantype]
+ntimepoints = len(scans)
 
 # get region/vial-wise data for each timepoint
-vialTimeseries = numpy.zeros(ntimepoints, nvials)
-for t, phantom in enumerate(scanTimeseries):
+vialTimeseries = numpy.zeros((ntimepoints, nvials))
+for t, phantom in enumerate(scans):
     print('Examining scan {0} of {1}'.format(t,ntimepoints))
     regionalStats = statsByRegion(phantom, vialAtlas)
-    vialTimeseries[t, :] = regionalStats['average'])
+    vialTimeseries[t, :] = regionalStats['average']
 
 # plot development over time for each vial
 plt.plot(numpy.arange(ntimepoints), vialTimeseries) 
