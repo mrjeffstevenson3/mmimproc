47f5e2b42ac37fc7c5554bd8866c0d416ca9bc58
Jasper van den Bosch <japsai@gmail.com>
2015-10-21 14:28:29 -0700

    Example of T1 fitting with LSE

--- pylabs/qt1/testfit.py
+++ pylabs/qt1/testfit.py
@@ -1,5 +1,7 @@
 import os, glob, collections
 from os.path import join
+import numpy
+from scipy.optimize import curve_fit
 
 path = 'data/phantomsampledata/'
 
@@ -12,3 +14,24 @@ for xy in ('x','y'):
             lines = datafile.readlines()
         data[coords][xy] = [float(l) for l in lines]
 
+
+def func(x, a, b, c):
+    return a * numpy.exp(-b * x) + c
+
+
+for coords in data.keys():
+    print('Running for coordinates: '+coords)
+    X = data[coords]['x']
+    Y = data[coords]['y']
+
+    popt, pcov = curve_fit(func, X, Y)
+
+
+
+# todo polarity restoration
+
+#y = S0 (1-V*e pow())
+
+# 80,63,0  1727
+# 46,65,0  1113
+# 46,30,0   688
