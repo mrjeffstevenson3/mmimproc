019ce097b047f8ff22eb16b2163e0911237b8d51
Jasper van den Bosch <japsai@gmail.com>
2015-10-21 15:05:52 -0700

    T1 fitting; decent solutions by scipy curve_fit

--- pylabs/qt1/testfit.py
+++ pylabs/qt1/testfit.py
@@ -16,15 +16,20 @@ for xy in ('x','y'):
 
 
 def func(x, a, b, c):
-    return a * numpy.exp(-b * x) + c
+    return numpy.abs(a * (1 - b * numpy.exp(-x/c)))
 
 
 for coords in data.keys():
     print('Running for coordinates: '+coords)
-    X = data[coords]['x']
-    Y = data[coords]['y']
+    X = numpy.array(data[coords]['x'])
+    Y = numpy.array(data[coords]['y'])
 
-    popt, pcov = curve_fit(func, X, Y)
+
+    ai = Y[0]
+    bi = 2 # -Y[0]*2
+    ci = 1000
+    popt, pcov = curve_fit(func, X, Y, p0=[ai, bi, ci])
+    print('   fitted parameters: a {0} b {1} c {2}'.format(*popt))
 
 
 
