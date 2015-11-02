08678d5968f3fcd3b5927099a5bf9b149de1a151
Jasper van den Bosch <jasperb@uw.edu>
2015-11-02 15:30:43 -0800

    Fixed progressbar for integer counters

--- pylabs/utils/progress.py
+++ pylabs/utils/progress.py
@@ -2,7 +2,7 @@ import sys
 
 
 def progressbar(done, total):
-    amtDone = done/total
-    template = "\r [{0:50s}] {1:.1f}%"
+    amtDone = done/float(total)
+    template = "\r [{0:50s}] {1:.2f}%"
     sys.stdout.write(template.format('=' * int(amtDone * 50), amtDone * 100))
     sys.stdout.flush()
