5bde539755745116cd30a495b55567d3292b089d
Jasper van den Bosch <jasperb@uw.edu>
2015-11-02 18:13:48 -0800

    Qt1 phantom pipeline fitting and atlassing part updated. Requirements file lsits cloud library now as we can use it to serialize collections.defaultdict.

--- pylabs/qt1/pipeline_jasper.py
+++ pylabs/qt1/pipeline_jasper.py
@@ -1,78 +1,73 @@
 from __future__ import print_function
-import os, fnmatch, glob, collections, datetime
+import os, fnmatch, glob, collections, datetime, cPickle
 from os.path import join
+from collections import defaultdict
 import numpy
-from nipype.interfaces import fsl
 import matplotlib.pyplot as plt
 from pylabs.regional import statsByRegion
 from pylabs.correlation.atlas import atlaslabels
 from pylabs.utils.paths import getlocaldataroot
-from pylabs.qt1.fitting import t1fitCombinedFile
+from pylabs.qt1.fitting import t1fit
 
-def dateFromPath(path):
-    dateSegment = path.split('/')[-3]
-    dateStr = dateSegment.split('_')[2]
-    return datetime.datetime.strptime(dateStr, '%Y%m%d').date()
-    
+### FITTING
 
-#loop through timepoints, pick a method and parameter,
-sessions = glob.glob(join(getlocaldataroot(),'phantom_qT1_disc','phantom*'))
-sessionsByType = collections.defaultdict(list)
-for session in sessions:
-    scanFolders = glob.glob(join(session,'fitted*'))
-    for scanFolder in scanFolders:
-        files = glob.glob(join(scanFolder,'*_mag_b1corr.nii.gz'))
-        if len(files) > 0:
-            scantype = os.path.basename(scanFolder).split('_')[1]
-            sessionsByType[scantype].append(files[0])
-for scantype in sessionsByType.keys():
-    msg = '{0} {1} sessions'
-    print(msg.format(scantype.ljust(25), len(sessionsByType[scantype])))
-
-## FITTING
-
-# loop over scantypes here..
-sampleScantype = 'seir'
-sessions = sessionsByType[sampleScantype]
-seirSessions = dict()
-for s, session in enumerate(sessions):
-    print('Fitting session {0} of {1}..'.format(s, len(sessions)))
-    t1filename = session.replace('.nii','_t1.nii')
-    date = dateFromPath(t1filename)
-    seirSessions[date] = t1filename
-    if not os.path.isfile(t1filename):
-        try:
-            IRs = [500, 1500, 3000]
-            t1fitCombinedFile(session, IRs, scantype='IR', t1filename=t1filename)
-        except ValueError:
-            print("Don't have all images for this session. Continuing.")
+datadir = join(getlocaldataroot(),'phantom_qT1_disc')
+imageDictFile = join(datadir,'conv_scans_dict.txt')
+with open(imageDictFile) as dfile:
+    images = cPickle.load(dfile)
+t1fitImages = defaultdict(lambda: defaultdict(dict)) # method, TR, date
+for sessionString in images.keys():
+    date = datetime.datetime.strptime(sessionString, '%Y%m%d').date()
+    sessiondir = subdir = join(datadir,'phantom_qT1_{0}'.format(sessionString))
+    for method in images[sessionString].keys():
+        if method == 'b1map':
+            continue
+        subdir = join(sessiondir,'fitted_{0}_qT1'.format(method))
+        for TR in images[sessionString][method]:
+            series = images[sessionString][method][TR]
+            if len(series) < 3:
+                continue
+            files, X = zip(*sorted(series, key=lambda s: s[1]))
+            files = [join(subdir, f+'.nii') for f in files]
+            outdir = join(datadir, 'T1_{0}_TR{1}'.format(method, TR))
+            if not os.path.isdir(outdir):
+                os.mkdir(outdir)
+            fname = 'T1_{0}_TR{1}_{2}'.format(method, TR, sessionString)
+            t1filepath = join(outdir, fname)
+            if method.upper() == 'SPGR':
+                t1fit(files, X, t1filename=t1filepath, scantype='SPGR', TR=TR)
+            else:
+                t1fit(files, X, t1filename=t1filepath)
+            t1fitImages[method][TR][date] = t1filepath
 
 
-## ATLASSING
+### ATLASSING
 
 atlasfile = 't1_phantom_mask_course.nii.gz'
 vialAtlas = join('data','atlases',atlasfile)
 labels = atlaslabels(atlasfile)
 nvials = len(labels)
 
-# get region/vial-wise data for each timepoint
-dates = sorted(seirSessions.keys())
-scansInOrder = [seirSessions[d] for d in dates]
-ntimepoints = len(dates)
-vialTimeseries = numpy.zeros((ntimepoints, nvials))
-for t, scan in enumerate(scansInOrder):
-    print('Sampling vials for scan {0} of {1}'.format(t,ntimepoints))
-    if not os.path.isfile(scan): # temp fix to deal with session with only two vols
-        print('Missing t1 image: '+scan)
-        continue
-    regionalStats = statsByRegion(scan, vialAtlas)
-    vialTimeseries[t, :] = regionalStats['average']
+for method in t1fitImages.keys():
+    for TR in t1fitImages[method].keys():
+        plotname = '{0}_{1}.png'.format(method, TR)
+        sessions = t1fitImages[method][TR]
 
-vialTimeseries = numpy.delete(vialTimeseries, 0, 1)
-del labels[0]
+        dates = sorted(sessions.keys())
+        scansInOrder = [sessions[d] for d in dates]
+        ntimepoints = len(dates)
+        vialTimeseries = numpy.zeros((ntimepoints, nvials))
+        for t, scan in enumerate(scansInOrder):
+            print('Sampling vials for scan {0} of {1}'.format(t,ntimepoints))
+            regionalStats = statsByRegion(scan, vialAtlas)
+            vialTimeseries[t, :] = regionalStats['average']
 
-# plot development over time for each vial
-lines = plt.plot(dates, vialTimeseries) 
-plt.legend(lines, labels, loc=8)
-plt.show()
+        # Get rid of background
+        vialTimeseries = numpy.delete(vialTimeseries, 0, 1)
+        del labels[0]
+
+        # plot development over time for each vial
+        lines = plt.plot(dates, vialTimeseries) 
+        plt.legend(lines, labels, loc=8)
+        plt.savefig(plotname)
 
