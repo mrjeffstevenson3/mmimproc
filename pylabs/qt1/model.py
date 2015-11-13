bafc31b85dd5f838ed25ac4ea5e4625d678ff136
Jasper van den Bosch <jasperb@uw.edu>
2015-11-12 16:37:56 -0800

    Reading in T1 phantom vial concentrations.

---
+++ pylabs/qt1/model.py
@@ -0,0 +1,11 @@
+import numpy
+
+# read in constants (concentrations per vial etc)
+concentrationsFile = 'data/T1_phantom_vials_percGel_gdEDTA_concentrations.csv'
+concentrations = numpy.loadtxt(concentrationsFile)
+
+# read in temperature values by vial and session
+
+# establish model
+
+# calculate expected T1 by vial and session
