4f49d89c2cc11e6f9b8616132d92dab03e2642f0
Jasper van den Bosch <jasperb@uw.edu>
2015-11-13 13:43:28 -0800

    Reading in phantom temperature data

---
+++ pylabs/qt1/temperaturedata.py
@@ -0,0 +1,22 @@
+from os.path import join
+import glob, datetime
+
+def decodeDatetime(rawstr):
+    fmt ='%m/%d/%y @ %H:%M%p'
+    datetimeStr = rawstr.strip('\t\n').replace('/201','/1')
+    return datetime.datetime.strptime(datetimeStr, fmt)
+
+def decodeTemp(rawstr):
+    return float(rawstr.strip('\t\n')[:-1])
+
+tempsdir = '/diskArray/data/scs/phantom_temp_readings'
+sessions = {}
+for fpath in glob.glob(join(tempsdir,'*temp.txt')):
+    with open(fpath) as tempfile:
+        lines = tempfile.readlines()
+    sessionDate = decodeDatetime(lines[3]).date()
+    dt1 = decodeDatetime(lines[3])
+    T1 = decodeTemp(lines[4])
+    dt2 = decodeDatetime(lines[7])
+    T2 = decodeTemp(lines[8])
+    sessions[sessionDate] = (dt1, T1, dt2, T2)
