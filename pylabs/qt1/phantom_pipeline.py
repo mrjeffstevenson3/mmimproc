fa4ba03c9879957b382ca4c9c6440cc8d89a81bf
todd98028 <toddr@uw.edu>
2015-12-01 12:21:13 -0800

    this updates the phantom pipeline to working status

--- pylabs/qt1/phantom_pipeline.py
+++ pylabs/qt1/phantom_pipeline.py
@@ -14,7 +14,7 @@ from niprov import Context
 from pylabs.utils._options import PylabsOptions
 _opts = PylabsOptions()
 prov = Context()
-#opts.dryrun = False
+#opts.dryrun = True
 #opts.verbose = True
 verbose = True
 
@@ -26,13 +26,11 @@ phantdirs = sorted(glob(pathjoin(fs, 'phantom_qT1_disc/phantom_qT1_*')), key=lam
 scanner = phantdirs[0].split('/')[-2]
 scanner = str(scanner.split('_')[-1])
 scandateexception = ['20141108']
-phantom_disc_ddata = defaultdict(list)
-phantom_slu_ddata = defaultdict(list)
+phantom_ddata = defaultdict(list)
 
 # for i, p in enumerate(phantdirs):
 #     print i, p
-
-phantdirs = [phantdirs[40]]
+#phantdirs = [phantdirs[40]]
 
 for dir in phantdirs:
     b1mapdir = pathjoin(dir, 'B1map_qT1')
@@ -57,81 +55,59 @@ for dir in phantdirs:
         phantSEIRHSparfiles = sort_par_glob(glob(pathjoin(dir, 'source_parrec/*_IR*_128_HS*.PAR')))
         phantTSEIRparfiles = sort_par_glob(glob(pathjoin(dir, 'source_parrec/*_IRTSE*.PAR')))
 
-    b1map_counter = collections.defaultdict(int)
     for parfile in phantB1parfile:
         scaling = 'dv'
-        b1map_counter[sdate] += 1
-        key, val = phantom_B1_midslice_par2mni(parfile=parfile, outdir=b1mapdir, exceptions=scandateexception,
-                                               outfilename='b1map',run=b1map_counter[sdate], scaling=scaling)
+        key, val = phantom_B1_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, outdir=b1mapdir, exceptions=scandateexception, 
+                                               outfilename='b1map', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+            phantom_ddata[k].append(v)
 
-    seir_counter = collections.defaultdict(int)
-    seir_counter[sdate] += 1
     for parfile in phantSEIRparfiles:
         scaling = 'fp'
-        key, val = phantom_midslice_par2mni(parfile=parfile, method='seir', outdir=seirdir, exceptions=scandateexception,
-                                               outfilename='orig_seir',run=seir_counter[sdate], scaling=scaling)
+        key, val = phantom_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, method='seir', outdir=seirdir, exceptions=scandateexception,
+                                               outfilename='orig_seir', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+             phantom_ddata[k].append(v)
 
     seirhs_counter = collections.defaultdict(int)
     seirhs_counter[sdate] += 1
     for parfile in phantSEIRHSparfiles:
         scaling = 'fp'
-        key, val = phantom_midslice_par2mni(parfile=parfile, method='seirhs', outdir=seirhsdir, exceptions=scandateexception,
-                                               outfilename='orig_seir',run=seirhs_counter[sdate], scaling=scaling)
+        key, val = phantom_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, method='seirhs', outdir=seirhsdir, exceptions=scandateexception,
+                                               outfilename='orig_seir', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+            phantom_ddata[k].append(v)
 
     spgr_counter = collections.defaultdict(int)
     spgr_counter[sdate] += 1
     for parfile in phantSPGRparfiles:
         scaling = 'fp'
-        key, val = phantom_midslice_par2mni(parfile=parfile, method='orig_spgr', outdir=spgrdir, exceptions=scandateexception,
-                                               outfilename='orig_spgr',run=spgr_counter[sdate], scaling=scaling)
+        key, val = phantom_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, method='orig_spgr', outdir=spgrdir, exceptions=scandateexception,
+                                               outfilename='orig_spgr', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+            phantom_ddata[k].append(v)
 
     seirepi_counter = collections.defaultdict(int)
     seirepi_counter[sdate] += 1
     for parfile in phantSEIREPIparfiles:
         scaling = 'fp'
-        key, val = phantom_midslice_par2mni(parfile=parfile, method='seirepi', outdir=seirepidir, exceptions=scandateexception,
-                                               outfilename='seirepi',run=seirepi_counter[sdate], scaling=scaling)
+        key, val = phantom_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, method='seirepi', outdir=seirepidir, exceptions=scandateexception,
+                                               outfilename='seirepi', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+            phantom_ddata[k].append(v)
 
     tseir_counter = collections.defaultdict(int)
     tseir_counter[sdate] += 1
     for parfile in phantTSEIRparfiles:
         scaling = 'fp'
-        key, val = phantom_midslice_par2mni(parfile=parfile, method='tseir', outdir=seirepidir, exceptions=scandateexception,
-                                               outfilename='tseir',run=tseir_counter[sdate], scaling=scaling)
+        key, val = phantom_midslice_par2mni(parfile=parfile, datadict=phantom_ddata, method='tseir', outdir=seirepidir, exceptions=scandateexception,
+                                               outfilename='tseir', scaling=scaling)
         for k, v in zip(key, val):
-            if scanner == 'disc':
-                phantom_disc_ddata[k].append(v)
-            elif scanner == 'slu':
-                phantom_slu_ddata[k].append(v)
+            phantom_ddata[k].append(v)
 
 if scanner == 'disc':
     with open(pathjoin('/'.join(phantdirs[0].split('/')[0:-1]), 'phantom_disc_dict.txt'), "wb") as f:
-        f.write(dumps(phantom_disc_ddata))
+        f.write(dumps(phantom_ddata))
 elif scanner == 'slu':
     with open(pathjoin('/'.join(phantdirs[0].split('/')[0:-1]), 'phantom_slu_dict.txt'), "wb") as f:
-        f.write(dumps(phantom_slu_ddata))
+        f.write(dumps(phantom_ddata))
