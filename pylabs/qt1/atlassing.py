67a16f89d4aa47ba26cb214e1e4ef87ac57c6803
Jasper van den Bosch <jasperb@uw.edu>
2015-12-04 19:36:47 -0800

    Working with new file structure for coreg phantom files for atlassing

---
+++ pylabs/qt1/atlassing.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import os, fnmatch, glob, collections, datetime, cPickle, sys
+from os.path import join
+import numpy
+import matplotlib.pyplot as plt
+from pylabs.regional import statsByRegion
+from pylabs.correlation.atlas import atlaslabels
+from pylabs.utils.paths import getlocaldataroot
+
+
+def findfile(rootdir, method, TR, date, runIndex, b1corr, coreg):
+    b1corrtag = {True:'_b1corr',False:''}
+    trstr = str(TR).replace('.','p')
+    dirvars = (method, trstr)
+    if coreg:
+        dirtem = 'T1_{0}_TR{1}_reg2723_dec4{2}'
+        dirvars += (b1corrtag[b1corr],)
+    else:
+        dirtem = 'T1_{0}_TR{1}'
+    dirpath = join(rootdir, dirtem.format(*dirvars))
+    filetem = 'T1_{0}_TR{1}_{2}_{3}{4}.nii.gz'
+    filevars = (method, trstr, date, runIndex, b1corrtag[b1corr])
+    filepath = join(dirpath, filetem.format(*filevars))
+    if not os.path.isfile(filepath):
+        return None
+    return filepath
+
+
+### ATLASSING
+
+coreg = False
+rootdir = join(getlocaldataroot(),'phantom_qT1_disc')
+imageDictFile = join(rootdir,'phantom_disc_dict_dec3.txt')
+with open(imageDictFile) as dfile:
+    images = cPickle.load(dfile)
+
+vialdata = {}
+
+for key, run in images.items():
+    date = key[0]
+    method = key[1]
+    TR = key[2]
+    runIndex = key[3]
+
+    for b1corr in [True,False]:
+        # check if we have file
+        filepath = findfile(rootdir, method, TR, date, runIndex, b1corr, coreg)
+
+        print(filepath)
+        #regionalStats = statsByRegion(scan, vialAtlas)
+
+
+
+#atlasfile = 't1_phantom_mask_course.nii.gz'
+#vialAtlas = join('data','atlases',atlasfile)
+#labels = atlaslabels(atlasfile)
+#nvials = len(labels)
+
+#for key, timeseries in t1fitTimeseries.items():
+#    method = key[0]
+#    TR = key[1]
+
+#    plotname = '{0}_{1}.png'.format(method, TR)
+
+#    dates = sorted(timeseries.keys())
+#    scansInOrder = [timeseries[d] for d in dates]
+#    ntimepoints = len(dates)
+#    vialTimeseries = numpy.zeros((ntimepoints, nvials))
+#    for t, scan in enumerate(scansInOrder):
+#        print('Sampling vials for scan {0} of {1}'.format(t,ntimepoints))
+#        regionalStats = statsByRegion(scan, vialAtlas)
+#        vialTimeseries[t, :] = regionalStats['average']
+
+#    # Get rid of background
+#    vialTimeseries = numpy.delete(vialTimeseries, 0, 1)
+#    del labels[0]
+
+#    # plot development over time for each vial
+#    lines = plt.plot(dates, vialTimeseries) 
+#    plt.legend(lines, labels, loc=8)
+#    plt.savefig(plotname)
