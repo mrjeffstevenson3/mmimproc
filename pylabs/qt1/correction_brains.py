d7cad777958a50cb59ef1e0f76f8e3141871b13a
Jasper van den Bosch <jasperb@uw.edu>
2015-12-12 12:06:50 -0800

    Fixed bug in correction and saved factors to tsv files.

--- pylabs/qt1/correction_brains.py
+++ pylabs/qt1/correction_brains.py
@@ -28,7 +28,10 @@ for b1corr in [True, False]:
     #factors[b1corr].byNearestDate()
     factors[b1corr].byDateRangeInterceptAndSlope(subjects.values())
 
+tabledata = {True:{}, False:{}}
+
 for subject in subjects.keys():
+    acqdate = subjects[subject]
     print('Subject {0} data acquired {1}.'.format(subject, acqdate))
     subjectdir = join(rootdir, subject)
     for b1corr in [True, False]: 
@@ -48,5 +51,15 @@ for subject in subjects.keys():
         nibabel.save(corrimg, outfile)
         with open(rationalefilepath, 'w') as logfile:
             logfile.write(rationale)
+        tabledata[b1corr][subject] = x
 
+
+for b1corr in [True, False]: 
+    tdata = tabledata[b1corr]
+    b1corrtag = {False:'',True:'_b1corr'}[b1corr]
+    tablefname = 'fcor_table_{0}{1}.tsv'.format(factors[b1corr].name, b1corrtag)
+    lines = ['{0}\t{1}'.format(s, tdata[s]) for s in tdata.keys()]
+    content = '\n'.join(lines)
+    with open(tablefname, 'w') as tablefile:
+        tablefile.write(content)
 
