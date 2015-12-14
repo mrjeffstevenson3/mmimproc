475413cef678673acda52cd540d4dce544273dd9
Jasper van den Bosch <jasperb@uw.edu>
2015-12-14 15:58:45 -0800

    Gilad v Tofts comparison now with difference to model numbers

--- pylabs/qt1/compare_gilad_vs_tofts.py
+++ pylabs/qt1/compare_gilad_vs_tofts.py
@@ -8,7 +8,7 @@ with open(datafilepath) as datafile:
     factordata = pickle.load(datafile)
 
 giladkey = ('gilad_spgr_mag', 11.0, True)
-toftskey = ('orig_spgr_mag', 11.0, True)
+toftskey = ('orig_spgr_mag', 11.0, False)
 
 giladdates = factordata[giladkey]['dates']
 toftsdates = factordata[toftskey]['dates']
@@ -19,16 +19,28 @@ toftsDateIndices = [toftsdates.index(d) for d in commondates]
 
 gilad = numpy.array(factordata[giladkey]['observed'])[giladDateIndices,:]
 tofts = numpy.array(factordata[toftskey]['observed'])[toftsDateIndices,:]
-difference = gilad-tofts
-reldiff = (difference/gilad)*100
-
-print('Overall mean difference: {0}%'.format(numpy.abs(reldiff).mean()))
-
+model = numpy.array(factordata[giladkey]['model'])[giladDateIndices,:]
 
 atlasfname = 'T1_seir_mag_TR4000_2014-07-23_mask.nii.gz'
 labels = atlaslabels(atlasfname)
 labels = labels[1:] # remove label for background
 dates = sorted(commondates)
 
+## Difference to model:
+giladRelDiff = (model-gilad)/model
+toftsRelDiff = (model-tofts)/model
+
+print('Tofts mean absolute difference from model: {:.2%}'.format(
+    numpy.abs(toftsRelDiff).mean()))
+print('Gilad mean absolute difference from model: {:.2%}'.format(
+    numpy.abs(giladRelDiff).mean()))
+
+
+
+
+## Direct difference
+difference = gilad-tofts
+reldiff = (difference/gilad)*100
+print('Overall mean difference: {:.2}%'.format(numpy.abs(reldiff).mean()))
 plotT1Timeseries(dates, difference, labels, 'gilad-v-tofts_absdiff', dtype='absdiff')
-plotT1Timeseries(dates, reldiff, labels, 'gilad-v-tofts_refdiff', dtype='reldiff') # 
+plotT1Timeseries(dates, reldiff, labels, 'gilad-v-tofts_refdiff', dtype='reldiff') 
