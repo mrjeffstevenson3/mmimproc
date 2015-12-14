2eccbc02f1ec0a38c73e137e91cda48b0ddff8cc
Jasper van den Bosch <jasperb@uw.edu>
2015-12-14 14:31:26 -0800

    phantom fit deathmatch comparison now makes plots too

--- pylabs/qt1/compare_gilad_vs_tofts.py
+++ pylabs/qt1/compare_gilad_vs_tofts.py
@@ -1,4 +1,6 @@
 import pickle, numpy
+from pylabs.qt1.plotting import plotT1Timeseries
+from pylabs.correlation.atlas import atlaslabels
 
 coregtag = {True:'coreg',False:'nocoreg'}[True]
 datafilepath = 'data/t1_factordata_{0}.pickle'.format(coregtag)
@@ -18,3 +20,15 @@ toftsDateIndices = [toftsdates.index(d) for d in commondates]
 gilad = numpy.array(factordata[giladkey]['observed'])[giladDateIndices,:]
 tofts = numpy.array(factordata[toftskey]['observed'])[toftsDateIndices,:]
 difference = gilad-tofts
+reldiff = (difference/gilad)*100
+
+print('Overall mean difference: {0}%'.format(numpy.abs(reldiff).mean()))
+
+
+atlasfname = 'T1_seir_mag_TR4000_2014-07-23_mask.nii.gz'
+labels = atlaslabels(atlasfname)
+labels = labels[1:] # remove label for background
+dates = sorted(commondates)
+
+plotT1Timeseries(dates, difference, labels, 'gilad-v-tofts_absdiff', dtype='absdiff')
+plotT1Timeseries(dates, reldiff, labels, 'gilad-v-tofts_refdiff', dtype='reldiff') # 
