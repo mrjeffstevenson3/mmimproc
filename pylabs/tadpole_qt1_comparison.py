917c935dd5bf434887d5362c9f0d2b32e7e3e05f
Jasper van den Bosch <jasperb@uw.edu>
2016-01-19 15:20:26 -0800

    Outlined pipeline for tadpole qt1 FA range evaluation

--- pylabs/tadpole_qt1_comparison.py
+++ pylabs/tadpole_qt1_comparison.py
@@ -1,8 +1,15 @@
 from pylabs.qt1.brain_qt1_pipeline_v2 import sort_par_glob, convertParFiles
 
+## Evaluate which flip angles are required to do an adequate SPGR QT1
+## based on tadpole phantom 999
+
 fs = getlocaldataroot()
-datadir = join(fs, 'tadpole/tadpole_999')
-subjSPGRparfiles = glob(pathjoin(datadir, 'source_parrec/*T1_MAP*.PAR'))
-subjSPGRparfiles = sort_par_glob(subjSPGRparfiles)
-niioutdir = pathjoin(datadir, 'source_nii')
-convertParFiles(subjSPGRparfiles, niioutdir)
+subjectdir = join(fs, 'phantom_qT1_slu/sub-phant20160113')
+qt1dir = join(subjectdir, 'anat')
+
+## call phantom_pipeline
+## model_pipeline
+## fitting_phantoms
+## coregister_phantoms
+## atlassing (phantoms)
+
