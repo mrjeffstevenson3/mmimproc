dea48c581f5b878d41694a761667a924dc96b1c8
Jasper van den Bosch <jasperb@uw.edu>
2016-01-19 15:59:18 -0800

    Converting par2mni for tadpole qt1 FA range evaluation

--- pylabs/tadpole_qt1_comparison.py
+++ pylabs/tadpole_qt1_comparison.py
@@ -1,15 +1,40 @@
-from pylabs.qt1.brain_qt1_pipeline_v2 import sort_par_glob, convertParFiles
+from os.path import join
+import collections 
+from pylabs.utils.paths import getlocaldataroot
+from pylabs.conversion.phantom_conv import phantom_midslice_par2mni as par2mni
+
+import glob
+def sortedParGlob (filefilter):
+    return sorted(glob.glob(filefilter), key=lambda f: int(f.split('_')[-2]))
 
 ## Evaluate which flip angles are required to do an adequate SPGR QT1
 ## based on tadpole phantom 999
 
 fs = getlocaldataroot()
-subjectdir = join(fs, 'phantom_qT1_slu/sub-phant20160113')
-qt1dir = join(subjectdir, 'anat')
+projectdir = join(fs, 'phantom_qT1_slu')
+subj = 'sub-phant20160113'
+subjectdir = join(projectdir, subj)
 
-## call phantom_pipeline
-## model_pipeline
+## convert parrecs to nifti
+anatdir = join(subjectdir, 'anat')
+niftiDict = collections.defaultdict(list)
+spgrFilter = 'source_parrec/*SPGR*.PAR'
+phantSPGRparfiles = sortedParGlob(join(subjectdir, spgrFilter))
+scaling = 'fp'
+method = 'orig_spgr'
+fname = subj+'_'+'orig_spgr'
+for parfile in phantSPGRparfiles:
+    key, val = par2mni(parfile=parfile, datadict=niftiDict, method=method, 
+        outdir=anatdir, exceptions=[], outfilename=fname, scaling=scaling)
+    for k, v in zip(key, val):
+        niftiDict[k].append(v)
+
 ## fitting_phantoms
 ## coregister_phantoms
+## model_pipeline
 ## atlassing (phantoms)
+
+
+
+
 
