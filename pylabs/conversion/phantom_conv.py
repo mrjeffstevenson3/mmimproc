fixed IR real and mag swapping between slu and disc. scaling to mni works for all acq.

pylabs/conversion/phantom_conv.py
@@ -85,7 +85,7 @@ def phantom_B1_midslice_par2mni(parfile, datadict, outdir=None, exceptions=None,
 
     in_slice_mag = in_data_ras[:,:,mid_slice_num-1,0]
     in_slice_phase = in_data_ras[:,:,mid_slice_num-1,tdim-1]
-    mnizoomfactor = 218*spacing/float(fov)
+    mnizoomfactor = 1/spacing
     slice_mag218 = scipy.ndimage.zoom(in_slice_mag, mnizoomfactor, order=0)
     slice_phase218 = scipy.ndimage.zoom(in_slice_phase, mnizoomfactor, order=0)
     slice_mag_mni = slice_mag218[18:200,:]
@@ -240,8 +240,36 @@ def phantom_midslice_par2mni(parfile, datadict, method, outdir=None, exceptions=
     if method == 'orig_spgr' and len(pr_hdr._shape) == 4 and scanner == 'slu':
         in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 1]
 
-    mnizoomfactor = 218*spacing/float(fov)
-    slice_mag218 = scipy.ndimage.zoom(in_slice_mag, mnizoomfactor, order=0)
+    if method == 'orig_spgr' and len(pr_hdr._shape) == 4 and scanner == 'disc':
+        in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 0]
+
+    if method != 'orig_spgr' and method != 'tseir' and scanner == 'slu':
+        in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 1]
+
+    if method != 'orig_spgr' and method != 'tseir' and scanner == 'disc':
+        in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 0]
+
+    if method == 'tseir' and scanner == 'slu':
+        in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 2]
+
+    if method == 'tseir' and scanner == 'disc':
+        in_slice_mag = in_data_ras[:,:, mid_slice_num-1, 0]
+
+
+    mmcropfactor = fov/218.
+    newsizediff = in_slice_mag.shape[0] - (in_slice_mag.shape[0]*mmcropfactor)
+    crop = round(newsizediff/2.)
+
+    if newsizediff <= 0:
+        crop = abs(crop)
+        crop_in_slice_mag = in_slice_mag[crop: -crop, crop: -crop]
+    else:
+        newdim = in_slice_mag.shape[0]+(crop*2)
+        crop_in_slice_mag = np.zeros((newdim, newdim))
+        crop_in_slice_mag[crop: -crop, crop: -crop] = in_slice_mag
+
+    zoomto218 = 218./crop_in_slice_mag.shape[0]
+    slice_mag218 = scipy.ndimage.zoom(crop_in_slice_mag, zoomto218, order=0)
     slice_mag_mni = slice_mag218[18:200,:]
 
     if scandate not in exceptions:
@@ -297,10 +325,36 @@ def phantom_midslice_par2mni(parfile, datadict, method, outdir=None, exceptions=
     prov.log(outfilename+'_mag_1slmni_'+str(run)+'.nii', 'magnitude of '+outfilename, parfile)
 
 
-    if len(pr_hdr._shape) == 4 and pr_hdr._shape[3] == 2 and method != 'orig_spgr':
-        in_slice_real = in_data_ras[:,:, mid_slice_num-1,1]
-        slice_real218 = scipy.ndimage.zoom(in_slice_real, mnizoomfactor, order=0)
+    if len(pr_hdr._shape) == 4 and pr_hdr._shape[3] >= 2 and method != 'orig_spgr':
+
+        if method != 'tseir' and scanner == 'slu':
+            in_slice_real = in_data_ras[:,:, mid_slice_num-1,0]
+
+        if method != 'tseir' and scanner == 'disc':
+            in_slice_real = in_data_ras[:,:, mid_slice_num-1,1]
+
+        if method == 'tseir' and scanner == 'slu':
+            in_slice_real = in_data_ras[:,:, mid_slice_num-1, 0]
+
+        if method == 'tseir' and scanner == 'disc':
+            in_slice_real = in_data_ras[:,:, mid_slice_num-1, 1]
+
+        mmcropfactor = fov/218.
+        newsizediff = in_slice_real.shape[0] - (in_slice_real.shape[0]*mmcropfactor)
+        crop = round(newsizediff/2.)
+
+        if newsizediff <= 0:
+            crop = abs(crop)
+            crop_in_slice_real = in_slice_real[crop: -crop, crop: -crop]
+        else:
+            newdim = in_slice_real.shape[0]+(crop*2)
+            crop_in_slice_real = np.zeros((newdim, newdim))
+            crop_in_slice_real[crop: -crop, crop: -crop] = in_slice_real
+
+        zoomto218 = 218./crop_in_slice_real.shape[0]
+        slice_real218 = scipy.ndimage.zoom(crop_in_slice_real, zoomto218, order=0)
         slice_real_mni = slice_real218[18:200,:]
+
         if scandate not in exceptions:
             slice_real_mni = np.fliplr(slice_real_mni)
         elif scandate in exceptions: