fd61359c10accc883ae863311ee2add597b2a352
mrjeffs <mrjeffs@uw.edu>
2016-02-03 17:32:33 -0800

    1st subj mega press

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -2,9 +2,15 @@ from pylabs.utils._run import Shell
 import subprocess
 shell = Shell()
 
-mpath = '/home/toddr/Software/gannett_spectro/Gannet2/Gannet2.0-master'
-act = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_act.SDAT'
-ref = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_ref.SDAT'
+#mpath = '/home/toddr/Software/gannett_spectro/Gannet2/Gannet2.0-master'
+#act = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_act.SDAT'
+#ref = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_ref.SDAT'
+mpath = '/Users/mrjeffs/Software/Gannet2.0-master'
+act = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_act.SDAT'
+ref = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_ref.SDAT'
+
+
+
 
 mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
         mpath, act, ref)
