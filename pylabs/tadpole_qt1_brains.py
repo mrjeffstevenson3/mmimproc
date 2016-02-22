158143cc2953eb7a4cd27132efba0a727effc9ac
Jasper <japsai@gmail.com>
2016-02-22 13:56:06 -0800

    Started separate (for now) pipeline for tadpole qt1 brains.

---
+++ pylabs/tadpole_qt1_brains.py
@@ -0,0 +1,61 @@
+from os.path import join
+import collections, itertools, os
+from pylabs.utils.paths import getlocaldataroot
+from pylabs.utils.files import sortedParGlob
+from pylabs.conversion.phantom_conv import (phantom_midslice_par2mni, 
+                                        phantom_B1_midslice_par2mni)
+from pylabs.qt1.fitting_phantoms import fitPhantoms
+## Evaluate which flip angles are required to do an adequate SPGR QT1
+
+fs = getlocaldataroot()
+projectdir = join(fs, 'phantom_qT1_slu')
+subj = 'sub-phant2016-01-13'
+subjectdir = join(projectdir, subj)
+
+## convert parrecs to nifti
+niftiDict = collections.defaultdict(list)
+parfiles = sortedParGlob(join(subjectdir, 'source_parrec/*.PAR'))
+for parfile in parfiles:
+    args = {}
+    if 'SPGR' in parfile or 'IRTSE' in parfile:
+        if 'SPGR' in parfile:
+            method = 'orig_spgr'
+        else:
+            method = 'tseir'
+        args['scaling'] = 'fp'
+        args['method'] = method
+        args['outdir'] = join(subjectdir, 'anat')
+        par2mni = phantom_midslice_par2mni
+    elif 'B1' in parfile and 'fixed' in parfile:
+        method = 'b1map'
+        args['scaling'] = 'dv'
+        args['outdir'] = join(subjectdir, 'fmap')
+        par2mni = phantom_B1_midslice_par2mni
+    else: 
+        continue
+    args['parfile'] = parfile
+    args['datadict'] = niftiDict
+    args['flipexception'] = ['']
+    args['protoexception'] = ['20160113']
+    args['outfilename'] = subj+'_'+method
+    key, val = par2mni(**args)
+    for k, v in zip(key, val):
+        niftiDict[k].append(v)
+
+xdict = {}
+for key in niftiDict:
+    if key[1]=='orig_spgr_mag':
+        ## Figure out which combinations of flip angles we can test
+        spgrKey = [k for k in niftiDict.keys() if k[1]=='orig_spgr_mag'][0]
+        spgrFilesAndFlipAngles = niftiDict[spgrKey]
+        allAngles = sorted([i[1] for i in spgrFilesAndFlipAngles])
+        combinations = list(itertools.combinations(allAngles, 3))
+        xdict[key] = combinations
+
+t1images = fitPhantoms(niftiDict, projectdir=projectdir, xdict=xdict, 
+    skipExisting=True)
+
+
+
+
+
