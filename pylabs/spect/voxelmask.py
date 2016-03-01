6b63b88964e4fb9f0c02449cf5dac2a70a2a8f52
Jasper <japsai@gmail.com>
2016-02-29 19:47:11 -0800

    MRS voxel mask; combined algorithm and data shape

--- pylabs/spect/voxelmask.py
+++ pylabs/spect/voxelmask.py
@@ -1,4 +1,6 @@
 from nipype.interfaces import fsl
+import nibabel
+import matplotlib.pyplot as plt
 
 
 coordsfile = 'pylabs/spect/coords.txt'
@@ -8,21 +10,52 @@ coords = []
 with open(coordsfile) as cfile:
     lines = cfile.readlines()
     for line in lines:
-        coords.append(line.split()[0:3])
+        coords.append([float(n) for n in line.split()[0:3]])
+coords = numpy.round(coords).astype(int)
+
+raise ValueError
 
 sides = [
-(0,1)
-(1,2)
-(2,3)
-(4,5)
-(5,6)
-(6,7)
-(0,4)
-(1,5)
-(2,6)
-(3,7)
-(0,3)
-(4,7)
+(0,1),
+(1,2),
+(2,3),
+(4,5),
+(5,6),
+(6,7),
+(0,4),
+(1,5),
+(2,6),
+(3,7),
+(0,3),
+(4,7),
 ]
+nsides = len(sides)
+
+img = nibabel.load(basefile)
+data = img.get_data()
+bright = data.max()
+dims = data.shape
+
+for x in range(dims[0]):
+    for y in range(dims[1]):
+        for z in range(dims[2]):
+            sideResults = []
+            for side in sides:
+                sideVector = coords[side[0]] - coords[side[1]]
+                pointVector = coords[side[0]] - numpy.array((x,y,z))
+                sideResults.append(0 < numpy.cross(sideVector,pointVector))
+            if sum(sideResults) == nsides:
+                data[x,y] = bright
+
+
+plt.imshow(data[:,100,:])
+plt.show()
+
+
+#In  my coordinate system, 
+#X is A/P, 0 at back of brain
+#Y is L/R, 0 at right of brain
+#Z is slice direction, 0 at bottom of brain
+
 
 
