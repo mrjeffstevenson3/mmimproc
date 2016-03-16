97bca26a103367e500689613271eb6a4fec721b8
Jeff Stevenson <mrjeffs@uw.edu>
2016-03-16 15:33:19 -0700

    save to view conflicts in tadpole

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -3,8 +3,8 @@ import subprocess
 shell = Shell()
 
 mpath = '/home/toddr/Software/gannett_spectro/Gannet2/Gannet2.0-master'
-act = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_act.SDAT'
-ref = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_ref.SDAT'
+act = '/media/DiskArray/shared_data/js/tadpole/TADPOLE998D/SPARSDAT/TADPOLE_998D_WIP_GABAMM_TE80_7_2_raw_act.SDAT'
+ref = '/media/DiskArray/shared_data/js/tadpole/TADPOLE998D/SPARSDAT/TADPOLE_998D_WIP_GABAMM_TE80_7_2_raw_ref.SDAT'
 
 mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
         mpath, act, ref)
