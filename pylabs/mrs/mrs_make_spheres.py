9ec75a685b19090543a434b758aa163b6f71fbfa
Jeff Stevenson <mrjeffs@uw.edu>
2016-03-29 15:50:41 -0700

    crop and rotate and make shape = mni in python

--- pylabs/mrs/mrs_make_spheres.py
+++ pylabs/mrs/mrs_make_spheres.py
@@ -1,6 +1,8 @@
 import sys, os, shutil
 from glob import glob
 from os.path import join as pathjoin
+from collections import defaultdict
+import dill #to use as pickle replacement of lambda dict
 import numpy as np
 import scipy.ndimage
 import nibabel
@@ -13,11 +15,20 @@ import subprocess
 opts = PylabsOptions()
 prov = Context()
 fslbet = fsl.BET()
+fslmaths = fsl.BinaryMaths()
 
+targetdims = (182, 218, 200)
 identity_matrix = np.eye(4)
 mni_affine = np.array([[-1, 0, 0, 90], [0, 1, 0, -126], [0, 0, 1, -72], [0, 0, 0, 1]])
 psl2ras = np.array([[0., 0., -1., 0.], [-1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 0., 1.]])
+templatedict = defaultdict(lambda: defaultdict(list))
 
+a = [('ANTS12-0Months3T_head_bias_corrected.nii.gz', {'zcutoff': 14, 'ztrans': 0, 'xrot': -22, 'origdims': (141, 178, 144)}),
+     ('ANTS6-0Months3T_head_bias_corrected.nii.gz', {'zcutoff': 41, 'ztrans': 26.5, 'xrot': -16, 'origdims': (147, 170, 176)}),
+     ('ANTS15-0Months_head_bias_corrected.nii.gz', {'zcutoff': 19, 'ztrans': -4, 'xrot': -26, 'origdims': (150, 180, 155)}),
+      ]
+for i in a:
+    templatedict[i[0]] = i[1]
 
 def printmessage(msg, indent=0):
     if verbose:
@@ -33,15 +44,27 @@ prov.dryrun = True
 fs = getlocaldataroot()
 pathtotemplates = pathjoin(fs, 'tadpole/sphere_sources')
 outputdir = pathjoin(fs, 'tadpole/atlases2')
-templatefiles = set(glob(pathjoin(fs, pathtotemplates, '*_head.nii.gz'))) - set(glob(pathjoin(fs, pathtotemplates, '*_t2w_*')))
+templatefiles = set(glob(pathjoin(fs, pathtotemplates, '*_head_bias_corrected.nii.gz'))) - set(glob(pathjoin(fs, pathtotemplates, '*_t2w_*')))
 templatefiles = list(templatefiles)
 if outputdir and not os.path.exists(pathjoin(outputdir, 'tmp')):
     os.makedirs(pathjoin(outputdir, 'tmp'))
 
-tf = templatefiles[2]
+tf = templatefiles[1]
 
 for tf in templatefiles:
-    tf_age = tf.split('/')[-1].split('_')[-2][4:]
+    tfname = tf.split('/')[-1]
+    tf_age = tfname.split('_')[-2][4:]
+    tf_img = nibabel.load(tf)
+    tf_data = np.array(tf_img.dataobj)
+    tf_data_zcrop = tf_data
+    tf_data_zcrop[:,:,0:templatedict[tfname]['zcutoff']] = 0
+    tf_data_rot = scipy.ndimage.interpolation.rotate(tf_data_zcrop, templatedict[tfname]['xrot'],  axes=(2, 1))
+    shape_diff = [int(round((a - b)/2)) for a, b in zip(list(targetdims), list(tf_data_rot.shape))]
+    if shape_diff[2] < 0 and abs(shape_diff[2]*2) < templatedict[tfname]['zcutoff']:
+        mni_array = np.zeros(targetdims)
+        temp = tf_data_rot[:,:,abs(shape_diff[2]*2):]
+        mni_array[shape_diff[0]:-shape_diff[0]-1, shape_diff[1]:-shape_diff[1],:] = temp
+
     fslbet.inputs.in_file = tf
     fslbet.inputs.frac = 0.3
     fslbet.inputs.surfaces = True
