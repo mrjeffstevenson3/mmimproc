0bf28785933a08f1e8e470eea052e6ce5d168b59
Jasper van den Bosch <jasperb@uw.edu>
2016-04-11 19:30:16 -0700

    saturation correction test added fitting code

--- pylabs/qt1/saturation_correction_test.py
+++ pylabs/qt1/saturation_correction_test.py
@@ -1,12 +1,27 @@
 import collections
 from os.path import join
 from pylabs.utils.paths import getlocaldataroot
+import glob
+from pylabs.qt1.fitting import spgrformula
+from scipy.optimize import curve_fit
 
 fs = getlocaldataroot()
 projectdir = join(fs, 'phantom_qT1_slu')
-subjects = ['sub-phant2016-03-02', 'sub-phant2016-03-11']
-niftiDict = collections.defaultdict(list)
+subject = 'sub-phant2016-03-02'
+anatdir = join(projectdir, subject, 'anat')
 
-## convert parrecs to nifti
-for subj in subjects:
-    subjectdir = join(projectdir, subj)
+anglefiles = sorted(glob.glob(join(anatdir,'*14*1.nii')))
+X = [7,10,15,20,30]
+
+
+## fitting
+ai = 15*Y.max()  # S0
+bi = 1000           # T1
+p0=[ai, bi]
+spgrformula.TR = TR
+formula = spgrformula
+poi = 1
+
+popt, pcov = curve_fit(formula, Xv, Y, p0=p0)
+
+
