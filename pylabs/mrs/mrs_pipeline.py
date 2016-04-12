2ee63f0de716b7b515856a5c4befedc5bd5e8806
mrjeffs <mrjeffs@uw.edu>
2016-04-12 01:02:06 -0700

    latest subj 901

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -6,9 +6,10 @@ shell = Shell()
 #act = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_act.SDAT'
 #ref = '/media/DiskArray/shared_data/js/tadpole/TADPOLE_999A/SPARSDAT/TADPOLE_999A_WIP_AM_GABA_21_2_raw_ref.SDAT'
 mpath = '/Users/mrjeffs/Software/Gannet2.0-master'
-act = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_act.SDAT'
-ref = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_ref.SDAT'
-
+#act = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_act.SDAT'
+#ref = '/Users/mrjeffs/data/tadpole/TADPOLE_998B/SPARSDAT/TADPOLE_998b_WIP_GABA_MACRO_TE80_1P9_1P5_23MSPULSE_128N_9_2_raw_ref.SDAT'
+act = '/Users/mrjeffs/Documents/Research/tadpole/TADPOLE901_GABA/TADPOLE901_WIP_GABAMM_TE80_12_2_raw_act.SDAT'
+ref = '/Users/mrjeffs/Documents/Research/tadpole/TADPOLE901_GABA/TADPOLE901_WIP_GABAMM_TE80_12_2_raw_ref.SPAR'
 
 
 
