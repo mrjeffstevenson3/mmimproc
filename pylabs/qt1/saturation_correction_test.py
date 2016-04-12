13aff0d852f95659ccc619b32ab776beaa5983c6
Jasper van den Bosch <jasperb@uw.edu>
2016-04-11 20:37:58 -0700

    saturation test added plots

--- pylabs/qt1/saturation_correction_test.py
+++ pylabs/qt1/saturation_correction_test.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import collections
 from os.path import join
 import numpy
@@ -10,9 +11,10 @@ from pylabs.regional import statsByRegion
 from pylabs.correlation.atlas import atlaslabels
 import numpy as np
 from numpy import cos, sin, exp
+import matplotlib.pyplot as plt
 
 def frac_sat(a, TR, T1):
-    return round(((1-cos(np.radians(a)))*exp(-TR/T1))/(1-(cos(np.radians(a))*exp(-TR/T1))), 5)
+    return round(((1-cos(a))*exp(-TR/T1))/(1-(cos(a)*exp(-TR/T1))), 5)
 
 fs = getlocaldataroot()
 projectdir = join(fs, 'phantom_qT1_slu')
@@ -44,15 +46,32 @@ Y = numpy.array(Y)
 
 ## fitting
 TR = 14.0
-ai = 15*Y.max()  # S0
+#ai = 15*Y.max()  # S0
+ai = Y.max()/sin(X[1])
 bi = 1000           # T1
 p0=[ai, bi]
 spgrformula.TR = TR
 formula = spgrformula
 
 popt, pcov = curve_fit(formula, X, Y, p0=p0)
+T1 = popt[1]
 
-fsat = frac_sat(popt[0], TR, popt[1])
+## plot
+Xrange = numpy.radians(numpy.arange(200))
+fit = [formula(x, *popt) for x in Xrange]
+plt.plot(Xrange, fit) 
+plt.plot(X, Y, 'bo')
+
+
+## sat
+for x, y in zip(X,Y):
+    fsat = frac_sat(x, TR, T1)
+    print((x, y, fsat))
+
+sat = numpy.array([frac_sat(x, TR, T1) for x in Xrange])
+plt.plot(Xrange, sat*10000) 
+plt.show()
+
 
 
 
