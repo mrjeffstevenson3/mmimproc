3bbed1c46f59fea2ba075b240e5ab8254416f996
Jasper van den Bosch <jasperb@uw.edu>
2016-04-11 20:04:24 -0700

    saturation correction test progress

--- pylabs/qt1/saturation_correction_test.py
+++ pylabs/qt1/saturation_correction_test.py
@@ -1,9 +1,18 @@
 import collections
 from os.path import join
+import numpy
 from pylabs.utils.paths import getlocaldataroot
 import glob
 from pylabs.qt1.fitting import spgrformula
+from pylabs.qt1.coregister_phantoms import coregisterPhantoms
 from scipy.optimize import curve_fit
+from pylabs.regional import statsByRegion
+from pylabs.correlation.atlas import atlaslabels
+import numpy as np
+from numpy import cos, sin, exp
+
+def frac_sat(a, TR, T1):
+    return round(((1-cos(np.radians(a)))*exp(-TR/T1))/(1-(cos(np.radians(a))*exp(-TR/T1))), 5)
 
 fs = getlocaldataroot()
 projectdir = join(fs, 'phantom_qT1_slu')
@@ -13,15 +22,41 @@ anatdir = join(projectdir, subject, 'anat')
 anglefiles = sorted(glob.glob(join(anatdir,'*14*1.nii')))
 X = [7,10,15,20,30]
 
+### coregistration
+#coregAngleFiles = coregisterPhantoms(anglefiles, projectdir=projectdir)
+
+
+## atlassing
+atlasfname = 'phantom_slu_mask_20160113.nii.gz'
+atlasfpath = join(projectdir,atlasfname)
+labels = atlaslabels(atlasfname)
+indexOf15 = labels.index('15')
+
+YRegionalData = []
+for fpath in anglefiles:
+    YRegionalData.append(statsByRegion(fpath, atlasfpath))
+
+Y = [stats['average'][indexOf15] for stats in YRegionalData]
+
+X = numpy.radians(X)
+Y = numpy.array(Y)
+
 
 ## fitting
+TR = 14.0
 ai = 15*Y.max()  # S0
 bi = 1000           # T1
 p0=[ai, bi]
 spgrformula.TR = TR
 formula = spgrformula
-poi = 1
 
-popt, pcov = curve_fit(formula, Xv, Y, p0=p0)
+popt, pcov = curve_fit(formula, X, Y, p0=p0)
+
+fsat = frac_sat(popt[0], TR, popt[1])
+
+
+
+
+
 
 
