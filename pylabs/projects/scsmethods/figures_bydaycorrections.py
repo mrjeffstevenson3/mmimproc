619b6c04125ffdaa854b2e8e44e92a1a6de74540
Jasper van den Bosch <jasperb@uw.edu>
2016-04-25 10:13:50 -0700

    scs figure data pandas approach

--- pylabs/projects/scsmethods/figures_bydaycorrections.py
+++ pylabs/projects/scsmethods/figures_bydaycorrections.py
@@ -7,10 +7,12 @@ from niprov import Context as ProvenanceContext
 provenance = ProvenanceContext()
 from pylabs.correlation.atlas import atlaslabels
 from pylabs.qt1.vials import vialNumbersByAscendingT1
-usedVials = range(7, 18+1)
-vialOrderT1 = [str(v) for v in vialNumbersByAscendingT1 if v in usedVials]
+#usedVials = range(7, 18+1)
+#vialOrderT1 = [str(v) for v in vialNumbersByAscendingT1 if v in usedVials]
 atlasfname = 'phantom_slu_mask_20160113.nii.gz'
 vials = atlaslabels(atlasfname)
+del vials[vials.index('0')]
+del vials[vials.index('18')]
 
 rootdir = join(getlocaldataroot(),'self_control','hbm_group_data','qT1')
 method = ('orig_spgr_mag', 14.0, True)
@@ -22,19 +24,19 @@ datafilepath = 'data/t1_factordata_{0}.pickle'.format(coregtag)
 with open(datafilepath) as datafile:
     allmethods = pickle.load(datafile)
 methoddata = allmethods[method]
-dates = meth
-vialAverage = pandas.DataFrame(index=data['dates'])
+dates = methoddata['dates']
 data = {}
 for item in ['model', 'observed']:
-    data[item] = pandas.DataFrame(columns=vials, index=dates)
+    itemdata = numpy.array(methoddata[item])
+    data[item] = pandas.DataFrame(itemdata, columns=vials, index=dates)
+data = pandas.Panel(data)
+data['diff'] = data['observed']-data['model']
+data['%'] = data['diff']/data['model']*100
 
 
-
-    vialAverage[col] = [numpy.array(d).mean() for d in data[col]]
-vialAverage['diff'] = vialAverage['observed']-vialAverage['model']
-vialAverage['%'] = vialAverage['diff']/vialAverage['model']*100
 
 ## Table 1: Vial average export
+vialAverage = data.mean(axis=2)
 content = vialAverage.to_latex()
 header = "\documentclass[12pt]{article}\n\\usepackage{booktabs}\n\\begin{document}\n"
 footer = "\end{document}"
@@ -44,25 +46,25 @@ with open('table1.tex','w') as tablefile:
 pdf = latex.build_pdf(tabledoc)
 pdf.save_to('table1.pdf')
 
-factor = CorrectionFactor(method, coreg=True)
-factor.byNearestDate()
+#factor = CorrectionFactor(method, coreg=True)
+#factor.byNearestDate()
 
 
-## determine date range
-subjects = {}
-for subjectdir in glob.glob(join(rootdir, 'scs*')):
-    subject = os.path.basename(subjectdir)
-    parrecfile = glob.glob(join(subjectdir, 'source_parrec', '*.PAR'))[0]
-    (img, status) = provenance.add(parrecfile)
-    acqdate = img.provenance['acquired'].date()
-    subjects[subject] = acqdate
+### determine date range
+#subjects = {}
+#for subjectdir in glob.glob(join(rootdir, 'scs*')):
+#    subject = os.path.basename(subjectdir)
+#    parrecfile = glob.glob(join(subjectdir, 'source_parrec', '*.PAR'))[0]
+#    (img, status) = provenance.add(parrecfile)
+#    acqdate = img.provenance['acquired'].date()
+#    subjects[subject] = acqdate
 
-for subject in subjects.keys():
-    acqdate = subjects[subject]
-    print('Subject {0} data acquired {1}.'.format(subject, acqdate))
-    subjectdir = join(rootdir, subject)
-    rationalefilepath = outfile.replace('.nii.gz','.txt')
-    print('Applying correction factor to: '+targetfname)
-    x, rationale = factor.forDate(acqdate)
+#for subject in subjects.keys():
+#    acqdate = subjects[subject]
+#    print('Subject {0} data acquired {1}.'.format(subject, acqdate))
+#    subjectdir = join(rootdir, subject)
+#    rationalefilepath = outfile.replace('.nii.gz','.txt')
+#    print('Applying correction factor to: '+targetfname)
+#    x, rationale = factor.forDate(acqdate)
 
 
