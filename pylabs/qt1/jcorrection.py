8d5f8351792f4b832eff57d5271562f9395b9c9d
Jasper van den Bosch <jasperb@uw.edu>
2016-04-27 14:24:38 -0700

    Saturation; basic fit on brain 1 voxel

--- pylabs/qt1/jcorrection.py
+++ pylabs/qt1/jcorrection.py
@@ -1,19 +1,42 @@
 # J-correction for brains
 from __future__ import division
-import datetime
-from pylabs.qt1.formulas import spgrWithJ
+import datetime, glob, nibabel, numpy, scipy.optimize
+from numpy import radians
+from os.path import join
+from pylabs.utils.paths import getlocaldataroot
+from pylabs.qt1.formulas import spgrWithJ, spgrformula
+from niprov import Context
+provenance = Context()
 
 fs = getlocaldataroot()
-projectdir = join(fs, 'phantom_qT1_slu')
-date = datetime.date(2015, 6, 8)
-subject = 'sub-phant'+str(date)
+projectdir = join(fs, 'scsmethods')
+subject = 'sub-scs376'
 subjectdir = join(projectdir, subject)
+#'source_parrec'
 
-alphafiles = sorted(glob.glob(join(subjectdir,'anat',alphafilter)))
-b1file = join(subjectdir, 'fmap', '{}_b1map_phase_1.nii'.format(subject))
+alphafiles = sorted(glob.glob(join(subjectdir,'anat','*_fa_*brain.nii.gz')))
+b1filter = '{}_b1map_phase_rfov_reg2qT1.nii.gz'
+b1file = join(subjectdir, 'fmap', b1filter.format(subject))
 
 # ## Need j, TR, TE, T2s
-j = 9
+#provenance.get(forFile=alphafiles[0]).provenance['repetition-time']
+alphas = [2.,10.,20.]
+TR = 14. 
 T2s = 50
-TE = 4.6 ?
+TE = 4.6 #?
+j = 9
 # spgrWithJ(a, S0, T1)
+
+Sa = numpy.array([nibabel.load(f).get_data() for f in alphafiles])
+B1 = nibabel.load(b1file).get_data()
+assert B1.shape == Sa.shape[1:]
+Sa = Sa[:,100,100,100]
+B1 = B1[100,100,100]
+
+A = radians(alphas)
+T1i = 1000
+S0i = 15*Sa.max()
+Ab1 = A*(B1/100)
+spgrformula.TR = TR
+popt, pcov = scipy.optimize.curve_fit(spgrformula, Ab1, Sa, p0=[S0i, T1i])
+print(popt[1])
