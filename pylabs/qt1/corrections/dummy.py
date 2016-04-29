698a1169cbd2a06e2f1ae35ef2b724b944e87945
Jasper van den Bosch <jasperb@uw.edu>
2016-04-28 17:29:30 -0700

    Dummy correction better docs

--- pylabs/qt1/corrections/dummy.py
+++ pylabs/qt1/corrections/dummy.py
@@ -1,3 +1,13 @@
+from pylabs.qt1.simplefitting import fitT1
+
 
 def correct(S, A, B1, expected, fit, TR):
-    return fit
+    """Dummy implementation of a correction method
+    S           nvials * nflipangles    signal values per flip angle
+    A           nflipangles             flip angles in radians
+    B1          nvials                  B1 factor per vial
+    expected    nvials                  model T1 per vial
+    fit         nvials                  original T1 fit value
+    TR          one value               e.g. 14.
+    """
+    return fitT1(S, A, B1, TR)
