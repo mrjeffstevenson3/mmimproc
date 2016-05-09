bfcc3a6bf885f9f257e7c4b838c56c381e5e03e1
Jasper van den Bosch <jasperb@uw.edu>
2016-05-09 13:14:24 -0700

    Changed criteria for behavioral variable selection

--- pylabs/projects/roots/behavior.py
+++ pylabs/projects/roots/behavior.py
@@ -5,11 +5,41 @@ basics = data[0:25]
 allvars = data[25:].astype(float)
 pre = allvars[allvars.index.str.contains('PRE-')]
 post = allvars[allvars.index.str.contains('POST-')]
-selvarnames = ['R-PRE-little-helpers-task', 'R-PRE-SE-IAT-Score',
-               'T-PRE-SDQ-Hyperactivity-Scale-Score',
-               'T-PRE-Emotional-Regulation-Checklist-Lability-Negativity',
-               'T-PRE-Emotional-Regulation-Checklist-Emotion-Regulation',
-               'P-PRE-Child-Behavior-Questionnaire-Surgency',
-               'P-PRE-Child-Behavior-Checklist-Total-T-score']
-selectedvars = pre.loc[selvarnames].T
+
+# Select variables that represent a total of a set of items
+varnamesOfTotals = ['P-POST-Child-Behavior-Questionnaire-Surgency',
+       'P-POST-Child-Behavior-Questionnaire-Negative-Affect',
+       'P-POST-Child-Behavior-Questionnaire-Effortful-Control',
+       'P-POST-Child-Behavior-Checklist-Internal-T-score',
+       'P-POST-Child-Behavior-Checklist-External-T-score',
+       'P-POST-Child-Behavior-Checklist-Total-T-score',
+       'R-POST-Mean-Questions-1-6',
+       'R-POST-Affect-Knowledge-Test-Sum-of-8-Vignettes',
+       'R-POST-N-stickers-given-to-other-child',
+       'R-POST-little-helpers-task', 'R-POST-IAT-ID',
+       'R-POST-SE-IAT-Score', 'R-POST-Flag-Switch',
+       'R-POST-Peabody-Picture-Vocab-Test-Standard-Score',
+       'T-POST-SDQ-Emotion-Symptoms-Scale-Score',
+       'T-POST-SDQ-Conduct-Problems-Scale-Score',
+       'T-POST-SDQ-Hyperactivity-Scale-Score',
+       'T-POST-SDQ-Peer-problems-Scale-Score',
+       'T-POST-SDQ-Prosocial-Scale-Score',
+       'T-POST-SDQ-Total-Difficulties-Score',
+       'T-POST-Child-Behavior-Scale-Aggression-Scale',
+       'T-POST-Child-Behavior-Scale-Prosocial-Scale',
+       'T-POST-Emotional-Regulation-Checklist-Lability-Negativity',
+       'T-POST-Emotional-Regulation-Checklist-Emotion-Regulation']
+posttotalvars = post.loc[posttotalvarnames]
+
+# Get rid of variables with nans
+varmaskWithoutNans = posttotalvars.notnull().all(axis=1)
+postWithoutNans = posttotalvars[varmaskWithoutNans]
+
+selectedvars = postWithoutNans
+
+# pandas.set_option('display.large_repr', 'truncate')
+# pandas.set_option('display.max_columns', 0)
+
+
+
 
