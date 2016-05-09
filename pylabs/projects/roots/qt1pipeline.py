17d32863d6dd70f4527f6314f6d5a41f5d09f69e
Jasper van den Bosch <jasperb@uw.edu>
2016-05-09 10:58:57 -0700

    Clipping T1 fit to 3000.

--- pylabs/projects/roots/qt1pipeline.py
+++ pylabs/projects/roots/qt1pipeline.py
@@ -16,23 +16,24 @@ nsubjects = len(subjects)
 ## behavior
 from pylabs.projects.roots.behavior import selectedvars
 
-## fit T1
-#files = []
-#for s, subject in enumerate(subjects):
-#    print('Converting parrecs for {} of {}: {}'.format(s, nsubjects, subject))
-#    parrecdir = join(subjectdirs[s], 'source_parrec')
-#    parsfiles = glob.glob(join(parrecdir, '*T1_MAP*.PAR'))
-#    sfiles = [conv(p) for p in parsfiles]
-#    parb1file = glob.glob(join(parrecdir, '*B1MAP*.PAR'))[0]
-#    b1fileLowRes = conv(parb1file)
-#    b1file = b1mapcoreg_1file(b1fileLowRes, sfiles[0])
-#    outfpath = join(subjectdirs[s], 'qT1', 't1_{}.nii.gz'.format(subject))
-#    print('T1 fitting subject {} of {}: {}'.format(s, nsubjects, subject))
-#    fitT1WholeBrain(sfiles, b1file, outfpath)
-#    files.append(outfpath)
+# fit T1
+files = []
+for s, subject in enumerate(subjects):
+    print('Converting parrecs for {} of {}: {}'.format(s, nsubjects, subject))
+    parrecdir = join(subjectdirs[s], 'source_parrec')
+    parsfiles = glob.glob(join(parrecdir, '*T1_MAP*.PAR'))
+    sfiles = [conv(p) for p in parsfiles]
+    parb1file = glob.glob(join(parrecdir, '*B1MAP*.PAR'))[0]
+    b1fileLowRes = conv(parb1file)
+    b1file = b1mapcoreg_1file(b1fileLowRes, sfiles[0])
+    outfpath = join(subjectdirs[s], 'qT1', 't1_{}.nii.gz'.format(subject))
+    print('T1 fitting subject {} of {}: {}'.format(s, nsubjects, subject))
+    fitT1WholeBrain(sfiles, b1file, outfpath)
+    files.append(outfpath)
 
 
 ## alignment?
+# reg2t1flip_1vol.mat only two subj
 
 
 ## correlation
