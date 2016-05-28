53112e8e2cbc77bab6cb2b9c147630f87492843f
Jasper <japsai@gmail.com>
2016-05-28 10:53:10 -0700

    Stats table by atlas region

---
+++ tests/atlastabletest.py
@@ -0,0 +1,20 @@
+
+
+# atkas + stats > frame > table
+
+statfiles = {
+    'Affect-Knowledge-Test':{
+        '2minp':'R-POST-Affect-Knowledge-Test-Sum-of-8-Vignettes_2minp.nii.gz',
+        'r': 'R-POST-Affect-Knowledge-Test-Sum-of-8-Vignettes_r.nii.gz',
+        'tneg':'R-POST-Affect-Knowledge-Test-Sum-of-8-Vignettes_tneg.nii.gz',
+        'tpos':'R-POST-Affect-Knowledge-Test-Sum-of-8-Vignettes_tpos.nii.gz',
+    },
+    'Flag-Switch':{
+        '2minp':'R-POST-Flag-Switch_2minp.nii.gz',
+        'r': 'R-POST-Flag-Switch_r.nii.gz',
+        'tneg':'R-POST-Flag-Switch_tneg.nii.gz',
+        'tpos':'R-POST-Flag-Switch_tpos.nii.gz',
+    }
+}
+
+#pylabs.tables.atlasframe()
