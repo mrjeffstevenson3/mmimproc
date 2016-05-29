3875bfcedd0610161dea1652243a05061de93be3
Jasper <japsai@gmail.com>
2016-05-28 18:02:38 -0700

    Atlassing; multiindex.

--- pylabs/atlassing.py
+++ pylabs/atlassing.py
@@ -3,7 +3,12 @@ from pylabs.regional import statsByRegion
 
 def atlasWiseSignificantVoxelsFrame(statfiles, atlas, pmax=.001):
     thresh2minp = 2-pmax
-    for var in statfiles.keys():
+    varnames = statfiles.keys()
+    columns = pandas.MultiIndex.from_product(
+        [varnames, ('pos', 'neg'), ('r','k')], 
+        names=['variable', 'direction', 'stat'])
+    data = pandas.DataFrame(columns = columns)
+    for var in varnames:
         print('Atlassing stats for '+var)
         region2minp = statsByRegion(statfiles[var]['2minp'], atlas, 
                                     threshold=thresh2minp)
@@ -12,20 +17,17 @@ def atlasWiseSignificantVoxelsFrame(statfiles, atlas, pmax=.001):
 
         voxels = pandas.DataFrame(index=region2minp.index)
         voxels['significant'] = region2minp['superthreshold']
-        voxels['pos'] = regiontpos['all'].apply(lambda r: r > 0)
-        voxels['neg'] = data['pos'].apply(lambda r: r == False)
-        voxels['sigpos'] = voxels['significant'] * voxels['pos']
-        voxels['signeg'] = voxels['significant'] * voxels['neg']
+        voxels['allpos'] = regiontpos['all'].apply(lambda r: r > 0)
+        voxels['allneg'] = voxels['allpos'].apply(lambda r: r == False)
+        voxels['pos'] = voxels['significant'] * voxels['allpos']
+        voxels['neg'] = voxels['significant'] * voxels['allneg']
         voxels['r'] = regioncorr['all']
         voxels['p'] = 2-region2minp['all']
 
-        k = lambda row: row['sigpos'].sum()
-        r = lambda row: row['r'][row['sigpos']].mean()
-        data = pandas.DataFrame(index=region2minp.index)
-        data['k'] = voxels.apply(k, axis=1)
-        data['r'] = voxels.apply(r, axis=1)
-        #for directionLabel, d in {'pos': +1, 'neg': -1}.items():
-        #    data[directionLabel] = 
-
+        for direction in ('pos', 'neg'):
+            k = lambda row: row[direction].sum()
+            r = lambda row: row['r'][row[direction]].mean()
+            data[var, direction, 'k'] = voxels.apply(k, axis=1)
+            data[var, direction, 'r'] = voxels.apply(r, axis=1)
 
     return data
