a7475adc0eea2167b740edf0e241d251e10d2e67
Jasper van den Bosch <jasperb@uw.edu>
2016-06-01 16:34:03 -0700

    Slowly adapting phantom porocessing code

--- pylabs/projects/roots/phantoms.py
+++ pylabs/projects/roots/phantoms.py
@@ -2,9 +2,9 @@ from __future__ import division
 import collections, numpy, glob, datetime, pandas, itertools
 from numpy import cos, sin, exp, tan, radians, power
 import matplotlib.pyplot as plt
-from os.path import join, isfile
+from os.path import join, isfile, basename
 from niprov import Context
-from pylabs.utils.paths import getlocaldataroot
+from pylabs.utils.paths import getnetworkdataroot
 from pylabs.conversion.helpers import convertSubjectParfiles
 from pylabs.qt1.fitting import spgrformula
 from pylabs.qt1.model_pipeline import modelForDate
@@ -15,110 +15,106 @@ from pylabs.alignment.phantom import align, applyXformAndSave
 from pylabs.qt1.simplefitting import fitT1
 from pylabs.stats import ScaledPolyfit
 provenance = Context()
+provget = lambda  f: provenance.get(forFile=f).provenance
+singletr = lambda tr: tr[0] if isinstance(tr, list) else tr
 
 ## settings
-fs = getlocaldataroot()
-projectdir = join(fs, 'phantom_qT1_slu')
-subjectsByTR = {14.0:datetime.date(2016, 3, 2),
-                28.0:datetime.date(2016, 3, 2), 
-                56.0:datetime.date(2016, 3, 11)}
+fs = getnetworkdataroot()
+projectdir = join(fs, 'phantom_qT1_disc')
 alignmentTarget = join(projectdir, 'phantom_flipangle_alignment_target.nii')
 vialAtlas = join(projectdir,'phantom_alignment_target_round_mask.nii.gz')
 usedVials = range(7, 18+1)
 vialOrder = [str(v) for v in vialNumbersByAscendingT1 if v in usedVials]
+scanner = 'disc'
 
 import pylabs.qt1.corrections.polycurvet1 as correct
 correctionname = correct.__name__.split('.')[-1]
 
-## Get brain sample:
-from pylabs.qt1.brainsampling import sample
-
-TRs = subjectsByTR.keys()
 xform = {}
-xform = {14.0: {'rxy': 0, 'tx': 0, 'ty': 0}, 
-         28.0: {'rxy': -3, 'tx': 1, 'ty': 0}}
 adata = {}
 correction = {}
 phantom = {}
 brain = {}
-for TR in TRs:
-    phantom[TR] = pandas.DataFrame(index=vialOrder, 
-                    columns=['model','fit','corr'], dtype=float)
 
-    if TR ==56:
-        continue
-    date = subjectsByTR[TR]
-    subject = 'sub-phant'+str(date)
-    subjectdir = join(projectdir, subject)
+TR = 11.
+subjectdirs = glob.glob(join(projectdir, 'phantom_qT1_*'))
+for subjectdir in subjectdirs:
+    subject = basename(subjectdir)
+    date = datetime.datetime.strptime(subject.split('_')[2], '%Y%m%d').date()
+    phantom[date] = pandas.DataFrame(index=vialOrder, 
+                    columns=['model','fit'], dtype=float)
 
     ## par2nii
-    #niftiDict = convertSubjectParfiles(subject, subjectdir)
-
-    ## model_pipeline
-    phantom[TR]['model'] = modelForDate(date, 'slu')[vialOrder]
+    niftiDict = convertSubjectParfiles(subject, subjectdir)
 
     ## Gather files & align
-    alphafilter = '*{}*1.nii'.format(int(TR))
+    alphafilter = '*tr_{}*1.nii'.format(int(TR))
     alphafiles = sorted(glob.glob(join(subjectdir,'anat',alphafilter)))
-    assert TR == provenance.get(forFile=alphafiles[0]).provenance['repetition-time']
+    if not alphafiles:
+        print('\n\n\nNO SPGRS TR11 FOUND FOR {}\n\n\n'.format(subject))
+        continue
+    assert TR == singletr(provget(alphafiles[0])['repetition-time'])
     if not xform:
-        xform[TR] = align(alphafiles[0], alignmentTarget, delta=10)
+        xform[date] = align(alphafiles[0], alignmentTarget, delta=10)
+
+    ## model_pipeline
+    phantom[date]['model'] = modelForDate(date, scanner)[vialOrder]
 
     ## b1 map
     b1file = join(subjectdir, 'fmap', '{}_b1map_phase_1.nii'.format(subject))
     alignedB1file = b1file.replace('.nii', '_coreg.nii')
     if not xform:
-        applyXformAndSave(xform[TR], b1file, alignmentTarget, 
+        applyXformAndSave(xform[date], b1file, alignmentTarget, 
             newfile=alignedB1file, provenance=provenance)
     print('Sampling B1')
     B1 = averageByRegion(alignedB1file, vialAtlas).loc[vialOrder]
 
     ## transform and sample flip-angle files
-    adata[TR] = pandas.DataFrame()
+    adata[date] = pandas.DataFrame()
     for alphafile in alphafiles:
         alignedAlphafile = alphafile.replace('.nii', '_coreg.nii')
         if not xform:
-            applyXformAndSave(xform[TR], alphafile, alignmentTarget, 
+            applyXformAndSave(xform[date], alphafile, alignmentTarget, 
                 newfile=alignedAlphafile, provenance=provenance)
         print('Sampling signal for one flip angle..')
         vialAverages = averageByRegion(alignedAlphafile, vialAtlas)
         alpha = provenance.get(forFile=alignedAlphafile).provenance['flip-angle']
-        adata[TR][alpha] = vialAverages
-    adata[TR] = adata[TR].loc[vialOrder]
-    alphas = adata[TR].columns.values.tolist()
+        adata[date][alpha] = vialAverages
+    adata[date] = adata[date].loc[vialOrder]
+    alphas = adata[date].columns.values.tolist()
 
     A = radians(alphas)
 
     ## Initial fit on vials
-    phantom[TR]['fit'] = fitT1(adata[TR], A, B1, TR)
+    phantom[date]['fit'] = fitT1(adata[date], A, B1, TR)
     ## determine Correction
-    correction[TR] = correct.create(adata[TR], A, B1, phantom[TR]['model'], 
-                                    phantom[TR]['fit'], TR)
+    correction[date] = correct.create(adata[date], A, B1, phantom[date]['model'], 
+                                    phantom[date]['fit'], TR)
 
     try:
-        if isinstance(correction[TR][0], str):
-            correctionname = correction[TR][0]
+        if isinstance(correction[date][0], str):
+            correctionname = correction[date][0]
     except Exception as e:
         pass
     ## apply Correction fit on vials
-    phantom[TR]['corr'] = correct.apply(correction[TR], adata[TR], A, B1)
+    phantom[date]['corr'] = correct.apply(correction[date], adata[date], A, B1)
     ## apply Correction fit on brain sample
-    brain[TR] = pandas.DataFrame(index=sample.index, 
+    brain[date] = pandas.DataFrame(index=sample.index, 
                     columns=['fit','corr'], dtype=float)
     brainAngles = list(sample.columns.values[-3:])
-    brain[TR]['fit'] = sample['fit']
-    brain[TR]['corr'] = correct.apply(correction[TR], sample[brainAngles], 
+    brain[date]['fit'] = sample['fit']
+    brain[date]['corr'] = correct.apply(correction[date], sample[brainAngles], 
                                         radians(brainAngles), sample['B1'])
 
     ## plotting
     plt.figure()
     pltname = 'phantom_TR{}_{}'.format(TR, correctionname)
-    phantom[TR].plot.bar()
+    phantom[date].plot.bar()
     plt.title(pltname)
     plt.savefig(pltname+'.png')
     plt.figure()
     pltname = 'brain_TR{}_{}'.format(TR, correctionname)
-    brain[TR].plot.bar()
+    brain[date].plot.bar()
     plt.title(pltname)
     plt.savefig(pltname+'.png')
 
