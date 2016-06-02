65bc4affdd06f705efe73b8f820e848705f700ee
Jasper van den Bosch <jasperb@uw.edu>
2016-06-02 09:13:03 -0700

    Using the sampled phantom data for the phantom correction pipeline.

--- pylabs/io/mixed.py
+++ pylabs/io/mixed.py
@@ -1,10 +1,12 @@
-import json, pandas
+import json, pandas, datetime, dateutil.parser
 
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if hasattr(obj, 'to_json'):
             return obj.to_json(orient='records')
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
         return json.JSONEncoder.default(self, obj)
 
 
@@ -13,10 +15,11 @@ def listOfDictsToJson(data, fpath):
         json.dump(data, fp, cls=JSONEncoder)
 
 def listOfDictsFromJson(fpath):
-    toplevel = json.load(open(fpath)
+    toplevel = json.load(open(fpath))
     assert isinstance(toplevel, list)
     outlist = []
     for item in toplevel:
         item['data'] = pandas.read_json(item['data'])
+        item['date'] = dateutil.parser.parse(item['date']).date()
         outlist.append(item)
     return outlist
--- pylabs/qt1/phantoms.py
+++ pylabs/qt1/phantoms.py
@@ -2,7 +2,7 @@ from __future__ import division
 import collections, numpy, glob, datetime, pandas, itertools
 from os.path import join, isfile, basename
 from niprov import Context
-from pylabs.io.mixed import mixedDataToJson
+from pylabs.io.mixed import listOfDictsToJson
 from pylabs.utils.paths import getnetworkdataroot
 from pylabs.conversion.helpers import convertSubjectParfiles
 from pylabs.qt1.vials import vialNumbersByAscendingT1
@@ -11,6 +11,15 @@ from pylabs.alignment.phantom import align, applyXformAndSave
 provenance = Context()
 provget = lambda  f: provenance.get(forFile=f).provenance
 singletr = lambda tr: tr[0] if isinstance(tr, list) else tr
+
+"""
+This script loops through all phantom session directories and samples
+the phantom scans and the corresponding B1 field maps using 
+pylabs.regional.averageByRegion. The sampled data is then saved in a json file
+in the phantom project directory. This is a separate script since it can take
+a long time to convert and sample all these files, whereas the next steps
+in the pipeline (fitting e.g.) change frequently but are faster.
+"""
 
 ## settings
 fs = getnetworkdataroot()
@@ -26,32 +35,33 @@ xform = {}
 phantoms = []
 
 subjectdirs = glob.glob(join(projectdir, 'phantom_qT1_*'))
-for subjectdir in subjectdirs:
+for s, subjectdir in enumerate(subjectdirs):
+    print('Sampling phantom {} of {}'.format(s+1, len(subjectdirs)))
     phantom = {}
     subject = basename(subjectdir)
     datestr = subject.split('_')[2]
     phantom['date'] = datetime.datetime.strptime(datestr, '%Y%m%d').date()
 
     ## par2nii
-    niftiDict = convertSubjectParfiles(subject, subjectdir)
+    #niftiDict = convertSubjectParfiles(subject, subjectdir)
 
     ## Gather files & align
-    alphafilter = '*tr_{}*1.nii'.format(int(TR))
+    phantom['method'] = 'spgr'
+    alphafilter = '*tr_{}*1.nii'.format(int(11))
     alphafiles = sorted(glob.glob(join(subjectdir,'anat',alphafilter)))
     if not alphafiles:
-        print('\n\n\nNO SPGRS TR11 FOUND FOR {}\n\n\n'.format(subject))
         continue
     phantom['TR'] = singletr(provget(alphafiles[0])['repetition-time'])
 
-    if date not in xform:
-        phantom['xform'] = align(alphafiles[0], alignmentTarget, delta=10)
+    #if date not in xform:
+    phantom['xform'] = align(alphafiles[0], alignmentTarget, delta=10)
 
-    data = pandas.DataFrame()
+    data = pandas.DataFrame(index=vialOrder)
 
     ## b1 map
     b1file = join(subjectdir, 'fmap', '{}_b1map_phase_1.nii'.format(subject))
     alignedB1file = b1file.replace('.nii', '_coreg.nii')
-    applyXformAndSave(xform[date], b1file, alignmentTarget, 
+    applyXformAndSave(phantom['xform'], b1file, alignmentTarget, 
         newfile=alignedB1file, provenance=provenance)
     print('Sampling B1')
     data['b1'] = averageByRegion(alignedB1file, vialAtlas).loc[vialOrder]
@@ -60,7 +70,7 @@ for subjectdir in subjectdirs:
     
     for alphafile in alphafiles:
         alignedAlphafile = alphafile.replace('.nii', '_coreg.nii')
-        applyXformAndSave(xform[date], alphafile, alignmentTarget, 
+        applyXformAndSave(phantom['xform'], alphafile, alignmentTarget, 
             newfile=alignedAlphafile, provenance=provenance)
         print('Sampling signal for one flip angle..')
         vialAverages = averageByRegion(alignedAlphafile, vialAtlas)
