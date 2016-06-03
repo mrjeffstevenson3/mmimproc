05923760617d1af8d81c397ea48e9ff31b15cbbe
Jeff Stevenson <mrjeffs@uw.edu>
2016-06-03 13:29:12 -0700

    1st whole brain FA randomise run

--- pylabs/projects/roots/tbss_correl.py
+++ pylabs/projects/roots/tbss_correl.py
@@ -13,25 +13,26 @@ prov = niprov.Context()
 prov.dryrun = True
 prov.verbose = True
 
-#fs = getnetworkdataroot()
+fs = getnetworkdataroot()
 project = 'roots_of_empathy'
 subjects = [28, 29, 30, 37, 53, 65]
-fs = getlocaldataroot()
-statsdir = join(fs, project, 'mytbss_mf_v1', 'stats')
+#fs = getlocaldataroot()
+statsdir = join(fs, project, 'ants_diff_correlations', 'stats')
 behavdir = 'data/behavior/'
 csvfile = behavdir+'roots_behavior_transposed.csv'
 niprov.add(csvfile)
-maskfile = join(statsdir, 'mean_FA_skeleton_mask.nii.gz')
+maskfile = join(statsdir, 'all_FA_qformfix_reg2templ_mask.nii.gz')
 
-imgtemplate = 'all_{0}_skeletonised.nii.gz'
+#imgtemplate = 'all_{0}_skeletonised.nii.gz'
+imgtemplate = 'all_{0}_qformfix_reg2templ.nii.gz'
 #measures = ['F1', 'F2', 'FA', 'L1', 'MD', 'MO', 'RA', 'AD', 'L2', 'L3']
-#measures = ['FA']
-measures = ['F2']
+measures = ['FA']
+#measures = ['F2']
 skellist = [imgtemplate.format(m) for m in measures]
 images = [statsdir+'/'+i for i in skellist]
 [niprov.add(img) for img in images]
 
-exptag='testrun_filter_gender_n500'
+exptag='whole_brain_filter_gender_n500'
 matfiledir = join(statsdir,'matfiles','matfiles_'+exptag)
 resultdir = join(statsdir,'randpar',exptag)
 qsubdir = join(resultdir, 'qsubdir_defunctcommands')
@@ -51,24 +52,24 @@ masks = {img: maskfile for img in images} # Mask is the same for all images
 combs = {img:matfiles for img in images}
 randparfiles = multirandpar(combs, designfile, masks=masks, niterations=500,
      tbss=True, workdir=qsubdir, outdir=resultdir)
-
-corrPfiles = [f+'_tfce_corrp_tstat1.nii.gz' for f in randparfiles]
-corrPfiles += [f+'_tfce_corrp_tstat2.nii.gz' for f in randparfiles]
-waitForFiles(corrPfiles, interval=5) # every 5 seconds check if files done.
-
-selectedCorrPfiles = select(corrPfiles, withVoxelsOverThresholdOf(.95))
-
-atlasfile = 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz'
-atlas = join('data','atlases',atlasfile)
-report(selectedCorrPfiles, atlas, atlaslabels(atlasfile),
-    relevantImageFilenameSegment=-4)
-
-combs = deconstructRandparFiles(selectedCorrPfiles, matdir=matfiledir, imgdir=statsdir)
-
-prov.dryrun = True
-exptag='testrun_filtered_gender_2col_n5000_select'
-resultdir = join(statsdir,'randomise_runs',exptag)
-masks = {img: maskfile for img in combs.keys()} # Mask is the same for all images
-randparfiles = multirandpar(combs, designfile, masks=masks, niterations=5000,
-    tbss=True, workdir=qsubdir, outdir=resultdir)
-
+#
+# corrPfiles = [f+'_tfce_corrp_tstat1.nii.gz' for f in randparfiles]
+# corrPfiles += [f+'_tfce_corrp_tstat2.nii.gz' for f in randparfiles]
+# waitForFiles(corrPfiles, interval=5) # every 5 seconds check if files done.
+#
+# selectedCorrPfiles = select(corrPfiles, withVoxelsOverThresholdOf(.95))
+#
+# atlasfile = 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz'
+# atlas = join('data','atlases',atlasfile)
+# report(selectedCorrPfiles, atlas, atlaslabels(atlasfile),
+#     relevantImageFilenameSegment=-4)
+#
+# combs = deconstructRandparFiles(selectedCorrPfiles, matdir=matfiledir, imgdir=statsdir)
+#
+# prov.dryrun = True
+# exptag='testrun_filtered_gender_2col_n5000_select'
+# resultdir = join(statsdir,'randomise_runs',exptag)
+# masks = {img: maskfile for img in combs.keys()} # Mask is the same for all images
+# randparfiles = multirandpar(combs, designfile, masks=masks, niterations=5000,
+#     tbss=True, workdir=qsubdir, outdir=resultdir)
+#
