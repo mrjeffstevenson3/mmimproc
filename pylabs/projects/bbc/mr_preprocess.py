d5e7e3585a2fd2577e5472f73704eef4fbece9ce
Jeff Stevenson <mrjeffs@uw.edu>
2016-07-14 14:02:16 -0700

    setting up bbc project folder + preproc pipeline

---
+++ pylabs/projects/bbc/mr_preprocess.py
@@ -0,0 +1,41 @@
+import os, niprov, cPickle
+from os.path import join
+from collections import defaultdict
+from nipype.interfaces import fsl
+import subprocess
+from datetime import datetime
+from cloud.serialization.cloudpickle import dumps
+from pylabs.structural.brain_extraction import struc_bet
+from pylabs.conversion.brain_convert import conv_subjs
+from pylabs.utils.paths import getnetworkdataroot, getlocaldataroot
+prov = niprov.ProvenanceContext()
+flt = fsl.FLIRT(bins=640, interp='nearestneighbour', cost_func='mutualinfo', output_type='NIFTI')
+applyxfm = fsl.ApplyXfm(output_type='NIFTI')
+#fs = getnetworkdataroot()
+fs = getlocaldataroot()
+project = 'bbc'
+subtemplate = 'sub-bbc{sid}'
+subjid = [101, 105, 106, 108, 109, 113, 116, 118, 119, 120, 202, 208, 209, 211, 212, 215, 218, 219, 231, 236, 241, 243, 249, 252, 253]
+#subjects = [subtemplate.format(sid=str(s)) for s in subjid]
+subjects = ['sub-bbc105']
+
+#stages to run
+convert = True
+b1corr = False
+bet = False
+prefilter = False
+templating = False
+
+def default_to_regular(d):
+    if isinstance(d, defaultdict):
+        d = {k: default_to_regular(v) for k, v in d.iteritems()}
+    return d
+
+#run conversion if needed
+if convert:
+    niftiDict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    niftiDict, niftiDF = conv_subjs(project, subjects, niftiDict)
+
+else:
+    with open(join(fs, project, 'niftiDict_all_subj_201605232241.pickle'), 'rb') as f:
+        niftiDict = cPickle.load(f)
