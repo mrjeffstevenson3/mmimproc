d67ce383eeac5e58ebef30a84267df3a89c2038e
Jeff Stevenson <mrjeffs@uw.edu>
2016-08-04 14:24:17 -0700

    really day 2

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -3,8 +3,8 @@ import subprocess
 shell = Shell()
 
 mpath = '/home/mrjeffs/Software/gannett_spectro/Gannet2/Gannet2.0-master'
-act = '/mnt/users/js/tadpole/JONA_DAY1/TADPOLE_PR20160803_WIP_GABAMM_TE80_120DYN_3_2_raw_act.SDAT'
-ref = '/mnt/users/js/tadpole/JONA_DAY1/TADPOLE_PR20160803_WIP_GABAMM_TE80_120DYN_3_2_raw_ref.SDAT'
+act = '/mnt/users/js/tadpole/JONAH_DAY2/source_parrec/TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_act.SPAR'
+ref = '/mnt/users/js/tadpole/JONAH_DAY2/source_parrec/TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_ref.SPAR'
 
 mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
         mpath, act, ref)
