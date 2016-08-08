86a5950aaf5a0b64e2b2b651c634dd36cf5421fb
Jeff Stevenson <mrjeffs@uw.edu>
2016-08-08 09:54:12 -0700

    mrs pipeline now uses new gannett with SDAT files processed into mrs folder

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -1,13 +1,30 @@
-from pylabs.utils import Shell, paths
-import subprocess
+from pylabs.utils import Shell, paths, InDir
+import os, subprocess
+from os.path import join
+from pylabs.utils.paths import getnetworkdataroot
+fs = getnetworkdataroot()
+project = 'tadpole'
+subject = 'JONAH_DAY2'
+actfname = 'TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_act.SDAT'
+reffname = 'TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_ref.SDAT'
+
 shell = Shell()
 
+try:
+    os.makedirs(join(fs, project, subject, 'mrs'))
+except OSError:
+    if not os.path.isdir(join(fs, project, subject, 'mrs')):
+        raise
+tempdir = InDir(join(fs, project, subject, 'mrs'))
+
 mpath = paths.getgannettpath()
-act = '/mnt/users/js/tadpole/JONAH_DAY2/source_parrec/TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_act.SPAR'
-ref = '/mnt/users/js/tadpole/JONAH_DAY2/source_parrec/TADPOLE_PR20160804_WIP_GABAMM_TE80_120DYN_3_2_raw_ref.SPAR'
+act = join(fs, project, subject, 'source_parrec', actfname)
+ref = join(fs, project, subject, 'source_parrec', reffname)
 
 mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
         mpath, act, ref)
 
 cmd = 'matlab -nodisplay -nosplash -nodesktop -r "{0}"'.format(mcode)
+tempdir.__enter__()
 subprocess.check_call(cmd, shell=True)
+os.chdir(tempdir._orig_dir)
