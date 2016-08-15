5cf5594a1970102f6fb520c24b1fe697ccce370d
Jeff Stevenson <mrjeffs@uw.edu>
2016-08-15 15:14:36 -0700

    made ffactor a variable

--- pylabs/projects/bbc/bbc_bet.sh
+++ pylabs/projects/bbc/bbc_bet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd /media/DiskArray/shared_data/js/bbc/myvbm/ants_vbm_template/orig_vbm
 list="sub-bbc116_ses-1_wempr_1 "`imglob sub*_mpr_?.nii`
+ffactor=0.45
 PYLABS='/home/toddr/Software/pylabs'
 imagelist="";
 for subject in ${list}
@@ -13,7 +14,7 @@ flirt -in ${PYLABS}/data/atlases/MNI152_T1_1mm-com-mask8k.nii.gz -ref ${subject}
 declare -a com=( $(fslstats ${subject}_mask8k.nii -l 5000 -C) )
 #fslmaths ${subject}.nii -roi 0 -1 0 -1 ${zcut[2]} -1 0 1 ${subject}_zcut2
 fslmaths ${subject}.nii -mas ${subject}_mask.nii ${subject}_zcut2
-bet ${subject}_zcut2 ${subject}_brain -c ${com[*]} -m -f 0.45
+bet ${subject}_zcut2 ${subject}_brain -c ${com[*]} -m -f $ffactor
 susan ${subject}_brain -1 1 3 1 0 ${subject}_brain_susan1
 fslmaths ${subject}_brain_susan1 -mas ${subject}_brain_mask ${subject}_brain_susan
 mean=`fslstats ${subject}_brain_susan -M`
