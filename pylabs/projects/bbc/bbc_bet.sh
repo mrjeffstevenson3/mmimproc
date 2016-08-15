fcc18ec77c041855d8132567bbd82647c056b5b6
Jeff Stevenson <mrjeffs@uw.edu>
2016-08-15 14:44:30 -0700

    normalised to 5000

--- pylabs/projects/bbc/bbc_bet.sh
+++ pylabs/projects/bbc/bbc_bet.sh
@@ -16,6 +16,8 @@ fslmaths ${subject}.nii -mas ${subject}_mask.nii ${subject}_zcut2
 bet ${subject}_zcut2 ${subject}_brain -c ${com[*]} -m -f 0.45
 susan ${subject}_brain -1 1 3 1 0 ${subject}_brain_susan1
 fslmaths ${subject}_brain_susan1 -mas ${subject}_brain_mask ${subject}_brain_susan
-imagelist="$imagelist $subject ${subject}_brain_susan";
+mean=`fslstats ${subject}_brain_susan -M`
+fslmaths ${subject}_brain_susan -div $mean -mul 5000 ${subject}_brain_susan_nl
+imagelist="$imagelist $subject ${subject}_brain_susan_nl";
 done
 slicesdir -o $imagelist
