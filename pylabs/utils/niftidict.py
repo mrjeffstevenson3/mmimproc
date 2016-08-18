fc49d8eb3388a6de78a2d010333b213c575b3c00
Jeff Stevenson <mrjeffs@uw.edu>
2016-08-18 15:01:39 -0700

    works better with 212 as target. switching to com

--- pylabs/utils/niftidict.py
+++ pylabs/utils/niftidict.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pylabs.utils.paths import getnetworkdataroot, getlocaldataroot
 #prov = niprov.ProvenanceContext()
 fs = getnetworkdataroot()
-orig = np.asarray([0, 0, 0])
+origin = np.asarray([0, 0, 0])
 project = 'bbc'
 niipickle = join(fs, project, 'bbc_niftiDict_all_subj_201608021057.pickle')
 fpath = join(fs, project, 'myvbm', 'ants_vbm_template', 'orig_vbm')
@@ -49,34 +49,36 @@ for f in fname:
     session = f.split('_')[1]
     offsets[f] = niftiDict[(subject, session, 'anat')][f]['orig_affine'][:3,3]
     offcenter[f] = niftiDict[(subject, session, 'anat')][f]['off_center']  #note y coord [0] index is inverted per philips
+#reorder offcenter to xyz with y in RAS coord system eg A is positive
+for k, v in offcenter.items():
+    offcenter[k] = np.asarray([v[2], -v[0], v[1]])
 
-min_offcenter = np.asarray(offcenter.values())[spatial.KDTree(np.asarray(offcenter.values())).query(orig)[1]]
-target_midkey = [key for key, value in offcenter.items() if np.array_equal(np.asarray(value), min_offcenter)][0]
+min_offcenter = np.asarray(offcenter.values())[spatial.KDTree(np.asarray(offcenter.values())).query(origin)[1]]
+#target_midkey = [key for key, value in offcenter.items() if np.array_equal(np.asarray(value), min_offcenter)][0]
+target_midkey = 'sub-bbc212_ses-2_mpr_1'
 target_outkey = (target_midkey.split('_')[0], target_midkey.split('_')[1], 'anat')
 tfname = join(fpath, target_midkey + '_brain_susan_nl.nii.gz')
 target_img = nibabel.load(tfname)
 target_hdr = target_img.header
 target_shape = target_img.shape
+target_affine = target_img.affine
 target_zooms = target_hdr.get_zooms()
+target_vox_offc = np.rint(offcenter[target_midkey] * target_zooms)
 
 diff_affine = {}
 diff_offcenter = {}
-for f in fname:
-    subject = f.split('_')[0]
-    session = f.split('_')[1]
-    diffa = niftiDict[(subject, session, 'anat')][f]['orig_affine'][:3, 3] - median_offsets
-    diff_affine[f] = np.zeros((4,4))
-    for i in range(3):
-        diff_affine[f][i][3]= diffa[0][i]
-    diffo = niftiDict[(subject, session, 'anat')][f]['off_center'] - median_offcenter
-    diff_offcenter[f] = [o - m for o, m in zip(offcenter[f], median_offcenter)]
-
 new_affine = {}
 for f in fname:
     subject = f.split('_')[0]
     session = f.split('_')[1]
+    diffa = target_affine[:3, 3] - niftiDict[(subject, session, 'anat')][f]['orig_affine'][:3, 3]
+    diff_affine[f] = np.zeros((4,4))
+    for i in range(3):
+        diff_affine[f][i][3]= diffa[i]
+    diff_offcenter[f] = [t - o for t, o in zip(offcenter[target_midkey], offcenter[f])]
     new_affine[f] = np.array(niftiDict[(subject, session, 'anat')][f]['orig_affine']) - diff_affine[f]
 
+diff_vox = {}
 for f in fname:
     nfname = join(fpath, f + '_brain_susan_nl.nii.gz')
     subject = f.split('_')[0]
@@ -84,17 +86,19 @@ for f in fname:
     oimg = nibabel.load(nfname)
     ohdr = oimg.header
     oaffine = oimg.affine
-    assert np.allclose(niftiDict[(subject, session, 'anat')][f]['orig_affine'], oaffine, 6), 'affines are not the same for ' + f
-    xyzroll = [int(round(diff_affine[f][i, 3] / z)) for i, z in enumerate(ohdr.get_zooms())]
+    ozooms = ohdr.get_zooms()
+    diff_vox[f] = [np.rint(do * z) for do, z in zip(diff_offcenter[f], ozooms)]
+    #assert np.allclose(niftiDict[(subject, session, 'anat')][f]['orig_affine'], oaffine, 6), 'affines are not the same for ' + f
+    #xyzroll = [int(round(diff_affine[f][i, 3] / z)) for i, z in enumerate(ohdr.get_zooms())]
+    ohdr.set_data_dtype(np.float64)
     oimg_data = oimg.get_data()
-    rimg_data = oimg_data
-    for i, r in enumerate(xyzroll):
-        if r > 0:
-            rimg_data = np.roll(rimg_data, r, axis=i)
-        if r < 0:
-            rimg_data = np.roll(rimg_data, rimg_data.shape[i] - r, axis=i)
+    for i, r in enumerate(diff_vox[f]):
+        # if r >= 1:
+        oimg_data = np.roll(oimg_data, int(r), axis=i)
+        # if r <= 1:
+        #     oimg_data = np.roll(oimg_data, int(oimg_data.shape[i]) - abs(int(r)), axis=i)
 
-    nimg = nibabel.nifti1.Nifti1Image(rimg_data, np.array(new_affine[f]), ohdr)
+    nimg = nibabel.nifti1.Nifti1Image(oimg_data, np.array(new_affine[f]), ohdr)
     nhdr = nimg.header
     nhdr.set_qform(new_affine[f], code=1)
     nhdr.set_sform(new_affine[f], code=1)
