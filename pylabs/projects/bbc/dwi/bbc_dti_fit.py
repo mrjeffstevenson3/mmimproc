95cc473584731eda931318898b0e55420b7be249
Jeff Stevenson <mrjeffs@uw.edu>
2016-10-18 12:45:12 -0700

    camino part finished

--- pylabs/projects/bbc/dwi/bbc_dti_fit.py
+++ pylabs/projects/bbc/dwi/bbc_dti_fit.py
@@ -26,6 +26,8 @@ for dwif in dwi_fnames:
         fdwi_basen = dwif + '_eddy_corrected'
         if ec_meth == 'cuda_repol':
             fdwi_basen += '_repol'
+        elif ec_meth == 'cuda_repol_std2':
+            fdwi_basen += '_repol_std2'
         fdwi = infpath / str(fdwi_basen + '.nii.gz')
         fbvecs = infpath / str(fdwi_basen + '.eddy_rotated_bvecs')
         fbvals = Path(*infpath.parts[:-1]) / str(dwif + '.bvals')
@@ -40,31 +42,69 @@ for dwif in dwi_fnames:
             mask = mask_img.get_data()
             for m in ['WLS', 'OLS']: # 'RESTORE']:
                 if m == 'RESTORE':
-                    sigma = ne.estimate_sigma(data)
-                    tenmodel = dti.TensorModel(gtab, fit_method=m, sigma=sigma)
+                    #dipy restore fails
+                    # sigma = ne.estimate_sigma(data, N=1)  #N=1 for SENSE reconstruction (Philips scanners)
+                    # tenmodel = dti.TensorModel(gtab, fit_method=m, sigma=sigma, jac=False)
+                    run_subprocess('fsl2scheme -bvecfile '+str(fbvecs)+' -bvalfile '+str(fbvals)+' > scheme.txt')
+                    cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile scheme.txt -model ldt_wtd -noisemap noise_map.Bdouble -bgmask '
+                    cmd += str(mask_fname)+' -outputfile linear_tensor.Bfloat'  # -residualmap $tmpdir/residual_map.Bdouble
+                    run_subprocess(cmd)
+                    ## grab noise map twice b/c of strange camino bug where the noise map is undersized
+                    cmd = 'cat noise_map.Bdouble noise_map.Bdouble | voxel2image -inputdatatype double -header '+str(mask_fname)+' -outputroot noise_map'
+                    run_subprocess(cmd)
+                    ## square root of the variance of the noise is sigma
+                    run_subprocess('fslmaths noise_map -sqrt sigma_map')
+                    ## get median of sigma map
+                    result = run_subprocess('fslstats sigma_map -P 50')
+                    sigma = result[0].strip(' \n')
+                    ## do the fitting
+                    cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile scheme.txt -model restore -sigma '+sigma+' -outliermap outlier_map.Bbyte -bgmask '
+                    cmd += str(mask_fname)+' -outputfile restore_tensor.Bfloat'
+                    run_subprocess(cmd)
+                    ## rename and convert files
+                    run_subprocess('cat restore_tensor.Bfloat | fa -header '+str(fdwi)+' -outputfile '+str(fdwi_basen+'_'+m+'_fa.nii.gz'))
+                    run_subprocess('cat restore_tensor.Bfloat | md -header '+str(fdwi)+' -outputfile '+str(fdwi_basen+'_'+m+'_md.nii.gz'))
+                    run_subprocess('cat restore_tensor.Bfloat | voxel2image -components 8 -header '+str(fdwi)+' -outputroot '+str(fdwi_basen+'_'+m+'_tensor_'))
+                    run_subprocess('cat outlier_map.Bbyte | voxel2image -inputdatatype byte -components 15 -header '+str(fdwi)+' -outputroot '+str(fdwi_basen+'_'+m+'_outlier_map_'))
+                    run_subprocess('cat restore_tensor.Bfloat | dteig | voxel2image -components 12 -inputdatatype double -header '+str(fdwi)+' -outputroot eigsys_')
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0001.nii.gz') exit_code
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0002.nii.gz log_s0
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0003.nii.gz dxx
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0004.nii.gz dxy
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0005.nii.gz dxz
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0006.nii.gz dyy
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0007.nii.gz dyz
+                    imcp str(fdwi_basen+'_'+m+'_tensor_0008.nii.gz dzz
+                    imcp eigsys_0001.nii.gz '+str(fdwi_basen+'_'+m+'_L1')
+                    imcp eigsys_0005.nii.gz '+str(fdwi_basen+'_'+m+'_L2')
+                    imcp eigsys_0009.nii.gz '+str(fdwi_basen+'_'+m+'_L3')
+                    'fslmerge -t '+str(fdwi_basen+'_'+m+'_V1')+' eigsys_0002.nii.gz  eigsys_0003.nii.gz  eigsys_0004.nii.gz'
+                    'fslmerge -t '+str(fdwi_basen+'_'+m+'_V2')+' eigsys_0006.nii.gz  eigsys_0007.nii.gz  eigsys_0008.nii.gz'
+                    'fslmerge -t '+str(fdwi_basen+'_'+m+'_V3')+' eigsys_0010.nii.gz  eigsys_0011.nii.gz  eigsys_0012.nii.gz'
+
                 else:
                     tenmodel = dti.TensorModel(gtab, fit_method=m)
-                fit = tenmodel.fit(data, mask)
-                fa = fit.fa
-                fa_img = nib.nifti1.Nifti1Image(fa, img.affine)
-                fa_img.set_qform(img.affine, code=1)
-                nib.save(fa_img, str(infpath / str(fdwi_basen +'_'+m+'_fa.nii')))
-                md = fit.md
-                md_img = nib.nifti1.Nifti1Image(md, img.affine)
-                md_img.set_qform(img.affine, code=1)
-                nib.save(md_img, str(infpath / str(fdwi_basen +'_'+m+'_md.nii')))
-                rd = fit.rd
-                rd_img = nib.nifti1.Nifti1Image(rd, img.affine)
-                rd_img.set_qform(img.affine, code=1)
-                nib.save(rd_img, str(infpath / str(fdwi_basen +'_'+m+'_rd.nii')))
-                ad = fit.ad
-                ad_img = nib.nifti1.Nifti1Image(ad, img.affine)
-                ad_img.set_qform(img.affine, code=1)
-                nib.save(ad_img, str(infpath / str(fdwi_basen +'_'+m+'_ad.nii')))
-                mo = fit.mode
-                mo_img = nib.nifti1.Nifti1Image(mo, img.affine)
-                mo_img.set_qform(img.affine, code=1)
-                nib.save(mo_img, str(infpath / str(fdwi_basen +'_'+m+'_mo.nii')))
+                    fit = tenmodel.fit(data, mask)
+                    fa = fit.fa
+                    fa_img = nib.nifti1.Nifti1Image(fa, img.affine)
+                    fa_img.set_qform(img.affine, code=1)
+                    nib.save(fa_img, str(infpath / str(fdwi_basen +'_'+m+'_fa.nii')))
+                    md = fit.md
+                    md_img = nib.nifti1.Nifti1Image(md, img.affine)
+                    md_img.set_qform(img.affine, code=1)
+                    nib.save(md_img, str(infpath / str(fdwi_basen +'_'+m+'_md.nii')))
+                    rd = fit.rd
+                    rd_img = nib.nifti1.Nifti1Image(rd, img.affine)
+                    rd_img.set_qform(img.affine, code=1)
+                    nib.save(rd_img, str(infpath / str(fdwi_basen +'_'+m+'_rd.nii')))
+                    ad = fit.ad
+                    ad_img = nib.nifti1.Nifti1Image(ad, img.affine)
+                    ad_img.set_qform(img.affine, code=1)
+                    nib.save(ad_img, str(infpath / str(fdwi_basen +'_'+m+'_ad.nii')))
+                    mo = fit.mode
+                    mo_img = nib.nifti1.Nifti1Image(mo, img.affine)
+                    mo_img.set_qform(img.affine, code=1)
+                    nib.save(mo_img, str(infpath / str(fdwi_basen +'_'+m+'_mo.nii')))
 
 
 
