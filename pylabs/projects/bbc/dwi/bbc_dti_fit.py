af8e6fc028b429e6b8d2851944938e78ed249dc1
Jeff Stevenson <mrjeffs@uw.edu>
2016-10-19 22:01:37 -0700

    cleanup

--- pylabs/projects/bbc/dwi/bbc_dti_fit.py
+++ pylabs/projects/bbc/dwi/bbc_dti_fit.py
@@ -19,7 +19,7 @@ fname_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [fname_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwi_passed_qc]
 
 for dwif in dwi_fnames:
-#    for ec_meth in ['cuda_repol_std2']:     # death match ['cuda_defaults', 'cuda_repol', 'cuda_repol_std2']:
+    #for ec_meth in ['cuda_repol_std2']:     # death match ['cuda_defaults', 'cuda_repol', 'cuda_repol_std2']:
     ec_meth = 'cuda_repol_std2'
     infpath = fs / project / dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / ec_meth
     fdwi_basen = dwif + '_eddy_corrected'
@@ -48,11 +48,12 @@ for dwif in dwi_fnames:
                 # sigma = ne.estimate_sigma(data, N=1)  #N=1 for SENSE reconstruction (Philips scanners)
                 # tenmodel = dti.TensorModel(gtab, fit_method=m, sigma=sigma, jac=False)
                 run_subprocess('fsl2scheme -bvecfile '+str(fbvecs)+' -bvalfile '+str(fbvals)+' > '+m+'/scheme.txt')
-                cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile '+m+'/scheme.txt -model ldt_wtd -noisemap '+m+'/noise_map.Bdouble -bgmask '
-                cmd += str(mask_fname)+' -outputfile '+m+'/linear_tensor.Bfloat'  # -residualmap $tmpdir/residual_map.Bdouble
+                cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile '+m+'/scheme.txt -model ldt_wtd -noisemap '
+                cmd += m+'/noise_map.Bdouble -bgmask '+str(mask_fname)+' -outputfile '+m+'/linear_tensor.Bfloat'
                 run_subprocess(cmd)
                 ## grab noise map twice b/c of strange camino bug where the noise map is undersized
-                cmd = 'cat '+m+'/noise_map.Bdouble '+m+'/noise_map.Bdouble | voxel2image -inputdatatype double -header '+str(mask_fname)+' -outputroot '+m+'/noise_map'
+                cmd = 'cat '+m+'/noise_map.Bdouble '+m+'/noise_map.Bdouble | voxel2image -inputdatatype double -header '
+                cmd += str(mask_fname)+' -outputroot '+m+'/noise_map'
                 run_subprocess(cmd)
                 ## square root of the variance of the noise is sigma
                 run_subprocess('fslmaths '+m+'/noise_map -sqrt '+m+'/sigma_map')
@@ -60,30 +61,42 @@ for dwif in dwi_fnames:
                 result = run_subprocess('fslstats '+m+'/sigma_map -P 50')
                 sigma = result[0].strip(' \n')
                 ## do the fitting
-                cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile '+m+'/scheme.txt -model restore -sigma '+sigma+' -outliermap '+m+'/outlier_map.Bbyte -bgmask '
+                cmd = 'modelfit -inputfile '+str(fdwi)+' -schemefile '+m+'/scheme.txt -model restore -sigma '
+                cmd += sigma+' -outliermap '+m+'/outlier_map.Bbyte -bgmask '
                 cmd += str(mask_fname)+' -outputfile '+m+'/restore_tensor.Bfloat'
                 run_subprocess(cmd)
                 with WorkingContext(m):
                     ## rename and convert files back to nii
-                    run_subprocess('cat restore_tensor.Bfloat | fa -header '+str(fdwi)+' -outputfile '+str(fdwi_basen)+'_'+m.lower()+'_fa.nii.gz')
-                    run_subprocess('cat restore_tensor.Bfloat | md -header '+str(fdwi)+' -outputfile '+str(fdwi_basen)+'_'+m.lower()+'_md.nii.gz')
-                    run_subprocess('cat restore_tensor.Bfloat | voxel2image -components 8 -header '+str(fdwi)+' -outputroot '+str(fdwi_basen)+'_'+m.lower()+'_tensor_')
-                    run_subprocess('cat outlier_map.Bbyte | voxel2image -inputdatatype byte -components '+str(num_dirs)+' -header '+str(fdwi)+' -outputroot '+str(fdwi_basen+'_'+m.lower()+'_outlier_map_'))
-                    run_subprocess('cat restore_tensor.Bfloat | dteig | voxel2image -components 12 -inputdatatype double -header '+str(fdwi)+' -outputroot eigsys_')
-                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor '+str(fdwi_basen)+'_'+m.lower()+'_tensor_0003.nii.gz '
-                                    +str(fdwi_basen)+'_'+m.lower()+'_tensor_0004.nii.gz '
-                                    +str(fdwi_basen)+'_'+m.lower()+'_tensor_0005.nii.gz '
-                                    +str(fdwi_basen)+'_'+m.lower()+'_tensor_0006.nii.gz '
-                                    +str(fdwi_basen)+'_'+m.lower()+'_tensor_0007.nii.gz '
-                                    +str(fdwi_basen)+'_'+m.lower()+'_tensor_0008.nii.gz ')
-                    run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor -fmedian '+str(fdwi_basen + '_' + m.lower()+ '_cam2fsl_tensor_mf'))
-                    run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor_mf -tensor_decomp '+str(fdwi_basen+'_'+m.lower()+'_cam2fsl_tensor_mf'))
+                    run_subprocess('cat restore_tensor.Bfloat | fa -header '+str(fdwi)+' -outputfile '+str(
+                        fdwi_basen)+'_'+m.lower()+'_fa.nii.gz')
+                    run_subprocess('cat restore_tensor.Bfloat | md -header '+str(fdwi)+' -outputfile '+str(
+                        fdwi_basen)+'_'+m.lower()+'_md.nii.gz')
+                    run_subprocess('cat restore_tensor.Bfloat | voxel2image -components 8 -header '+str(
+                        fdwi)+' -outputroot '+str(fdwi_basen)+'_'+m.lower()+'_tensor_')
+                    run_subprocess('cat outlier_map.Bbyte | voxel2image -inputdatatype byte -components '+str(
+                        num_dirs)+' -header '+str(fdwi)+' -outputroot '+str(fdwi_basen+'_'+m.lower()+'_outlier_map_'))
+                    run_subprocess('cat restore_tensor.Bfloat | dteig | voxel2image -components 12 '
+                                   '-inputdatatype double -header '+str(fdwi)+' -outputroot eigsys_')
+                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0003.nii.gz '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0004.nii.gz '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0005.nii.gz '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0006.nii.gz '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0007.nii.gz '
+                                   +str(fdwi_basen)+'_'+m.lower()+'_tensor_0008.nii.gz ')
+                    run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor -fmedian '+str(
+                        fdwi_basen + '_' + m.lower()+ '_cam2fsl_tensor_mf'))
+                    run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_cam2fsl_tensor_mf -tensor_decomp '+str(
+                        fdwi_basen+'_'+m.lower()+'_cam2fsl_tensor_mf'))
                     run_subprocess('imcp eigsys_0001.nii.gz '+str(fdwi_basen)+'_'+m.lower()+'_L1')
                     run_subprocess('imcp eigsys_0005.nii.gz '+str(fdwi_basen)+'_'+m.lower()+'_L2')
                     run_subprocess('imcp eigsys_0009.nii.gz '+str(fdwi_basen)+'_'+m.lower()+'_L3')
-                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V1 eigsys_0002.nii.gz  eigsys_0003.nii.gz  eigsys_0004.nii.gz')
-                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V2 eigsys_0006.nii.gz  eigsys_0007.nii.gz  eigsys_0008.nii.gz')
-                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V3 eigsys_0010.nii.gz  eigsys_0011.nii.gz  eigsys_0012.nii.gz')
+                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V1 '
+                                    'eigsys_0002.nii.gz  eigsys_0003.nii.gz  eigsys_0004.nii.gz')
+                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V2 '
+                                    'eigsys_0006.nii.gz  eigsys_0007.nii.gz  eigsys_0008.nii.gz')
+                    run_subprocess('fslmerge -t '+str(fdwi_basen)+'_'+m.lower()+'_V3 '
+                                    'eigsys_0010.nii.gz  eigsys_0011.nii.gz  eigsys_0012.nii.gz')
                     #fix output headers
                     for f in [str(fdwi_basen)+'_'+m.lower()+'_fa.nii.gz', str(fdwi_basen)+'_'+m.lower()+'_md.nii.gz', str(fdwi_basen)+'_'+m.lower()+'_L1.nii.gz']:
                         r_img = nib.load(f)
@@ -136,14 +149,14 @@ for dwif in dwi_fnames:
                                                err_msg='output qform in header does not match input qform')
                 nib.save(mo_img, str(infpath / m / str(fdwi_basen +'_'+m.lower()+'_dipy_mo.nii')))
                 if m == 'OLS':
-                    run_subprocess('dtifit --data='+str(fdwi)+' -m '+str(mask_fname)+' --bvecs='+str(fbvecs)+' --bvals='+str(fbvals)+' --sse --save_tensor -o '+str(infpath / m / str(fdwi_basen +'_'+m.lower()+'_fsl')))
+                    run_subprocess('dtifit --data='+str(fdwi)+' -m '+str(mask_fname)+' --bvecs='+str(fbvecs)+' --bvals='+str(
+                        fbvals)+' --sse --save_tensor -o '+str(infpath / m / str(fdwi_basen +'_'+m.lower()+'_fsl')))
                     with WorkingContext(str(infpath / m)):
-                        run_subprocess('fslmaths '+str(fdwi_basen)+ '_'+m.lower()+'_fsl_tensor -fmedian '+str(fdwi_basen + '_' + m.lower() + '_fsl_tensor_mf'))
-                        run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_fsl_tensor_mf -tensor_decomp '+str(fdwi_basen + '_' + m.lower() + '_fsl_tensor_mf'))
+                        run_subprocess('fslmaths '+str(fdwi_basen)+ '_'+m.lower()+'_fsl_tensor -fmedian '+str(fdwi_basen+'_'+m.lower()+'_fsl_tensor_mf'))
+                        run_subprocess('fslmaths '+str(fdwi_basen)+'_'+m.lower()+'_fsl_tensor_mf -tensor_decomp '+str(fdwi_basen+'_'+m.lower()+'_fsl_tensor_mf'))
                 if m == 'WLS':
-                    run_subprocess(
-                        'dtifit --data=' + str(fdwi) + ' -m ' + str(mask_fname) + ' --bvecs=' + str(fbvecs) + ' --bvals=' + str(
-                            fbvals) + ' --sse --save_tensor --wls -o ' + str(infpath / m / str(fdwi_basen + '_' + m.lower() + '_fsl')))
+                    run_subprocess('dtifit --data='+str(fdwi)+' -m '+str(mask_fname)+' --bvecs='+str(fbvecs)+' --bvals='+str(
+                            fbvals)+' --sse --save_tensor --wls -o '+str(infpath / m / str(fdwi_basen+'_'+m.lower()+'_fsl')))
                     with WorkingContext(str(infpath / m)):
                         run_subprocess('fslmaths ' + str(fdwi_basen) + '_' + m.lower() + '_fsl_tensor -fmedian ' + str(
                             fdwi_basen + '_' + m.lower() + '_fsl_tensor_mf'))
