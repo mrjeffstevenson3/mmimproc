1cefcb35fc0dfe01fcffd2ba9578dd1faec13ef5
Jeff Stevenson <mrjeffs@uw.edu>
2016-10-26 20:34:34 -0700

    made sym link to orig vbm

--- pylabs/projects/bbc/reg/reg_dwi2vbm.py
+++ pylabs/projects/bbc/reg/reg_dwi2vbm.py
@@ -19,12 +19,15 @@ dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
 vbm_fnames = [vbm_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in vbmpairing]
+orig_vbmdir = fs / project / 'reg' / 'orig_paired_vbm_staring_point'
+if not orig_vbmdir.is_symlink():
+    orig_vbmdir.symlink_to(fs / project / 'myvbm' / 'ants_vbm_template_pairedLH' / 'orig_vbm', target_is_directory=True)
 
 with WorkingContext(str(fs / project / 'reg' / 'FA_fsl_wls_tensor_mf_ero_paired')):
     for d in dwi_fnames[1:]:
-        print d
         mask = fs / project / d.split('_')[0] / d.split('_')[1] / 'dwi' / str(d+'_S0_brain_mask.nii')
         oFA = fs / project / d.split('_')[0] / d.split('_')[1] / 'dwi' / 'cuda_repol_std2' / 'WLS' / str(d+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA.nii.gz')
         out = fs / project / 'reg' / 'FA_fsl_wls_tensor_mf_ero_paired' / str(d + '_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero.nii.gz')
         cmd = 'fslmaths '+str(mask)+' -ero -mul '+str(oFA)+' '+str(out)
         run_subprocess(cmd)
+
