a47a3bdf7ca477c5a793c6c5c5d67992d9b2f9ff
mrjeffs <mrjeffs@uw.edu>
2016-11-04 19:49:30 -0700

    start of tensor convert

--- pylabs/projects/bbc/dwi/generate_vtk.py
+++ pylabs/projects/bbc/dwi/generate_vtk.py
@@ -1,6 +1,5 @@
 import os, inspect
 from pathlib import *
-from os.path import split
 import numpy as np
 import nibabel as nib
 import niprov, pylabs
@@ -11,21 +10,12 @@ from pylabs.utils import run_subprocess, WorkingContext
 #setup paths and file names to process
 fs = Path(getnetworkdataroot())
 pylabs_atlasdir = Path(*Path(inspect.getabsfile(pylabs)).parts[:-2]) / 'data' / 'atlases'
-slicer_path = Path(*Path(inspect.getabsfile(pylabs)).parts[:-3]) / 'Slicer-dir' / 'Slicer'
 MNI_atlases = {'mori': pylabs_atlasdir / 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz',
                 'aal_motor': pylabs_atlasdir / 'aal_1mm_motorcortex.nii',
                 'mori_LeftPostIntCap-35': pylabs_atlasdir / 'mori_LeftPostIntCap-35.nii',
                 'mori_RightPostIntCap-123': pylabs_atlasdir / 'mori_RightPostIntCap-123.nii',
                 'mori_base_mask52only': pylabs_atlasdir / 'mori_base_mask52only.nii'
                 }
-cmd = { 'mori': None,
-        'aal_motor': pylabs_atlasdir / 'aal_1mm_motorcortex.nii',
-        'mori_LeftPostIntCap-35': pylabs_atlasdir / 'mori_LeftPostIntCap-35.nii',
-        'mori_RightPostIntCap-123': pylabs_atlasdir / 'mori_RightPostIntCap-123.nii',
-        'mori_base_mask52only':
-
-
-    }
 project = 'bbc'
 fa2t1_outdir = 'reg_subFA2suborigvbmpaired'
 fadir = 'FA_fsl_wls_tensor_mf_ero_paired'
@@ -38,19 +28,3 @@ MNI2templ_invwarp = templdir / 'bbc_pairedLH_template_reg2MNI_1InverseWarp.nii.g
 MNI2templ_aff = templdir / 'bbc_pairedLH_template_reg2MNI_0GenericAffine.mat'
 dwi2vbmsubjdir = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired'
 dwi_reg_append = '_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_'
-
-# dwif = dwi_fnames[1]
-# vbmf = vbm_fnames[1]
-for dwif, vbmf in zip(dwi_fnames[1:], vbm_fnames[1:]):
-    for k, a in MNI_atlases.iteritems():
-        execwdir = fs / project /  dwif.split('_')[0] / dwif.split('_')[1] / 'dwi'
-        mov = a
-        ref = execwdir / str(dwif+'_S0_brain.nii')
-        outf = execwdir / str(dwif+'_'+k+'.nii')
-        iwarp_templ2vbmsubj = templdir / str(vbmf+'InverseWarp.nii.gz')
-        iwarp_vbmsub2dwi = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired' / str(dwif+ dwi_reg_append +'1InverseWarp.nii.gz')
-        aff_templ2vbmsubj = templdir / str(vbmf+'Affine.txt')
-        aff_vbmsub2dwi = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired' / str(dwif + dwi_reg_append + '0GenericAffine.mat')
-        warpfiles = [str(MNI2templ_invwarp), str(iwarp_templ2vbmsubj), str(iwarp_vbmsub2dwi)]
-        affine_xform = [str(MNI2templ_aff), str(aff_templ2vbmsubj), str(aff_vbmsub2dwi)]
-        subj2templ_applywarp(str(mov), str(ref), str(outf), warpfiles, str(execwdir), affine_xform=affine_xform, inv=True)
