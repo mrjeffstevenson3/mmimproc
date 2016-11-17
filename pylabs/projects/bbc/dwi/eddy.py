1a9e0f5633cd9aee81c7116c2b2e283619e8e5a3
mrjeffs <mrjeffs@uw.edu>
2016-11-16 22:25:17 -0800

    revised eddy

--- pylabs/projects/bbc/dwi/eddy.py
+++ pylabs/projects/bbc/dwi/eddy.py
@@ -1,10 +1,11 @@
 #this script takes converted bbc dti data, skull strips and performs eddy current correction with and without
 #--repol replace outliers
 import os, inspect, datetime
+from pathlib import *
 from os.path import join, basename, dirname, isfile, isdir, split
 import numpy as np
 import nibabel as nib
-import niprov, pylabs
+import pylabs
 from scipy.ndimage.measurements import center_of_mass as com
 from nipype.interfaces import fsl
 flt = fsl.FLIRT(bins=640, interp='nearestneighbour', cost_func='mutualinfo', output_type='NIFTI')
@@ -13,12 +14,17 @@ bet = fsl.BET(output_type='NIFTI')
 from dipy.io import read_bvals_bvecs
 from dipy.core.gradients import gradient_table
 from dipy.segment.mask import applymask
-prov = niprov.ProvenanceContext()
+from pylabs.io.images import savenii
 from pylabs.projects.bbc.dwi.passed_qc import dwi_passed_qc, dwi_passed_101
-from pylabs.utils.paths import getnetworkdataroot
+from pylabs.utils.provenance import ProvenanceWrapper
+provenance = ProvenanceWrapper()
 from pylabs.utils import run_subprocess, WorkingContext
+from pylabs.utils.paths import getnetworkdataroot
 fs = getnetworkdataroot()
-pylabs_basepath = split(split(inspect.getabsfile(pylabs))[0])[0]
+pylabs_basepath = Path(*Path(inspect.getabsfile(pylabs)).parts[:-1])
+MNI_bet_zcut = pylabs_basepath / 'data' / 'atlases' / 'MNI152_T1_1mm_bet_zcut.nii.gz'
+MNI_bet_zcut_mask = pylabs_basepath / 'data' / 'atlases' / 'MNI152_T1_1mm_bet_zcut_mask.nii.gz'
+MNI_bet_com = pylabs_basepath / 'data' / 'atlases' / 'MNI152_T1_1mm-com-mask8k.nii.gz'
 project = 'bbc'
 fname_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [fname_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwi_passed_qc]
@@ -41,36 +47,42 @@ for dwif in dwi_fnames:
         nS0_img = nib.Nifti1Image(S0, img.affine)
         nS0_img.set_qform(img.affine, code=1)
         nib.save(nS0_img, S0_fname)
-        prov.log(S0_fname, 'S0 dwi from '+dwif, fdwi, code=__file__)
+        provenance.log(S0_fname, 'S0 dwi from '+dwif, fdwi, code=__file__)
+        #setup result object capture
+        fslresult = ()
+        dt = datetime.datetime.now()
+        fslresult += (str(dt),'flirt_zcut2S0')
         # make mat file to apply mask and com
-        flt.inputs.in_file = join(pylabs_basepath, 'data', 'atlases', 'MNI152_T1_1mm_bet_zcut.nii.gz')
+        flt.inputs.in_file = str(MNI_bet_zcut)
         flt.inputs.reference = S0_fname
         flt.inputs.out_matrix_file = S0_fname[: -6] + 'bet2S0.mat'
         flt.inputs.out_file = S0_fname[: -6] + 'S0_zcut.nii'
-        res = flt.run()
+        fslresult += flt.run()
         # apply mat file to center of mass ROI in MNI template
+        dt = datetime.datetime.now()
+        fslresult += (str(dt),'applyxfm_com2S0')
         applyxfm.inputs.in_matrix_file = S0_fname[: -6] + 'bet2S0.mat'
-        applyxfm.inputs.in_file = join(pylabs_basepath, 'data', 'atlases', 'MNI152_T1_1mm-com-mask8k.nii.gz')
+        applyxfm.inputs.in_file = str(MNI_bet_com)
         applyxfm.inputs.out_file = join(infpath, dwif + '_S0_match_bet_com_roi.nii')
         applyxfm.inputs.reference = S0_fname
         applyxfm.inputs.apply_xfm = True
-        result = applyxfm.run()
+        fslresult += applyxfm.run()
         # apply mat file to MNI mask file to cut off neck
+        dt = datetime.datetime.now()
+        fslresult += (str(dt),'applyxfm_mask2S0')
         applyxfm.inputs.in_matrix_file = S0_fname[: -6] + 'bet2S0.mat'
-        applyxfm.inputs.in_file = join(pylabs_basepath, 'data', 'atlases', 'MNI152_T1_1mm_bet_zcut_mask.nii.gz')
+        applyxfm.inputs.in_file = str(MNI_bet_zcut_mask)
         applyxfm.inputs.out_file = join(infpath, dwif + '_S0_mask.nii')
         applyxfm.inputs.reference = S0_fname
         applyxfm.inputs.apply_xfm = True
-        result = applyxfm.run()
+        fslresult += applyxfm.run()
         # chop off neck with MNI zcut
         zcut_data = nib.load(join(infpath, dwif + '_S0_mask.nii')).get_data()
         zcut_data_maskb = zcut_data > 0
         S0_mask = np.zeros(np.squeeze(S0).shape)  # need to add a fourth dim here
         S0_mask[zcut_data_maskb] = 1
         S0_zcut = applymask(S0, S0_mask)
-        nzcut_img = nib.nifti1.Nifti1Image(S0_zcut, img.affine)
-        nzcut_img.set_qform(img.affine, code=1)
-        nib.save(nzcut_img, join(infpath, dwif + '_S0_zcut.nii'))
+        savenii(S0_zcut, img.affine, join(infpath, dwif + '_S0_zcut.nii'))
 
         # get com for fsl bet
         com_data = nib.load(join(infpath, dwif + '_S0_match_bet_com_roi.nii')).get_data()
@@ -87,9 +99,9 @@ for dwif in dwi_fnames:
         bet.inputs.mask = True
         bet.inputs.skull = True
         bet.inputs.out_file = brain_outfname + '.nii'
-        betres = bet.run()
-        prov.log( brain_outfname + '.nii', 'brain extracted S0 dwi from ' + dwif, S0_fname, code=__file__)
-        prov.log(brain_outfname + '_mask.nii', 'dwi brain mask from ' + dwif, S0_fname, code=__file__)
+        fslresult += bet.run()
+        provenance.log(brain_outfname + '.nii', 'brain extracted S0 dwi from ' + dwif, S0_fname, code=__file__, provenance=fslresult)
+        provenance.log(brain_outfname + '_mask.nii', 'dwi brain mask from ' + dwif, S0_fname, code=__file__, provenance=fslresult)
         # make index and acquisition parameters files
         with open(join(infpath, 'index.txt'), 'w') as f:
             f.write('1 ' * len(gtab.bvals))
@@ -99,27 +111,6 @@ for dwif in dwi_fnames:
 
         with open(join(infpath, 'acq_params.txt'), 'w') as f:
             f.write('0 1 0 ' + dwell)
-        # execute eddy command in subprocess in local working directory using defaults
-        # outpath = join(infpath, 'cuda_defaults')
-        # if not isdir(outpath):
-        #     os.makedirs(outpath)
-        # cmd = ''
-        # cmd += 'eddy_cuda7.5 --acqp=acq_params.txt --bvals=' + fbvals + ' --bvecs=' + fbvecs
-        # cmd += ' --imain=' + fdwi + ' --index=index.txt --mask=' + brain_outfname + '_mask.nii '
-        # cmd += '--out=' + join(outpath, dwif + '_eddy_corrected')
-        # run_subprocess(cmd)
-        # prov.log(join(outpath, dwif + '_eddy_corrected.nii.gz'), 'dwi eddy current correction using default options', fdwi, code=__file__)
-        # # # execute eddy command in subprocess in local working directory using repol
-        # outpath = join(infpath, 'cuda_repol')
-        # if not isdir(outpath):
-        #     os.makedirs(outpath)
-        # cmd = ''
-        # cmd += 'eddy_cuda7.5 --acqp=acq_params.txt --bvals=' + fbvals + ' --bvecs=' + fbvecs
-        # cmd += ' --imain=' + fdwi + ' --index=index.txt --mask=' + brain_outfname + '_mask.nii '
-        # cmd += '--out=' + join(outpath, dwif + '_eddy_corrected_repol') + ' --repol'
-        # run_subprocess(cmd)
-        # prov.log(join(outpath, dwif + '_eddy_corrected_repol.nii.gz'), 'dwi eddy current correction using --repol options',
-        #          fdwi, code=__file__)
 
         # execute eddy command in subprocess in local working directory using repol and lower stddev and linear 2nd level model
         # winner of DWI preproc deathmatch Oct-2016
@@ -130,11 +121,11 @@ for dwif in dwi_fnames:
         output = ()
         dt = datetime.datetime.now()
         output += (str(dt),)
-        cmdt = (cmd,)
-        output += cmdt
         cmd += 'eddy_cuda7.5 --acqp=acq_params.txt --bvals=' + fbvals + ' --bvecs=' + fbvecs
         cmd += ' --imain=' + fdwi + ' --index=index.txt --mask=' + brain_outfname + '_mask.nii '
         cmd += '--out=' + join(outpath, dwif + '_eddy_corrected_repol_std2') + ' --repol --ol_sqr --slm=linear --ol_nstd=2 --niter=9 --fwhm=20,5,0,0,0,0,0,0,0'
+        cmdt = (cmd,)
+        output += cmdt
         output += run_subprocess(cmd)
         params = {}
         params['eddy cmd'] = cmd
@@ -143,11 +134,11 @@ for dwif in dwi_fnames:
         output = ()
         dt = datetime.datetime.now()
         output += (str(dt),)
+        cmd += 'fslmaths '+ join(outpath, dwif + '_eddy_corrected_repol_std2') + ' -thr 1 ' + join(outpath, dwif + '_eddy_corrected_repol_std2_thr1')
         cmdt = (cmd,)
         output += cmdt
-        cmd += 'fslmaths '+ join(outpath, dwif + '_eddy_corrected_repol_std2') + ' -thr 1 ' + join(outpath, dwif + '_eddy_corrected_repol_std2_thr1')
         output += run_subprocess(cmd)
         params['flsmaths clamping cmd'] = cmd
         params['flsmaths output'] = output
-        prov.log(join(outpath, dwif + '_eddy_corrected_repol_std2_thr1.nii.gz'), 'eddy using --repol --ol_sqr --slm=linear --ol_nstd=2 --niter=9 --fwhm=20,5,0,0,0,0,0,0,0',
+        provenance.log(join(outpath, dwif + '_eddy_corrected_repol_std2_thr1.nii.gz'), 'eddy using --repol --ol_sqr --slm=linear --ol_nstd=2 --niter=9 --fwhm=20,5,0,0,0,0,0,0,0',
                  fdwi, code=__file__, provenance=params)
