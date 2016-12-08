d7bbf5a213fe57e0af2ba7db64b25895bc69700a
Jeff Stevenson <mrjeffs@uw.edu>
2016-12-08 15:28:37 -0800

    fix com roll to always wrap com vol with 6 empty vox before com calculated and rolled

--- pylabs/projects/bbc/dwi/dwi_template.py
+++ pylabs/projects/bbc/dwi/dwi_template.py
@@ -18,8 +18,8 @@ pylabs_basepath = Path(*Path(inspect.getabsfile(pylabs)).parts[:-1])
 project = 'bbc'
 #eddy corrected method directory. should get from eddy.
 ec_meth = 'cuda_repol_std2_v2'
-#fit methods to loop over: excl: 'wls_dipy_mf_FA.nii', 'ols_dipy_mf_FA.nii',
-fitmeth = ['ols_fsl_tensor_mf_FA.nii.gz', 'wls_fsl_tensor_mf_FA.nii.gz']
+#fit methods to loop over: excl:
+fitmeth = ['wls_dipy_mf_FA.nii', 'ols_dipy_mf_FA.nii', 'ols_fsl_tensor_mf_FA.nii.gz', 'wls_fsl_tensor_mf_FA.nii.gz']
 
 fname_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [fname_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
@@ -31,7 +31,7 @@ mm_diff_com = {}
 diff_offcenter2com = {}
 com_affine = {}
 subj2com = {}
-for f in [dwi_fnames[0]]:
+for f in dwi_fnames:
     for m in fitmeth:
         infpath = fs / project / f.split('_')[0] / f.split('_')[1] / 'dwi' / ec_meth / m[0:3].upper()
         fdwi_basen = f + '_eddy_corrected_repol_std2_'
@@ -41,12 +41,6 @@ for f in [dwi_fnames[0]]:
         img_data = img.get_data()
         affine = img.affine
         zooms = hdr.get_zooms()
-        # try:
-        #     com_vox[f] = np.round(com(img_data)).astype(int)
-        #     diff_com[f] = np.asarray([s / 2. - c for s, c in zip(img_data.shape, com_vox[f])])
-        #     for i, r in enumerate(diff_com[f]):
-        #         img_data = np.roll(img_data, int(r), axis=i)
-        # except (TypeError):
         exp_data = np.zeros(tuple(map(sum, zip(img_data.shape), (12, 12, 12))))
         exp_data[6:img_data.shape[0] + 6, 6:img_data.shape[1] + 6, 6:img_data.shape[2] + 6] = img_data
         com_edata = com(exp_data)
