4c460081923619040e6962e1f6e2293e2516b2a2
mrjeffs <mrjeffs@uw.edu>
2016-12-12 09:16:57 -0800

    file setup for warpimage

--- pylabs/projects/bbc/dwi/reg_dwi2templ.py
+++ pylabs/projects/bbc/dwi/reg_dwi2templ.py
@@ -15,17 +15,21 @@ provenance = ProvenanceWrapper()
 #setup paths and file names to process
 fs = Path(getnetworkdataroot())
 
-meths = {'RESTORE':['_eddy_corrected_repol_std2_restore_cam_tensor_mf', '_eddy_corrected_repol_std2_restore'],
-            'OLS': ['_eddy_corrected_repol_std2_ols_fsl_tensor_medfilt.nhdr', '_eddy_corrected_repol_std2_ols_fsl_tensor.nhdr', '_eddy_corrected_repol_std2_ols_dipy_tensor.nhdr', '_eddy_corrected_repol_std2_ols_dipy_tensor_medfilt.nhdr'],
-            'WLS': ['_eddy_corrected_repol_std2_wls_fsl_tensor_medfilt.nhdr', '_eddy_corrected_repol_std2_wls_fsl_tensor.nhdr', '_eddy_corrected_repol_std2_wls_dipy_tensor.nhdr', '_eddy_corrected_repol_std2_wls_dipy_tensor_medfilt.nhdr']
+#loop over modality then fit method FA then dipy_wls
+
+mods = ['FA', 'MD']
+
+fitmeths = {'OLS': ['%(m)s_dipy', '%(m)s_cam', '%(m)s_fsl'],
+            'WLS': ['%(m)s_dipy', '%(m)s_cam', '%(m)s_fsl'],
+            'RESTORE': ['%(m)s_dipy', '%(m)s_cam'],
             }
-mods = {'RESTORE':[['_FA', '_L1', '_MD', '_MO'],['_fa', '_L1', '_md']]
-            'OLS': ['
-            'WLS': ['
-        }
+
+meths = ['wls_dipy', 'wls_fsl', 'wls_cam', 'wls_dipy_mf', 'wls_fsl_mf', 'wls_cam_mf',
+         'ols_dipy', 'ols_fsl', 'ols_cam', 'ols_dipy_mf', 'ols_fsl_mf', 'ols_cam_mf',
+         'restore_cam', 'restore_dipy', 'restore_cam_mf', 'restore_dipy_mf']
+
+
 project = 'bbc'
-fa2t1_outdir = 'reg_subFA2suborigvbmpaired_run2'
-fadir = 'FA_fsl_wls_tensor_mf_ero_paired'
 dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
@@ -34,10 +38,13 @@ templdir = fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space'
 templ_ref = templdir / 'bbc_pairedLH_template_resampled2dwi.nii'
 MNI2templ_invwarp = templdir / 'bbc_pairedLH_template_reg2MNI_1InverseWarp.nii.gz'
 MNI2templ_aff = templdir / 'bbc_pairedLH_template_reg2MNI_0GenericAffine.mat'
-dwi2vbmsubjdir = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired'
+dwi2vbmsubjdir = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2'
 dwi_reg_append = '_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_'
 
 for dwif, vbmf in zip(dwi_fnames[1], vbm_fnames[1]):
+
+    fa2t1_outdir = fs / project / 'reg' / 'dwi_in_template_space' / mods / '_'.join([mods, meths, 'warped2templ'])
+
     for k, m in meths.iteritems():
         execwdir = fs / project /  dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / 'cuda_repol_std2_v2'
         mov = a         # always dwi modality
@@ -49,12 +56,10 @@ for dwif, vbmf in zip(dwi_fnames[1], vbm_fnames[1]):
         aff_vbmsubj2templ = templdir / str(vbmf+'Affine.txt')
         warpfiles = [str(iwarp_vbmsub2dwi), str(iwarp_templ2vbmsubj)]
         affine_xform = [str(aff_vbmsub2dwi), str(aff_templ2vbmsubj)]
+
+
         for km, m in mods.iteritems():
             subj2templ_applywarp(str(mov), str(ref), str(outf)+'.nii', warpfiles, str(execwdir / km), affine_xform=affine_xform)
 
 
-
-'''
-Restore
-
-'''
+# need ref, in and out files, t1 to template and dwi to t1 warp and affines.
