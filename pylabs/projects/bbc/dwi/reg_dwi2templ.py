672f570848092444126ae0aa8c4fd249ce2c37a9
Jeff Stevenson <mrjeffs@uw.edu>
2016-12-12 15:47:41 -0800

    warp dwi 2 templ working version

--- pylabs/projects/bbc/dwi/reg_dwi2templ.py
+++ pylabs/projects/bbc/dwi/reg_dwi2templ.py
@@ -21,14 +21,14 @@ dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
 vbm_fnames = [vbm_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in vbmpairing]
-outf_fmat = '{dwif}_eddy_corrected_repol_std2_{f}_reg2vbmtempl.nii'
+outf_fmat = '{dwif}_eddy_corrected_repol_std2_{f}_{m}_reg2vbmtempl.nii'
 
 for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
     for k, fm in fitmethsd.iteritems():
         for f in fm:
             for m in mods:
                 mov = (fs / project / dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / 'cuda_repol_std2_v2' / k / '_'.join([dwif, 'eddy_corrected_repol_std2', f, m+fext[f]]))
-                outf = (fs / project / 'reg' / 'dwi_warps_in_template_space' / m / f / outf_fmat.format(dwif=dwif, f=f))
+                outf = (fs / project / 'reg' / 'dwi_warps_in_template_space' / m / f / outf_fmat.format(dwif=dwif, f=f, m=m))
                 warp1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Warp.nii.gz'))
                 aff1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Affine.txt'))
                 warp2 = (fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2' / str(dwif+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_1Warp.nii.gz'))
@@ -39,10 +39,3 @@ for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
                 if not execwdir.is_dir():
                     execwdir.mkdir(parents=True)
                 subj2templ_applywarp(str(mov), str(ref), str(outf), warpfiles, str(execwdir), affine_xform=affine_xform)
-                params = {}
-                params['warpfiles'] = warpfiles
-                params['affine_xform'] = affine_xform
-                params['execwdir'] = str(execwdir)
-                params['ref'] = str(ref)
-                provenance.log(str(mov), 'apply ants T1 template warps and affines to bring FA into template space', str(outf),
-                               script=__file__, provenance=params)
