751c6296fdff2b271bed29998a8aa1238502d625
Jeff Stevenson <mrjeffs@uw.edu>
2016-12-12 15:35:12 -0800

    warp dwi 2 templ with prov

--- pylabs/projects/bbc/dwi/reg_dwi2templ.py
+++ pylabs/projects/bbc/dwi/reg_dwi2templ.py
@@ -1,14 +1,7 @@
-import os, inspect
 from pathlib import *
-import datetime
-from os.path import split
-import numpy as np
-import nibabel as nib
-import pylabs
 from pylabs.alignment.ants_reg import subj2templ_applywarp
 from pylabs.projects.bbc.pairing import vbmpairing, dwipairing
 from pylabs.utils.paths import getnetworkdataroot
-from pylabs.utils import run_subprocess, WorkingContext
 #set up provenance
 from pylabs.utils.provenance import ProvenanceWrapper
 provenance = ProvenanceWrapper()
@@ -19,47 +12,46 @@ fs = Path(getnetworkdataroot())
 
 mods = ['FA', 'MD']
 
-fitmeths = {'OLS': ['%(m)s_dipy', '%(m)s_cam', '%(m)s_fsl'],
-            'WLS': ['%(m)s_dipy', '%(m)s_cam', '%(m)s_fsl'],
-            'RESTORE': ['%(m)s_dipy', '%(m)s_cam'],
+fitmethsd = {'OLS': ['ols_dipy_mf', 'ols_fsl_tensor_mf'],
+            'WLS': ['wls_dipy_mf', 'wls_fsl_tensor_mf'],
             }
+#             'RESTORE': ['%(m)s_dipy', '%(m)s_cam'],
 
-meths = ['wls_dipy', 'wls_fsl', 'wls_cam', 'wls_dipy_mf', 'wls_fsl_mf', 'wls_cam_mf',
-         'ols_dipy', 'ols_fsl', 'ols_cam', 'ols_dipy_mf', 'ols_fsl_mf', 'ols_cam_mf',
-         'restore_cam', 'restore_dipy', 'restore_cam_mf', 'restore_dipy_mf']
+allfitmeths = ['wls_dipy', 'wls_fsl', 'wls_cam', 'wls_dipy_mf', 'wls_fsl_mf', 'wls_cam_mf',
+                'ols_dipy', 'ols_fsl', 'ols_cam', 'ols_dipy_mf', 'ols_fsl_mf', 'ols_cam_mf',
+                'restore_cam', 'restore_dipy', 'restore_cam_mf', 'restore_dipy_mf']
+
+fext = {'ols_dipy_mf': '.nii', 'ols_fsl_tensor_mf': '.nii.gz','wls_dipy_mf': '.nii', 'wls_fsl_tensor_mf': '.nii.gz'}
 
 
 project = 'bbc'
+ref = fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / 'bbc_pairedLH_template_resampled2dwi.nii'
 dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
 vbm_fnames = [vbm_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in vbmpairing]
-templdir = fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space'
-templ_ref = templdir / 'bbc_pairedLH_template_resampled2dwi.nii'
-MNI2templ_invwarp = templdir / 'bbc_pairedLH_template_reg2MNI_1InverseWarp.nii.gz'
-MNI2templ_aff = templdir / 'bbc_pairedLH_template_reg2MNI_0GenericAffine.mat'
-dwi2vbmsubjdir = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2'
-dwi_reg_append = '_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_'
+outf_fmat = '{dwif}_eddy_corrected_repol_std2_{f}_reg2vbmtempl.nii'
 
-for dwif, vbmf in zip(dwi_fnames[1], vbm_fnames[1]):
-
-    fa2t1_outdir = fs / project / 'reg' / 'dwi_in_template_space' / mods / '_'.join([mods, meths, 'warped2templ'])
-
-    for k, m in meths.iteritems():
-        execwdir = fs / project /  dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / 'cuda_repol_std2_v2'
-        mov = a         # always dwi modality
-        ref = str(templ_ref)     # target is subj template t1 resampled to dwi res
-        outf = execwdir / str(dwif+'_'+k)
-        iwarp_dwi2vbmsub = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired' / str(dwif+ dwi_reg_append +'1InverseWarp.nii.gz')
-        iwarp_vbmsubj2templ = templdir / str(vbmf+'InverseWarp.nii.gz')
-        aff_dwi2vbmsub = fs / project / 'reg' / 'reg_subFA2suborigvbmpaired' / str(dwif + dwi_reg_append + '0GenericAffine.mat')
-        aff_vbmsubj2templ = templdir / str(vbmf+'Affine.txt')
-        warpfiles = [str(iwarp_vbmsub2dwi), str(iwarp_templ2vbmsubj)]
-        affine_xform = [str(aff_vbmsub2dwi), str(aff_templ2vbmsubj)]
-
-
-        for km, m in mods.iteritems():
-            subj2templ_applywarp(str(mov), str(ref), str(outf)+'.nii', warpfiles, str(execwdir / km), affine_xform=affine_xform)
-
-
-# need ref, in and out files, t1 to template and dwi to t1 warp and affines.
+for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
+    for k, fm in fitmethsd.iteritems():
+        for f in fm:
+            for m in mods:
+                mov = (fs / project / dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / 'cuda_repol_std2_v2' / k / '_'.join([dwif, 'eddy_corrected_repol_std2', f, m+fext[f]]))
+                outf = (fs / project / 'reg' / 'dwi_warps_in_template_space' / m / f / outf_fmat.format(dwif=dwif, f=f))
+                warp1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Warp.nii.gz'))
+                aff1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Affine.txt'))
+                warp2 = (fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2' / str(dwif+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_1Warp.nii.gz'))
+                aff2 = (fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2' / str(dwif+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_0GenericAffine.mat'))
+                warpfiles = [str(warp1), str(warp2)]
+                affine_xform = [str(aff1), str(aff2)]
+                execwdir = fs / project / 'reg' / 'dwi_warps_in_template_space' / m / f
+                if not execwdir.is_dir():
+                    execwdir.mkdir(parents=True)
+                subj2templ_applywarp(str(mov), str(ref), str(outf), warpfiles, str(execwdir), affine_xform=affine_xform)
+                params = {}
+                params['warpfiles'] = warpfiles
+                params['affine_xform'] = affine_xform
+                params['execwdir'] = execwdir
+                params['ref'] = ref
+                provenance.log(str(mov), 'apply ants T1 template warps and affines to bring FA into template space', str(outf),
+                               script=__file__, provenance=params)
