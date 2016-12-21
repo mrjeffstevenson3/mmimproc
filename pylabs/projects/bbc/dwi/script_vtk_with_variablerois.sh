8fe9cdfb063b53c0f780dbec182b1b0c93ec8119
Jeff Stevenson <mrjeffs@uw.edu>
2016-12-21 10:37:18 -0800

    typo

--- pylabs/projects/bbc/dwi/script_vtk_with_variablerois.sh
+++ pylabs/projects/bbc/dwi/script_vtk_with_variablerois.sh
@@ -7,21 +7,21 @@ sub35=PostIntCap-35
 sub123=PostIntCap-123
 sub43=SLF-43
 sub131=SLF-131
-
+#get bbc subject directories
 cd ${DATADIR}/bbc
 list=`ls -d sub-bbc*`
 rm -f ${DATADIR}/bbc/allvtk_run3.txt
+#loop over subject dirs
 for afolder in ${list}
 do
 echo working on ${afolder}
+#get vtk files to process
 cd ${DATADIR}/bbc/${afolder}/*/*/vtk_tensor_comp_run3
-
 list2=`ls *tensor*.vtk`
+#loop over vtk files
 for afile in ${list2}
 do
 echo working on ${afile}
-
-
 rm -f base.vtk aal_motor.vtk
 if [[ "$afile" == *"$sub70"* ]]; then
 cp *Left_frontal*.vtk base.vtk
