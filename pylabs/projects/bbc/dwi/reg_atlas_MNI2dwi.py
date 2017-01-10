ee68f391ebab8d1c2c1e3447c602798ee97ebf74
mrjeffs <mrjeffs@uw.edu>
2017-01-09 16:50:26 -0800

    adding vbm stats to rois

--- pylabs/projects/bbc/dwi/reg_atlas_MNI2dwi.py
+++ pylabs/projects/bbc/dwi/reg_atlas_MNI2dwi.py
@@ -17,7 +17,8 @@ slicer_path = Path(*Path(inspect.getabsfile(pylabs)).parts[:-3]) / 'Slicer-4.5.0
 #setup masks and templates:
 anat_atlas = pylabs_atlasdir / 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz'
 tract_atlas = pylabs_atlasdir / 'JHU-ICBM-tracts-prob-1mm.nii.gz'
-thr = {'thr': 5}    #global threshold for tract atlas to narrow fiber bundle channel
+JHU_thr = {'thr': 5}    #global threshold for tract atlas to narrow fiber bundle channel
+stat_thr = {'thr': 0.95, 'fn_thr': 95}
 
 MNI_atlases = {'mori': {'atlas_fname': 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz', 'roi_list': None, 'Sl_cmd': None},
                 'aal_motor': {'atlas_fname': 'aal_1mm_motorcortex.nii', 'roi_list': [1, 2, 3, 4, 7, 8, 19, 20, 23, 24, 57, 58, 59, 60, 61, 62, 63, 64, 69, 70], 'Sl_cmd': 'ModelMaker -l 1 -n '},
@@ -38,22 +39,22 @@ MNI_atlases = {'mori': {'atlas_fname': 'JHU_MNI_SS_WMPM_Type_I_matched.nii.gz', 
                 'mori_Left_STG-MTG-18-20':  {'atlas_fname': 'mori_Left_STG-MTG-18-20.nii', 'roi_list': [20,18], 'Sl_cmd': 'ModelMaker -l 1 -n '},
                 'mori_Right_STG-MTG-106-108': {'atlas_fname': 'mori_Right_STG-MTG-106-108.nii', 'roi_list': [106,108], 'Sl_cmd': 'ModelMaker -l 1 -n '},
 
-                'JHU_tracts_Left_ATR-1': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_ATR-1.nii', 'roi_list': 1, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_ATR-2': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_ATR-2.nii', 'roi_list': 2, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_CSP-3': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_CSP-3.nii', 'roi_list': 3, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_CSP-4': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_CSP-4.nii', 'roi_list': 4, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_Cing-5': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_Cing-5.nii', 'roi_list': 5, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_Cing-6': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_Cing-6.nii', 'roi_list': 6, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Forceps_Major-9': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Forceps_Major-9.nii', 'roi_list': 9, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Forceps_Minor-10': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Forceps_Minor-10.nii', 'roi_list': 10, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_IFOF-11': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_IFOF-11.nii', 'roi_list': 11, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_IFOF-12':  {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_IFOF-12.nii', 'roi_list': 12, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_ILF-13': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_ILF-13.nii', 'roi_list': 13, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_ILF-14': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_ILF-14.nii', 'roi_list': 14, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_SLF-15': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_SLF-15.nii', 'roi_list': 15, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_SLF-16': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_SLF-16.nii', 'roi_list': 16, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Left_Unc-17': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Left_Unc-17.nii', 'roi_list': 17, 'Sl_cmd': 'ModelMaker -l 1 -n '},
-                'JHU_tracts_Right_Unc-18': {'atlas_fname': 'JHU_tracts_thr%(thr)s_Right_Unc-18.nii', 'roi_list': 18, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_ATR-1': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_ATR-1.nii', 'roi_list': 1, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_ATR-2': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_ATR-2.nii', 'roi_list': 2, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_CSP-3': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_CSP-3.nii', 'roi_list': 3, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_CSP-4': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_CSP-4.nii', 'roi_list': 4, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_Cing-5': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_Cing-5.nii', 'roi_list': 5, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_Cing-6': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_Cing-6.nii', 'roi_list': 6, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Forceps_Major-9': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Forceps_Major-9.nii', 'roi_list': 9, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Forceps_Minor-10': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Forceps_Minor-10.nii', 'roi_list': 10, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_IFOF-11': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_IFOF-11.nii', 'roi_list': 11, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_IFOF-12':  {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_IFOF-12.nii', 'roi_list': 12, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_ILF-13': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_ILF-13.nii', 'roi_list': 13, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_ILF-14': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_ILF-14.nii', 'roi_list': 14, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_SLF-15': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_SLF-15.nii', 'roi_list': 15, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_SLF-16': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_SLF-16.nii', 'roi_list': 16, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Left_Unc-17': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Left_Unc-17.nii', 'roi_list': 17, 'Sl_cmd': 'ModelMaker -l 1 -n '},
+                'JHU_tracts_Right_Unc-18': {'atlas_fname': 'JHU_tracts_thr%(JHU_thr)s_Right_Unc-18.nii', 'roi_list': 18, 'Sl_cmd': 'ModelMaker -l 1 -n '},
 
                 'stats_vbm_WM_s2'  : {'atlas_fname': 'all_WM_mod_s2_n10000_exchbl_tfce_corrp_tstat2.nii.gz', 'roi_list': 1, 'Sl_cmd': 'TractographyLabelMapSeeding -m 2000 -l 2 -x -v 0.1 -a '},
                 }
@@ -66,7 +67,6 @@ tensors = {'RESTORE':['_eddy_corrected_repol_std2_restore_cam_tensor_medfilt.nhd
 project = 'bbc'
 fa2t1_outdir = 'reg_subFA2suborigvbmpaired_run2'
 fadir = 'FA_fsl_wls_tensor_mf_ero_paired'
-statdir = fs / project / 'reg' /
 dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
@@ -85,19 +85,19 @@ for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
         if 'mori' in k and not a['roi_list'] == None:
             make_mask_fm_atlas_parts(atlas=str(anat_atlas), roi_list=a['roi_list'], mask_fname=str(pylabs_atlasdir / a['atlas_fname']))
         elif 'JHU_' in k:
-            make_mask_fm_tracts(atlas=str(tract_atlas), volidx=a['roi_list'], thresh=thr, mask_fname=str(pylabs_atlasdir / (a['atlas_fname'] % thr)))
+            make_mask_fm_tracts(atlas=str(tract_atlas), volidx=a['roi_list'], thresh=JHU_thr, mask_fname=str(pylabs_atlasdir / (a['atlas_fname'] % JHU_thr)))
         elif 'aal_motor' in k:
             make_mask_fm_atlas_parts(atlas=str(pylabs_atlasdir / 'aal_1mm_reg2MNI_masked.nii.gz'), roi_list=a['roi_list'], mask_fname=str(pylabs_atlasdir / a['atlas_fname']))
         elif 'stats_vbm' in k:
-            make_mask_fm_tracts(atlas=str(vbm_statsdir / a['atlas_fname']), volidx=a['roi_list'], thresh={'thr': 0.95}, mask_fname=str(pylabs_atlasdir / (a['atlas_fname'] % {'thr': 0.95})))
+            make_mask_fm_tracts(atlas=str(vbm_statsdir / a['atlas_fname']), volidx=a['roi_list'], thresh=stat_thr, mask_fname=str(pylabs_atlasdir / (a['atlas_fname'] % {'thr': 0.95})))
         execwdir = fs / project / dwif.split('_')[0] / dwif.split('_')[1] / 'dwi'
         ref = execwdir / str(dwif+'_S0_brain.nii')
         if 'mori' in k or 'aal_motor' in k:
             mov = pylabs_atlasdir / a['atlas_fname']
             outf = execwdir / str(dwif+'_'+k+'.nii')
         if 'JHU_' in k:
-            mov = pylabs_atlasdir / (a['atlas_fname'] % thr)
-            outf = execwdir / str(dwif+'_'+ (a['atlas_fname'] % thr))
+            mov = pylabs_atlasdir / (a['atlas_fname'] % JHU_thr)
+            outf = execwdir / str(dwif+'_'+ (a['atlas_fname'] % JHU_thr))
         iwarp_templ2vbmsubj = templdir / str(vbmf+'InverseWarp.nii.gz')
         iwarp_vbmsub2dwi = dwi2vbmsubjdir / str(dwif+ dwi_reg_append +'1InverseWarp.nii.gz')
         aff_templ2vbmsubj = templdir / str(vbmf+'Affine.txt')
@@ -117,7 +117,7 @@ for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
                     if 'mori' in k or 'aal_motor' in k:
                         cmd += str(vtkdir / str(dwif + '_' + k))
                     if 'JHU_' in k:
-                        cmd += str(vtkdir / str(dwif + '_' + (a['atlas_fname'] % thr).split('.')[0]))
+                        cmd += str(vtkdir / str(dwif + '_' + (a['atlas_fname'] % JHU_thr).split('.')[0]))
                     cmd += ' '+ str(outf)
                     output = ()
                     dt = datetime.datetime.now()
@@ -133,7 +133,7 @@ for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
                     if 'mori' in k or 'aal_motor' in k:
                         provenance.log(str(vtkdir / str(dwif+'_'+k+'.vtk')), 'generate model vtk', str(outf), script=__file__, provenance=params)
                     elif 'JHU_' in k:
-                        provenance.log(str(vtkdir / str(dwif + '_' + (a['atlas_fname'] % thr).split('.')[0]) + '.vtk'), 'generate model vtk', str(outf), script=__file__, provenance=params)
+                        provenance.log(str(vtkdir / str(dwif + '_' + (a['atlas_fname'] % JHU_thr).split('.')[0]) + '.vtk'), 'generate model vtk', str(outf), script=__file__, provenance=params)
                 else:
                     for m, ts in tensors.iteritems():
                         tenpath = execwdir / 'cuda_repol_std2_v2' / m
