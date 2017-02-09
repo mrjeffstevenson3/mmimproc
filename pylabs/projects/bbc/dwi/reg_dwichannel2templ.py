61bfc9307295124a0c755ffbf824433d78787ee4
Jeff Stevenson <mrjeffs@uw.edu>
2017-02-09 13:31:08 -0800

    new CC channels

--- pylabs/projects/bbc/dwi/reg_dwichannel2templ.py
+++ pylabs/projects/bbc/dwi/reg_dwichannel2templ.py
@@ -10,9 +10,9 @@ provenance = ProvenanceWrapper()
 fs = Path(getnetworkdataroot())
 
 # winners of fit death match
-# fitmethsd = {'OLS': ['ols_dipy_medfilt', 'ols_fsl_tensor_medfilt'],
-#             'WLS': ['wls_dipy_medfilt', 'wls_fsl_tensor_medfilt']}
-fitmethsd = {'WLS': ['wls_fsl_tensor_medfilt']}
+fitmethsd = {'OLS': ['ols_dipy_tensor_medfilt', 'ols_fsl_tensor_medfilt'],
+            'WLS': ['wls_dipy_tensor_medfilt', 'wls_fsl_tensor_medfilt']}
+#fitmethsd = {'WLS': ['wls_fsl_tensor_medfilt']}
 
 channel_atlases = {'mori_LeftPostIntCap-35': {'atlas_fname': 'mori_LeftPostIntCap-35_channel.nii.gz', 'roi_list': [35]},
                 'mori_RightPostIntCap-123': {'atlas_fname': 'mori_RightPostIntCap-123_channel.nii.gz', 'roi_list': [123]},
@@ -31,7 +31,7 @@ ref = fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / 'bbc_paired
 dwi_templ = 'sub-bbc{sid}_ses-{snum}_{meth}_{runnum}'
 dwi_fnames = [dwi_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in dwipairing]
 vbm_templ = 'bbc_pairedLH_sub-bbc{sid}_ses-{snum}_{meth}_{runnum}_brain_susan_nl_comroll'
-vbm_fnames = [vbm_temp  l.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in vbmpairing]
+vbm_fnames = [vbm_templ.format(sid=str(s), snum=str(ses), meth=m, runnum=str(r)) for s, ses, m, r in vbmpairing]
 outf_fmat = '{dwif}_eddy_corrected_repol_std2_{f}_{a}_channel_reg2vbmtempl_FA_AD_RD_MD.nii'
 
 for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
@@ -39,14 +39,17 @@ for dwif, vbmf in zip(dwi_fnames, vbm_fnames):
         for f in fm:
             for a, c in channel_atlases.iteritems():
                 mov = (fs / project / dwif.split('_')[0] / dwif.split('_')[1] / 'dwi' / 'vtk_tensor_comp_run7' / '_'.join([dwif, 'eddy_corrected_repol_std2', f, c['atlas_fname']]))
-                outf = (fs / project / 'reg' / 'dwi_warps_in_template_space' / 'comb_FA_AD_RD_MD_4vol' / outf_fmat.format(dwif=dwif, f=f, a=a))
+                outf = (fs / project / 'reg' / 'dwi_warps_in_template_space' / str(f+'_comb_FA_AD_RD_MD_4vol') / outf_fmat.format(dwif=dwif, f=f, a=a))
                 warp1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Warp.nii.gz'))
                 aff1 = (fs / project / 'reg' / 'ants_vbm_pairedLH_in_template_space' / str(vbmf+'Affine.txt'))
                 warp2 = (fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2' / str(dwif+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_1Warp.nii.gz'))
                 aff2 = (fs / project / 'reg' / 'reg_subFA2suborigvbmpaired_run2' / str(dwif+'_eddy_corrected_repol_std2_wls_fsl_tensor_mf_FA_ero_reg2sorigvbm_0GenericAffine.mat'))
                 warpfiles = [str(warp1), str(warp2)]
                 affine_xform = [str(aff1), str(aff2)]
-                execwdir = fs / project / 'reg' / 'dwi_warps_in_template_space' / 'comb_FA_AD_RD_MD_4vol'
+                execwdir = fs / project / 'reg' / 'dwi_warps_in_template_space' / str(f+'_comb_FA_AD_RD_MD_4vol')
                 if not execwdir.is_dir():
                     execwdir.mkdir(parents=True)
-                subj2templ_applywarp(str(mov), str(ref), str(outf), warpfiles, str(execwdir), affine_xform=affine_xform, dims=4, args=['--use-BSpline'])
+                try:
+                    subj2templ_applywarp(str(mov), str(ref), str(outf), warpfiles, str(execwdir), affine_xform=affine_xform, dims=4, args=['--use-BSpline'])
+                except:
+                    print "exception caught for " + dwif + ". Missing " + k + " for " + f
--- pylabs/projects/bbc/dwi/script_vtk_with_variablerois.sh
+++ pylabs/projects/bbc/dwi/script_vtk_with_variablerois.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#this script reads a list of vtk files produced by reg_atlas_MNI2dwi.py and generates results files
 #processing flag: 0=no channel, floor, or target, just vtk stats for whole fiber bundle;
 #   1=channel only; 2=base only ; 3= vtk target only; 4=base and target together; 5=all - channel, base, target
 proc_method=1
@@ -16,8 +16,8 @@ sub131=SLF-131
 cd ${DATADIR}/bbc
 #list=`python -c "from pylabs.projects.bbc.pairing import dwipairing; \
 #    print(' '.join(['sub-bbc{sid}'.format(sid=str(s)) for s, ses, m, r in dwipairing]))"`
-list='sub-bbc108 sub-bbc211 sub-bbc231 sub-bbc241 sub-bbc243 sub-bbc249 sub-bbc253'
-#list=sub-bbc243
+#list='sub-bbc108 sub-bbc211 sub-bbc231 sub-bbc241 sub-bbc243 sub-bbc249 sub-bbc253'
+list=sub-bbc243
 #list=`ls -d sub-bbc*`
 rm -f ${DATADIR}/bbc/allvtk_channel_run${run}.txt
 #loop over subject dirs
@@ -56,7 +56,7 @@ do
 echo working on ${afile}
 FILESIZE=$(stat -c%s "$afile")
 echo $FILESIZE > filesize.txt
-rm -f base.vtk aal_motor.vtk channel.vtk fnew.vtk f.vtk usechannel.txt proc_method.txt
+rm -f base.vtk aal_motor.vtk channel.vtk fnew.vtk fnew1.vtk f.vtk usechannel.txt proc_method.txt
 echo "1" > usechannel.txt
 echo "1" > procmethod.txt
 
@@ -71,12 +71,46 @@ cp *Right_frontal*.vtk base.vtk
 cp *Right_occip*.vtk aal_motor.vtk
 cp *JHU*Right_IFOF-12.vtk channel.vtk
 fi
+#special case for corpus callosum
 if [[ "$afile" == *"$subcc"* ]]; then
-cp *aal_motor*.vtk base.vtk
-cp *aal_motor*.vtk aal_motor.vtk
-echo "0" > usechannel.txt
-${PYLABS}/pylabs/diffusion/readfiber_corpus_callosum_with_ceiling
-cp f.vtk fnew.vtk
+    cp *aal_motor*.vtk base.vtk
+    cp *aal_motor*.vtk aal_motor.vtk
+    echo "0" > usechannel.txt
+    ${PYLABS}/pylabs/diffusion/readfiber_corpus_callosum_with_ceiling
+    echo -n "${afile/.vtk/_nochannel.vtk} " >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    cat dti_results.txt >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    cp fnew.vtk ${afile/.vtk/_nochannel.vtk}
+    ${PYLABS}/pylabs/diffusion/makevolumetric_fromvtk
+    fslchfiletype NIFTI_GZ newvolume.hdr ${afile/.vtk/_channel.nii.gz}
+    fslorient -setqform $qform ${afile/.vtk/_channel.nii.gz}
+    fslorient -copyqform2sform ${afile/.vtk/_channel.nii.gz}
+    fslmaths ${afile/.vtk/_channel.nii.gz} -bin ${afile/.vtk/_channel_bin.nii.gz}
+    echo -n "${afile/.vtk/_channel.vtk} " >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    cat dti_results.txt >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    rm -f fnew.vtk
+    echo "1" > usechannel.txt
+    cp *JHU*Forceps_Major-9.vtk channel.vtk
+    ${PYLABS}/pylabs/diffusion/readfiber_withchannel_test
+    ${PYLABS}/pylabs/diffusion/makevolumetric_fromvtk
+    cp fnew.vtk ${afile/.vtk/_Forceps_Major_channel.vtk}
+    fslchfiletype NIFTI_GZ newvolume.hdr ${afile/.vtk/_Forceps_Major_channel.nii.gz}
+    fslorient -setqform $qform ${afile/.vtk/_Forceps_Major_channel.nii.gz}
+    fslorient -copyqform2sform ${afile/.vtk/_Forceps_Major_channel.nii.gz}
+    fslmaths ${afile/.vtk/_channel.nii.gz} -bin ${afile/.vtk/_Forceps_Major_channel.nii.gz}
+    echo -n "${afile/.vtk/_Forceps_Major_channel.vtk} " >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    cat dti_results.txt >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    rm -f fnew.vtk
+    cp *JHU*Forceps_Minor-10.vtk channel.vtk
+    ${PYLABS}/pylabs/diffusion/readfiber_withchannel_test
+    ${PYLABS}/pylabs/diffusion/makevolumetric_fromvtk
+    cp fnew.vtk ${afile/.vtk/_Forceps_Minor_channel.vtk}
+    fslchfiletype NIFTI_GZ newvolume.hdr ${afile/.vtk/_Forceps_Minor_channel.nii.gz}
+    fslorient -setqform $qform ${afile/.vtk/_Forceps_Minor_channel.nii.gz}
+    fslorient -copyqform2sform ${afile/.vtk/_Forceps_Minor_channel.nii.gz}
+    fslmaths ${afile/.vtk/_Forceps_Minor_channel.nii.gz} -bin ${afile/.vtk/_Forceps_Minor_channel_bin.nii.gz}
+    echo -n "${afile/.vtk/_Forceps_Minor_channel.vtk} " >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    cat dti_results.txt >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+    rm -f fnew.vtk
 fi
 if [[ "$afile" == *"$sub35"* ]]; then
 cp *base*.vtk base.vtk
@@ -98,19 +132,17 @@ cp *Right_STG-MTG-106-108*.vtk base.vtk
 cp *Right_pre-postCentGyr-94-95*.vtk aal_motor.vtk
 cp *JHU*Right_SLF-16.vtk channel.vtk
 fi
-
 if [[ "$afile" != *"$subcc"* ]]; then
 ${PYLABS}/pylabs/diffusion/readfiber_withchannel_test
-fi
 ${PYLABS}/pylabs/diffusion/makevolumetric_fromvtk
 cp fnew.vtk ${afile/.vtk/_channel.vtk}
 fslchfiletype NIFTI_GZ newvolume.hdr ${afile/.vtk/_channel.nii.gz}
-#fslcreatehd S0_hdr.txt ${afile/.vtk/_channel.nii.gz}
 fslorient -setqform $qform ${afile/.vtk/_channel.nii.gz}
 fslorient -copyqform2sform ${afile/.vtk/_channel.nii.gz}
 fslmaths ${afile/.vtk/_channel.nii.gz} -bin ${afile/.vtk/_channel_bin.nii.gz}
 echo "finished with ${afile}"
 echo -n "${afile} " >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
 cat dti_results.txt >> ${DATADIR}/bbc/allvtk_channel_run${run}.txt
+fi
 done
 done
