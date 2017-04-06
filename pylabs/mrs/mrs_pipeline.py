85ec484f3c1ce4729c80649840603984b7405af9
Jeff Stevenson <mrjeffs@uw.edu>
2017-04-06 09:32:09 -0700

    mrs for nbwr tested and works

--- pylabs/mrs/mrs_pipeline.py
+++ pylabs/mrs/mrs_pipeline.py
@@ -7,15 +7,15 @@ from pylabs.utils.paths import getnetworkdataroot
 fs = Path(getnetworkdataroot())
 project = 'nbwr'
 subject = 'sub-nbwr999b'
-rt_actfname = 'NWBR999B_WIP_RTGABAMM_TE80_120DYN_5_2_raw_act.SPAR'
-rt_reffname = 'NWBR999B_WIP_RTGABAMM_TE80_120DYN_5_2_raw_ref.SPAR'
-lt_actfname = 'NWBR999B_WIP_LTPRESS_TE80_GLU_48MEAS_9_2_raw_act.SPAR'
-lt_reffname = 'NWBR999B_WIP_LTPRESS_TE80_GLU_48MEAS_9_2_raw_ref.SPAR'
+rt_actfname = 'NWBR999B_WIP_RTGABAMM_TE80_120DYN_5_2_raw_act.SDAT'
+rt_reffname = 'NWBR999B_WIP_RTGABAMM_TE80_120DYN_5_2_raw_ref.SDAT'
+lt_actfname = 'NWBR999B_WIP_LTGABAMM_TE80_120DYN_8_2_raw_act.SDAT'
+lt_reffname = 'NWBR999B_WIP_LTGABAMM_TE80_120DYN_8_2_raw_ref.SDAT'
 source_path = fs / project / subject / 'ses-1' / 'source_parrec'
 results_dir = fs / project / subject / 'ses-1' / 'mrs'
 
 if not source_path.is_dir():
-    raise ValueError('source_parrec dir with mrs SPAR not found. '+str(source_path))
+    raise ValueError('source_parrec dir with mrs SDAT not found. '+str(source_path))
 
 if not results_dir.is_dir():
     results_dir.mkdir(parents=True)
@@ -24,13 +24,19 @@ shell = Shell()
 
 
 mpath = paths.getgannettpath()
-act = source_path / rt_actfname
-ref = source_path / rt_reffname
+rt_act = source_path / rt_actfname
+rt_ref = source_path / rt_reffname
+lt_act = source_path / lt_actfname
+lt_ref = source_path / lt_reffname
 
-mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
-        mpath, str(act), str(ref))
+rt_mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
+        mpath, str(rt_act), str(rt_ref))
+lt_mcode = "addpath('{0}'); MRS_struct = GannetLoad({{'{1}'}},{{'{2}'}}); MRS_struct = GannetFit(MRS_struct); exit;".format(
+        mpath, str(lt_act), str(lt_ref))
 
-cmd = 'matlab -nodisplay -nosplash -nodesktop -r "{0}"'.format(mcode)
+rt_cmd = 'matlab -nodisplay -nosplash -nodesktop -r "{0}"'.format(rt_mcode)
+lt_cmd = 'matlab -nodisplay -nosplash -nodesktop -r "{0}"'.format(lt_mcode)
 with WorkingContext(str(results_dir)):
-    subprocess.check_call(cmd, shell=True)
+    subprocess.check_call(rt_cmd, shell=True)
+    subprocess.check_call(lt_cmd, shell=True)
 
