9f6a1e17842d80ba994ad381c623da6747fcfae8
mrjeffs <mrjeffs@uw.edu>
2017-04-19 20:26:27 -0700

    correl roi csv rescaled tested

--- pylabs/projects/bbc/correl/stats_from_roi.py
+++ pylabs/projects/bbc/correl/stats_from_roi.py
@@ -1,43 +1,28 @@
-from __future__ import division
-from os.path import join
-import scipy.stats, math, datetime
-from numpy import square, sqrt
-from pylabs.utils import progress
-import pylabs.io.images
 from pathlib import *
 import pandas as pd
-from pandas.tools.util import cartesian_product
 import numpy as np
 import nibabel as nib
-from scipy.stats import spearmanr as sp_correl
 from pylabs.correlation.atlas import mori_region_labels
 from pylabs.alignment.resample import reslice_roi
 from pylabs.utils.paths import getnetworkdataroot
 from pylabs.utils.provenance import ProvenanceWrapper
-from pylabs.io.images import loadStack
-from pylabs.projects.bbc.pairing import FA_foster_pnames, FA_control_pnames, \
-    MD_foster_pnames, MD_control_pnames, RD_foster_pnames, RD_control_pnames, \
-    AD_foster_pnames, AD_control_pnames, GMVBM_foster_pnames, GMVBM_control_pnames, WMVBM_foster_pnames, \
-    WMVBM_control_pnames, foster_paired_behav_subjs, control_paired_behav_subjs, \
-    foster_behav_data, control_behav_data, behav_list, paired_vbm_foster_subjs_sorted, paired_vbm_control_subjs_sorted
-
+from pylabs.projects.bbc.pairing import foster_behav_data, control_behav_data, behav_list
 
 #set up roi
 provenance = ProvenanceWrapper()
 fs = Path(getnetworkdataroot())
 project = 'bbc'
-statsdir = fs/project/'stats'/'py_correl_3rdpass'
+statsdir = fs/project/'stats'/'py_correl_2ndpass'
 atlases_in_templ_sp_dir = fs/project/'reg'/'atlases_in_template_space'
 mori_atlas = atlases_in_templ_sp_dir/'mori_atlas_reg2template.nii.gz'
 JHUtracts_atlas = atlases_in_templ_sp_dir/'ilabsJHUtracts0_atlas_reg2template.nii.gz'
 t_thr=5.0
 min_cluster_size=10
-index_fname= statsdir/'foster_WM_PPVTSS_tpos_cluster_index_cthr10.nii.gz'
-index_num=37
+index_fname= statsdir/'foster_FA_CTOPPrnCS_tpos_cluster_index_cthr10.nii.gz'
+index_num=326
 prime_mod = str(index_fname.name).split('_')[1]
 prime_behav_tup = [x for x in behav_list if x[1] == str(index_fname.name).split('_')[2]][0]
 outfile = statsdir/str('roi'+str(index_num)+'_'+prime_mod+'_'+prime_behav_tup[1]+'_mm_stats.csv')
-
 roi_data = nib.load(str(index_fname)).get_data().astype(int)
 roi_mask = np.zeros(roi_data.shape)
 roi_mask[roi_data == index_num] = 1
@@ -67,7 +52,6 @@ atlas_regions = {'mori': ', '.join(mori_regions), 'JHUtract': ', '.join(JHUtract
 
 
 #set up all files to include and test
-
 pools = ['foster', 'control']
 modalities = ['FA', 'AD', 'RD', 'MD', 'GM', 'WM']
 modalities.remove(prime_mod)
@@ -105,21 +89,7 @@ control_results['sids'] = control_behav_data.index.str.strip('BBC')
 foster_results[prime_behav_tup[1]] = foster_behav_data[prime_behav_tup]
 control_results[prime_behav_tup[1]] = control_behav_data[prime_behav_tup]
 
-
-#behav_labels = [str(b[1]) for b in behav_list]
-#tstats = ['tpos', 'tneg', 'r']
-# stat_ftempl = '{pool}_{mod}_{behav}_{tstat}.nii.gz'
-# all_files = []
-# for p in pools:
-#     for m in modalities:
-#         all_files.append(statsdir/allfile_ftempl.format(pool=p, mod=m))
-#
-# subjs = [s for s in foster_paired_behav_subjs + control_paired_behav_subjs]
-# gp = [0] * len(foster_paired_behav_subjs) + [1] * len(control_paired_behav_subjs)
-# results_mi = pd.MultiIndex.from_arrays(cartesian_product([subjs,gp,behav_list]), names=('subject', 'group', 'behavior'))
-#
-# results = pd.DataFrame()
-
+# run other modalities
 for mod in modalities:
     foster_secondary_results = []
     control_secondary_results = []
@@ -145,24 +115,7 @@ for mod in modalities:
 comb_results = pd.concat([foster_results, control_results])
 comb_results['mori'] = atlas_regions['mori']
 comb_results['JHUtract'] = atlas_regions['JHUtract']
+comb_results[['FA', 'WM', 'GM']] = comb_results[['FA', 'WM', 'GM']].apply(lambda x: x*100)
+comb_results[['MD', 'AD', 'RD']] = comb_results[['MD', 'AD', 'RD']].apply(lambda x: x*10000)
 col_order = ['gp', 'sids', prime_behav_tup[1], prime_mod] + modalities + ['mori', 'JHUtract']
-# add mori region labels here
 comb_results.to_csv(str(outfile), columns=col_order, index=False)
-
-        #
-        # for s, m in zip(foster_behav_data.index, mean):
-        #     results_list.append({'subj': s, 'FA': m, 'gp': 0})
-        #     # how to make 'FA' a param? use % with string format?
-        # results = pd.DataFrame()
-        #
-        # pool, mod = str(afile.name).split('.')[0].split('_')
-
-        # if pool == 'foster':
-        #     behav = foster_behav_data[behavior]
-        #     sids = pd.Series(foster_behav_data.index.str.strip('BBC'))
-        #     gp = pd.Series([0]*len(sids))
-        # elif pool == 'control':
-        #     behav = control_behav_data[behavior]
-        #     sids = pd.Series(control_behav_data.index.str.strip('BBC'))
-        #     gp = pd.Series([1] * len(sids))
-        #
