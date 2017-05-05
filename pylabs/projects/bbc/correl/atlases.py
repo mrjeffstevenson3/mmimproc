e30e08f1decfc07719ffd8ed23d003793883becb
mrjeffs <mrjeffs@uw.edu>
2017-05-04 17:54:49 -0700

    mm atlasing wip

--- pylabs/projects/bbc/correl/atlases.py
+++ pylabs/projects/bbc/correl/atlases.py
@@ -12,27 +12,31 @@ makes atlas objects.
 project = 'bbc'
 atlases_in_templ_sp_dir = fs/project/'reg'/'atlases_in_template_space'
 # define atlases for labeling in template space
-atlas_midx = pd.MultiIndex.from_arrays([['data', 'data', 'shape', 'shape', 'zooms', 'zooms'], ['mori', 'JHU_tracts', 'mori', 'JHU_tracts', 'mori', 'JHU_tracts']])
-atlases = pd.DataFrame(index=['dwi', 'vbm'], columns=atlas_midx)
+atlas_midx = pd.MultiIndex.from_arrays([['dwi', 'dwi', 'vbm', 'vbm'], ['mori', 'JHU_tracts', 'mori', 'JHU_tracts']])
+atlases = pd.DataFrame(index=['img', 'zooms', 'shape'], columns=atlas_midx)
 # get atlas data
-
 mori_atlas_vbm = atlases_in_templ_sp_dir/'mori_atlas_reg2template.nii.gz'
 JHUtracts_atlas_vbm = atlases_in_templ_sp_dir/'ilabsJHUtracts0_atlas_reg2template.nii.gz'
 mori_atlas_dwi = atlases_in_templ_sp_dir/'mori_atlas_reg2template_resamp2dwi.nii.gz'
 JHUtracts_atlas_dwi = atlases_in_templ_sp_dir/'ilabsJHUtracts0_atlas_reg2template_resamp2dwi.nii.gz'
 
+atlases.sort_index(axis=1, inplace=True)
+atlases['vbm'].set_value('img', 'mori', nib.load(str(mori_atlas_vbm)))
+atlases['vbm'].set_value('img', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)))
+atlases['dwi'].set_value('img', 'mori', nib.load(str(mori_atlas_dwi)))
+atlases['dwi'].set_value('img', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)))
 
-atlases['shape'].set_value('vbm', 'mori', nib.load(str(mori_atlas_vbm)).get_data().shape)
-atlases['shape'].set_value('vbm', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).get_data().shape)
-atlases['shape'].set_value('dwi', 'mori', nib.load(str(mori_atlas_dwi)).get_data().shape)
-atlases['shape'].set_value('dwi', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).get_data().shape)
-atlases['zooms'].set_value('vbm', 'mori', nib.load(str(mori_atlas_vbm)).header.get_zooms())
-atlases['zooms'].set_value('vbm', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).header.get_zooms())
-atlases['zooms'].set_value('dwi', 'mori', nib.load(str(mori_atlas_dwi)).header.get_zooms())
-atlases['zooms'].set_value('dwi', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).header.get_zooms())
-atlases['data'].set_value('vbm', 'mori', nib.load(str(mori_atlas_vbm)).get_data())
-atlases['data'].set_value('vbm', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).get_data())
-atlases['data'].set_value('dwi', 'mori', nib.load(str(mori_atlas_dwi)).get_data().shape)
-atlases['data'].set_value('dwi', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).get_data())
+atlases['vbm'].set_value('zooms', 'mori', nib.load(str(mori_atlas_vbm)).header.get_zooms())
+atlases['vbm'].set_value('zooms', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).header.get_zooms())
+atlases['dwi'].set_value('zooms', 'mori', nib.load(str(mori_atlas_dwi)).header.get_zooms())
+atlases['dwi'].set_value('zooms', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).header.get_zooms())
 
+atlases['vbm'].set_value('shape', 'mori', nib.load(str(mori_atlas_vbm)).get_data().shape)
+atlases['vbm'].set_value('shape', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).get_data().shape)
+atlases['dwi'].set_value('shape', 'mori', nib.load(str(mori_atlas_dwi)).get_data().shape)
+atlases['dwi'].set_value('shape', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).get_data().shape)
 
+atlases['vbm'].set_value('affine', 'mori', nib.load(str(mori_atlas_vbm)).affine)
+atlases['vbm'].set_value('affine', 'JHU_tracts', nib.load(str(JHUtracts_atlas_vbm)).affine)
+atlases['dwi'].set_value('affine', 'mori', nib.load(str(mori_atlas_dwi)).affine)
+atlases['dwi'].set_value('affine', 'JHU_tracts', nib.load(str(JHUtracts_atlas_dwi)).affine)
