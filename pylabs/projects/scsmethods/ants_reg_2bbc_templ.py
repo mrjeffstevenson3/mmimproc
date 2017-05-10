50fc35a2f52184f4aabe284b20aceda957bc26c3
Jeff Stevenson <mrjeffs@uw.edu>
2017-05-10 10:29:25 -0700

    ants fmri + qT1 reg 2 template wip

--- pylabs/projects/scsmethods/ants_reg_2bbc_templ.py
+++ pylabs/projects/scsmethods/ants_reg_2bbc_templ.py
@@ -16,7 +16,7 @@ regdir = fs/project/'reg'/'ants_scsqT1_in_template_space'
 templdir = fs/project/'reg'/'ants_vbm_pairedLH_in_template_space'
 ref = templdir/'bbc_pairedLH_template_invT2c_resampled2scsqT1.nii.gz'
 qT1_fname_templ = 'scs{sid}_qT1_orig.nii.gz'
-subj_ids = [317, 328, 334, 347, 364, 371, 379, 384, 396, 318, 332, 335, 353, 370, 376, 381, 385]
+subj_ids = [317, 328, 334, 347, 364, 371, 379, 384, 396, 332, 335, 353, 370, 376, 381, 385]
 qT1_fnames = [qT1_fname_templ.format(sid=s) for s in subj_ids]
 
 if not regdir.is_dir():
@@ -26,7 +26,7 @@ for qT1f in qT1_fnames:
     results = ()
     subjdir = fs / source_data / 'hbm_group_data/qT1/qT1_b1corr_phantcorr_bydate_dec12b_reg2mni'
     mov = subjdir/ qT1f
-    out_fname = regdir/ appendposix(qT1f, '_reg2scsqT1T2template')
+    out_fname = regdir/ qT1f.replace('.nii.gz', '_reg2scsqT1T2template_')
     with WorkingContext(str(regdir)):
         cmd = 'antsRegistrationSyN.sh -d 3 -m '+str(mov)+' -f '+str(ref)+' -o '+str(out_fname)
         cmd += ' -n 30 -t s -p f -j 1 -s 3 -r 1'
