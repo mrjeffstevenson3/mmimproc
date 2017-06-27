3c3d516f111560b2f071e726d298fee6d1d38958
mrjeffs <M@ttiss14>
2017-06-27 10:41:28 -0700

    freesurf wip for nbwr

--- pylabs/projects/nbwr/struc/freesurfer.py
+++ pylabs/projects/nbwr/struc/freesurfer.py
@@ -13,6 +13,9 @@ provenance = ProvenanceWrapper()
 fs = Path(getnetworkdataroot())
 # number of cores to use
 mp = 20
+hires = True
+b1corr = True
+noise_filter = True
 
 if not Path(os.environ.get('ANTSPATH'), 'WarpImageMultiTransform').is_file():
     raise ValueError('must have ants installed with WarpImageMultiTransform in $ANTSPATH directory.')
@@ -35,10 +38,22 @@ for vbmf in vbm_fnames:
     curr_env = copy.copy(os.environ)
     vbmdir = subjects_dir / 'anat'
     vbm_fname = vbmdir/appendposix(vbmf, '.nii')  # no rms on this protocol
+    if b1corr:
+        ## b1corr function here
+
+    if noise_filter:
+        ## add susan filter function here
+
     fs_sid = vbmf+'_freesurf'
     results = ()
     with WorkingContext(str(subjects_dir)):
-        results += run_subprocess(['recon-all', '-openmp', '%.0f' % mp, '-subjid', fs_sid, '-i', str(vbm_fname), '-all'], env=curr_env)
+        if hires:
+            fs_sid += '_hires'
+            with open('freesurf_expert_opts.txt', mode='w') as optsf:
+                optsf.write('mris_inflate -n 15\n')
+            results += run_subprocess(['recon-all', '-openmp', '%.0f' % mp, '-hires', '-subjid', fs_sid, '-i', str(vbm_fname), '-all', '-expert', 'freesurf_expert_opts.txt'], env=curr_env)
+        else:
+            results += run_subprocess(['recon-all', '-openmp', '%.0f' % mp, '-subjid', fs_sid, '-i', str(vbm_fname), '-all'], env=curr_env)
         with open(fs_sid+'/'+fs_sid+'_log{:%Y%m%d%H%M}.json'.format(datetime.datetime.now()), mode='a') as logr:
             json.dump(results, logr, indent=2)
     fs_subj_ln = fs/project/'freesurf_subjs'/fs_sid
