4ba6816515962b7dfc70efce0a06198943d32c71
Jeff Stevenson <mrjeffs@uw.edu>
2017-09-13 10:17:00 -0700

    wip merge

--- pylabs/projects/nbwr/mrs/mrs_gaba_fit.py
+++ pylabs/projects/nbwr/mrs/mrs_gaba_fit.py
@@ -15,7 +15,7 @@ gannettpath = pylabs.utils.paths.getgannettpath()
 # instantiate subject id list container
 subjids_picks = SubjIdPicks()
 # list of subject ids to operate on
-picks = ['226'] # only does one subj until bug fix
+picks = ['038'] # only does one subj until bug fix
 setattr(subjids_picks, 'subjids', picks)
 setattr(subjids_picks, 'source_path', fs / project / 'sub-nbwr%(sid)s' / 'ses-1' / 'source_sparsdat')
 
