403a29f23238aa7aaebe1bb4c6a9d2852cfa145f
Jeff Stevenson <mrjeffs@uw.edu>
2017-09-19 15:45:56 -0700

    making corr metab df

--- pylabs/projects/nbwr/mrs/voi_tissue_fractions.py
+++ pylabs/projects/nbwr/mrs/voi_tissue_fractions.py
@@ -43,12 +43,12 @@ fast = fsl.FAST(
 fast.inputs.environ['FSLMULTIFILEQUIT'] = pylabs.opts.fslmultifilequit
 ext = pylabs.opts.nii_fext
 results_file = fs/project/'test_mrs_tissue_fractions.h5' # results of segmentation
-only_spm = False
+only_spm = True
 # instantiate subject id list container
 subjids_picks = SubjIdPicks()
 # list of subject ids to operate on
 #picks = ['007', '088', '107', '132', '144', '226', '307', '317'] # only does one subj until bug fix
-picks = ['038']
+picks = ['007', '038', '088', '107', '110', '132', '135', '144', '226', '307', '309', '317', '401', '404', '405', '407', '409']
 setattr(subjids_picks, 'subjids', picks)
 setattr(subjids_picks, 'source_path', fs / project / 'sub-nbwr%(sid)s' / 'ses-1' / 'source_sparsdat')
 
