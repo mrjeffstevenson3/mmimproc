d2c5979ca6eb5fe2d3ef3333b72d0fd735ec12bc
mrjeffs <mrjeffs@uw.edu>
2017-10-03 13:00:47 -0700

    added tadpole vbm and mrs matching to brain_convert

--- ipn/CSP_channel_segment_stats.py
+++ ipn/CSP_channel_segment_stats.py
@@ -3,6 +3,7 @@
 
 # In[1]:
 
+
 get_ipython().magic(u'matplotlib inline')
 from pathlib import *
 import pandas as pd
@@ -27,6 +28,7 @@ lcsp_data = pd.read_csv(LCSP_fname, header=None)
 
 # In[2]:
 
+
 #makes subj id fm 1st 10 chars of vtk_file_name
 lcsp_data['Subjid'] = lcsp_data.iloc[:, 0].str[0:10]
 lcsp_data.head(4)
@@ -34,11 +36,13 @@ lcsp_data.head(4)
 
 # In[3]:
 
+
 lcsp_data.rename(columns = {lcsp_data.columns[0]: 'fname', lcsp_data.columns[1]: 'modality', lcsp_data.columns[2]: 'seg1', lcsp_data.columns[3]: 'seg2', lcsp_data.columns[4]: 'seg3', lcsp_data.columns[5]: 'seg4', lcsp_data.columns[6]: 'seg5', lcsp_data.columns[7]: 'seg6', lcsp_data.columns[8]: 'seg7', lcsp_data.columns[9]: 'seg8', lcsp_data.columns[10]: 'seg9', lcsp_data.columns[11]: 'seg10'}, inplace = True)
 lcsp_data.drop('fname', axis=1, inplace=True)
 
 
 # In[4]:
+
 
 lcsp_data.set_index(['Subjid', 'modality'], inplace=True)
 lcsp_data.head(7)
@@ -46,11 +50,13 @@ lcsp_data.head(7)
 
 # In[5]:
 
+
 lcsp_data = lcsp_data.unstack(0)
 lcsp_data.head(7)
 
 
 # In[6]:
+
 
 lcsp_fa = lcsp_data.loc[['fa']]
 lcsp_fa = lcsp_fa.stack(0)
@@ -64,6 +70,7 @@ lcsp_fa.head(10), lcsp_ad.head(10),lcsp_rd.head(10),lcsp_md.head(10)
 
 
 # In[7]:
+
 
 lcsp_fa['209-101'] = lcsp_fa['sub-bbc209'] - lcsp_fa['sub-bbc108']
 lcsp_fa['211-105'] = lcsp_fa['sub-bbc211'] - lcsp_fa['sub-bbc105']
@@ -79,6 +86,7 @@ lcsp_fa[['209-101', '211-105', '208-106', '202-108', '249-113', '241-116', '243-
 
 # In[8]:
 
+
 #lcsp_fa.drop('fa', axis=0, inplace=True)
 lcsp_fa_sub = lcsp_fa[['209-101', '211-105', '208-106', '202-108', '249-113', '241-116', '243-118', '231-119', '253-120']]
 lcsp_fa_sub = lcsp_fa_sub.transpose()
@@ -90,17 +98,21 @@ lcsp_fa_sub.fillna(0, inplace=True)
 
 
 
+
 # In[14]:
+
 
 lcsp_fa_sub
 
 
 # In[16]:
 
+
 (0.0,)*10
 
 
 # In[24]:
+
 
 lcsp_fa_sub_stats = lcsp_fa_sub['fa'].apply(ss.ttest_1samp, axis=0, args=(0.0,)).apply(pd.Series)
 lcsp_fa_sub_stats.columns = ['fa-tstat', 'fa-p-value']
@@ -112,13 +124,16 @@ lcsp_fa_sub_stats
 
 
 
+
 # In[6]:
+
 
 lcsp_data = lcsp_data.stack(0)
 lcsp_data.head(7)
 
 
 # In[7]:
+
 
 lcsp_data = lcsp_data.unstack(0)
 lcsp_data.head(10)
@@ -129,13 +144,16 @@ lcsp_data.head(10)
 
 
 
+
 # In[8]:
+
 
 lcsp_data['211-105'] = lcsp_data['sub-bbc211']['fa'] - lcsp_data['sub-bbc105']['fa']
 lcsp_data['211-105']
 
 
 # In[13]:
+
 
 lcsp_data.head(7)
 
@@ -145,17 +163,21 @@ lcsp_data.head(7)
 
 
 
+
 # In[ ]:
+
 
 lcsp_data.set_index(['Subjid', 'modality'])
 
 
 # In[ ]:
 
+
 list(result.columns.levels[0])
 
 
 # In[ ]:
+
 
 lcsp_data.unstack(level=0)
 lcsp_data.unstack(level=0)
@@ -164,17 +186,20 @@ lcsp_data
 
 
 # In[ ]:
+
 
 
 
 
 # In[12]:
 
+
 lcsp_data.T
 lcsp_data
 
 
 # In[ ]:
+
 
 
 
--- ipn/Paros_mrs_stats_v1.py
+++ ipn/Paros_mrs_stats_v1.py
@@ -6,6 +6,8 @@
 
 # this is awraper function for mrs that collects the disparate mrs data, calculates group stats, and output to google spreadsheet.
 # first set global root data directory
+get_ipython().magic(u'matplotlib inline')
+import seaborn as sb
 import pylabs
 pylabs.datadir.target = 'scotty'
 from pathlib import *
@@ -21,15 +23,54 @@ prov = ProvenanceWrapper()
 fs = Path(getnetworkdataroot())
 stats_dir = fs / project / 'stats' / 'mrs'
 corr_metab = pd.read_hdf(str(stats_dir / 'all_nbwr_mrs_results_csfcorr_fits_forkam.h5'))
-asd_metab = corr_metab[corr_metab.index.str.replace('sub-nbwr','').astype('int') < 400]
-cntrl_metab = corr_metab[corr_metab.index.str.replace('sub-nbwr','').astype('int') >= 400]
+asd_grp = corr_metab.index.str.replace('sub-nbwr','').astype('int') < 400
+asd_metab = corr_metab[asd_grp]
+cntrl_metab = corr_metab[~asd_grp]
 ttest_results = pd.Series(ss.ttest_ind(asd_metab, cntrl_metab, axis=0, equal_var=False), index=['t-stat', 'p-value'])
 ttest_results_df = pd.DataFrame.from_dict({'t-stat': ttest_results['t-stat'], 'p-value': ttest_results['p-value']})
 col_map = {}
 for n, c in zip(range(0, len(corr_metab.columns)), corr_metab.columns):
     col_map[n] = c
 ttest_results_df.rename(index=col_map,inplace=True)
-ttest_results_df
+ttest_results_df.sort_values(by='p-value', ascending=True)
+
+
+# In[72]:
+
+
+corr_metab.loc[corr_metab.index[asd_grp], 'group'] = 'asd'
+corr_metab.loc[corr_metab.index[~asd_grp], 'group'] = 'typical'
+
+lt_corr_metab = corr_metab.loc[:, [x for x in corr_metab.columns if 'left' in x] + ['group']]
+rt_corr_metab = corr_metab.loc[:, [x for x in corr_metab.columns if 'right' in x] + ['group']]
+
+lt_corr_metab.loc[lt_corr_metab.index[asd_grp], [x for x in corr_metab.columns if 'left' in x]]#.sort_values(axis=0)
+
+
+# In[76]:
+
+
+#lt_corr_metab[asd_grp].index[
+#lt_corr_metab.loc[asd_grp, [x for x in corr_metab.columns if 'left' in x]] <>\
+lt_corr_metab.loc[asd_grp, [x for x in corr_metab.columns if 'left' in x]].median().add(lt_corr_metab.loc[asd_grp, [x for x in corr_metab.columns if 'left' in x]].mad().multiply(3))
+
+
+# In[19]:
+
+
+bp = sb.boxplot(data=lt_corr_metab,x='group', y=[x for x in corr_metab.columns if 'left' in x][-1], hue='group', )#, markers=['o', 'x'], kind='reg')
+
+
+# In[31]:
+
+
+mlr = sb.lmplot(x='right-Glu-80ms', y='right-GABA', hue='group', data=rt_corr_metab)
+
+
+# In[30]:
+
+
+bp = sb.factorplot(data=rt_corr_metab, kind='box', hue='group', y='right-Glu-80ms')
 
 
 # In[4]:
