dc721d4cb163f29d95932fcaefbb51e805d3de5a
mrjeffs <M@ttiss14>
2017-10-05 13:39:32 -0700

    added notes to prepare for fortran so Jeff can convert to pythonic software

--- pylabs/projects/nbwr/mrs/nbwr_spreadsheet_sep27_correlations_13_3.f
+++ pylabs/projects/nbwr/mrs/nbwr_spreadsheet_sep27_correlations_13_3.f
@@ -13,7 +13,10 @@ c
 	real tvalue(100)
 	character*1 c1
 	character*3 csubg,csubb,csubsav(100)
-	
+	REAL X,XNU,PROB1,PROB2,aver(2),mean1(100),mean2(100)
+	real dof(100),sdv(2),stdev1(100),stdev2(100)
+  	INTEGER NU, ERROR
+
 	open(11,file ='numcol1.txt')
 	read(11,*)numcol1
 	close(11)
@@ -25,6 +28,9 @@ c
 	close(11)
 	open(11,file ='numrow2.txt')
 	read(11,*)numrow2
+	close(11)
+	open(11,file = 'chosen_metabolite.txt')
+	read(11,*)ichosen_metabolite
 	close(11)
 	open(11,file ='all_nbwr.txt')
 	read(11,11)cfnall_nbwr
@@ -45,7 +51,6 @@ c
 	read(11,*)cheader(irow),(dnmr(ii,irow),ii=1,numcol1)
 	write(6,*)cheader(irow),dnmr(1,irow),dnmr(numcol1,irow),irow
 	enddo  !irow1
-	
 	close(11)
 	open(11,file = cfnbehav)
 	read(11,*)cheader2(1),(cfnlist2(ii),ii=1,numcol2)
@@ -60,8 +65,8 @@ c
 c
 
 C
-	do ii=13,13
-	do ibeh = 3,3
+	do ii=ichosen_metabolite,ichosen_metabolite
+	do ibeh = 1,1
 	index1 = 1
 	do irow = 2,numrow1-1
 	cfn = cheader(irow)
@@ -84,16 +89,33 @@ c		write(6,*)csubb
 	enddo
 	enddo
 	itsize = index1-1
-	open(12,file='pri_glutamate.txt')
+	open(12,file='output_correlation.txt')
+	open(13,file='output_correlation_stats.txt')
 	do i=1,itsize
 c	write(6,*)'test corr input ',dmnmr(i),behav1(i),i
 	write(12,*)dmnmr(i),behav1(i),csubsav(i)
 	enddo
 	call correlation(dmnmr,behav1,rcorr,itsize)
 	if(rcorr.gt.0.6.or.rcorr.lt.-0.6)write(6,*)'rcorr ',rcorr, ii,ibeh,itsize,cfnlist(ii),cfnlist2(ibeh)
+	rsize = itsize
+	rdenom = sqrt((1-(rcorr*rcorr))/(rsize-2))
+	rt = rcorr/rdenom
+	write(6,*)'this is the tvalue converted from correlation rvalue ',rt
+	write(13,*)rt
+	tt=rt
+	degreesof = rsize
+	PROB2=STUDNT(tt,degreesof,ERROR)
+  	write(6,*)' tvalue=', tt
+  	write(6,*)' PROB2=', PROB2*2.0
+  	write(13,*)PROB2*2.0
+  	write(6,*)' ERROR=', ERROR
+
+
 	enddo !ibehav
 	enddo !ii
 c
+	close(12)
+	close(13)
 	stop
 	end
 	
@@ -266,3 +288,57 @@ c	write(6,*)'rmeana rmeanb ',rmeana,rmeanb
 	endif
 	return
 	end
+c
+	REAL FUNCTION STUDNT (T, DOFF, IFAULT)
+! ----------------------------------------------------------------
+!  ALGORITHM AS 27  APPL. STATIST. VOL.19, NO.1
+!
+!  Calculate the upper tail area under Student's t-distribution
+!
+!  Translated from Algol by Alan Miller
+! ----------------------------------------------------------------
+	INTEGER IFAULT
+	REAL T, DOFF
+!  Local variables
+
+	REAL V, X, TT, TWO, FOUR, ONE, ZERO, HALF
+	REAL A1, A2, A3, A4, A5, B1, B2, C1, C2, C3, C4, C5, D1, D2, E1, E2, E3, E4, E5, F1, F2, G1, G2, G3, G4, G5, H1, H2, I1, I2, I3, I4, I5, J1, J2
+	LOGICAL POS
+	DATA TWO /2.0/, FOUR /4.0/, ONE /1.0/, ZERO /0.0/, HALF /0.5/
+	DATA A1, A2, A3, A4, A5 /0.09979441, -0.581821, 1.390993,-1.222452, 2.151185/, B1, B2 /5.537409, 11.42343/
+	DATA C1, C2, C3, C4, C5 /0.04431742, -0.2206018, -0.03317253,5.679969, -12.96519/, D1, D2 /5.166733, 13.49862/
+	DATA E1, E2, E3, E4, E5 /0.009694901, -0.1408854, 1.88993,-12.75532, 25.77532/, F1, F2 /4.233736, 14.3963/
+	DATA G1, G2, G3, G4, G5 /-9.187228E-5, 0.03789901, -1.280346,9.249528, -19.08115/, H1, H2 /2.777816, 16.46132/
+	DATA I1, I2, I3, I4, I5 /5.79602E-4, -0.02763334, 0.4517029,-2.657697, 5.127212/, J1, J2 /0.5657187, 21.83269/
+
+!  Check that number of degrees of freedom > 4.
+
+	IF (DOFF .LT. TWO) THEN
+	 IFAULT = 1
+	 STUDNT = - ONE
+	 RETURN
+	END IF
+
+	IF (DOFF .LE. FOUR) THEN
+	 IFAULT = DOFF
+	ELSE
+	 IFAULT = 0
+	END IF
+
+!  Evaluate series.
+
+	V = ONE / DOFF
+	POS = (T .GE. ZERO)
+	TT = ABS(T)
+	X = HALF * (ONE + TT * (((A1 + V * (A2 + V * (A3 + V * (A4 + V * A5)))) / (ONE - V * (B1 - V * B2))) + TT * (((C1 + V * (C2 + V * (C3 + V * (C4 + V * C5)))) /  (ONE - V * (D1 - V * D2))) +TT * (((E1 + V * (E2 + V * (E3 + V * (E4 + V * E5)))) / (ONE - V * (F1 - V * F2))) +TT * (((G1 + V * (G2 + V * (G3 + V * (G4 + V * G5)))) /  (ONE - V * (H1 - V * H2))) + TT * ((I1 + V * (I2 + V * (I3 + V * (I4 + V * I5)))) /(ONE - V * (J1 - V * J2))) ))))) ** (-8)
+	IF (POS) THEN
+	 STUDNT = X
+	ELSE
+	 STUDNT = ONE - X
+	END IF
+
+	RETURN
+	END
+
+!end of file Student.f90
+
