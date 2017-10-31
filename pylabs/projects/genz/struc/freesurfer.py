9772ba9302d66d7ccad40f3d9dfc2789a95b2628
mrjeffs@uw.edu <M@ttiss14>
2017-10-31 00:15:10 -0700

    testing fs genz

--- pylabs/projects/genz/struc/freesurfer.py
+++ pylabs/projects/genz/struc/freesurfer.py
@@ -22,7 +22,11 @@ antsRegistrationSyN = get_antsregsyn_cmd()
 # instantiate subject id list container
 subjids_picks = SubjIdPicks()
 # list of subject ids to operate on
-picks = [{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',}, {'subj': 'sub-genz996', 'session': 'ses-2', 'run': '1',},{'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1'}, {'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',}]
+picks = [{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',},
+         #{'subj': 'sub-genz996', 'session': 'ses-2', 'run': '1',},
+         #{'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1'},
+         #{'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',},
+         ]
 
 setattr(subjids_picks, 'subjids', picks)
 
