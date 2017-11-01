c9b3d35c94802ebff9f5d3b6e12ccb41569d8a9f
mrjeffs <mrjeffs@uw.edu>
2017-10-31 20:55:05 -0700

    bug fix

--- pylabs/projects/genz/mrs/mrs_gaba_fit.py
+++ pylabs/projects/genz/mrs/mrs_gaba_fit.py
@@ -20,7 +20,7 @@ subjids_picks = SubjIdPicks()
 picks = [{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',},
          {'subj': 'sub-genz996', 'session': 'ses-2', 'run': '1',},
          {'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1',},
-         {'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',}
+         {'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',},
          ]
 
 setattr(subjids_picks, 'subjids', picks)
