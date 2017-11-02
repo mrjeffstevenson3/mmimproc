2c370a52866b13811fe57a51c7954999b1b1ee08
mrjeffs <mrjeffs@uw.edu>
2017-11-01 21:40:50 -0700

    genz qt1 fit 2nd run

--- pylabs/projects/genz/qt1/brain_fit.py
+++ pylabs/projects/genz/qt1/brain_fit.py
@@ -41,10 +41,11 @@ pool = Pool(20)
 # instantiate subject id list container
 subjids_picks = SubjIdPicks()
 # list of dicts with subject ids, session, scan info to operate on
-picks = [{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',},
+picks = [
+         #{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',},
          {'subj': 'sub-genz996', 'session': 'ses-2', 'run': '1',},
-         {'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1'},
-         {'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',}
+         #{'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1'},
+         #{'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',}
          ]
 
 setattr(subjids_picks, 'subjids', picks)
@@ -67,14 +68,11 @@ if flip5:
 TR = float(spgr5_fa5_fnames[0].split('_')[3].split('-')[-1].replace('p','.'))
 results = ()
 
-# for testing purposes. comment out to run full loop.
-b1map, spgr05, spgr10, spgr15, spgr20, spgr30 = b1map_fnames[0], spgr5_fa5_fnames[0], spgr5_fa10_fnames[0], spgr5_fa15_fnames[0], spgr5_fa20_fnames[0], spgr5_fa30_fnames[0]
+# # for testing purposes. comment out to run full loop.
+# i = 3
+# b1map, spgr05, spgr10, spgr15, spgr20, spgr30 = b1map_fnames[i], spgr5_fa5_fnames[i], spgr5_fa10_fnames[i], spgr5_fa15_fnames[i], spgr5_fa20_fnames[i], spgr5_fa30_fnames[i]
 
 #for b1map, spgr05, spgr15, spgr30 in zip(b1map_fnames, spgr_fa5_fnames, spgr_fa15_fnames, spgr_fa30_fnames):
-
-# inside for loop:
-# thr spgr30 @ 2000
-# then loop over other
 
 for spgr05, spgr10, spgr15, spgr20, spgr30 in zip(spgr5_fa5_fnames, spgr5_fa10_fnames, spgr5_fa15_fnames, spgr5_fa20_fnames, spgr5_fa30_fnames):
     spgr_dir = fs/project/spgr30.split('_')[0] / spgr30.split('_')[1] / 'qt1'
@@ -83,6 +81,7 @@ for spgr05, spgr10, spgr15, spgr20, spgr30 in zip(spgr5_fa5_fnames, spgr5_fa10_f
         results += run_subprocess(thrcmd)
 
 for b1map, spgr05, spgr10, spgr15, spgr20, spgr30 in zip(b1map_fnames, spgr5_fa5_fnames, spgr5_fa10_fnames, spgr5_fa15_fnames, spgr5_fa20_fnames, spgr5_fa30_fnames):
+
     b1map_dir = fs/project/b1map.split('_')[0] / b1map.split('_')[1] / 'fmap'
     spgr_dir = fs/project/spgr30.split('_')[0] / spgr30.split('_')[1] / 'qt1'
     reg_dir = fs/project/spgr30.split('_')[0] / spgr30.split('_')[1] / 'reg' / reg_dir_name
@@ -90,14 +89,14 @@ for b1map, spgr05, spgr10, spgr15, spgr20, spgr30 in zip(b1map_fnames, spgr5_fa5
     if not reg_dir.is_dir():
         reg_dir.mkdir(parents=True)
     if not (b1map_dir/str(b1map+'_mag'+ext)).is_file():
-        b1magcmd = ['fslroi '+str(b1map_dir/b1map+'.nii')+' '+str(replacesuffix(b1map_dir/b1map, '_mag'+ext))+' 0 1']
+        b1magcmd = ['fslroi '+str(b1map_dir/str(b1map+'.nii'))+' '+str(replacesuffix(b1map_dir/b1map, '_mag'+ext))+' 0 1']
         results += run_subprocess(b1magcmd)
     b1regcmd = [str(antsRegistrationSyNQuick)+' -d 3 -m '+str(replacesuffix(b1map_dir/b1map, '_mag'+ext))+' -f '+
                 str(ref)+' -o '+str(replacesuffix(reg_dir/b1map, '_mag_'+reg_dir_name+'_'))+' -n 30 -t s -p f -j 1 -s 10 -r 1']
     # reg b1mag to spgr30
     results += run_subprocess(b1regcmd)
     if not (b1map_dir/str(b1map+'_phase'+ext)).is_file():
-        b1phasecmd = ['fslroi '+str(b1map_dir/str(b1map+'.nii'))+' '+str(replacesuffix(b1map_dir/b1map+'_phase'+ext))+' 2 1']
+        b1phasecmd = ['fslroi '+str(b1map_dir/str(b1map+'.nii'))+' '+str(replacesuffix(b1map_dir/b1map, '_phase'+ext))+' 2 1']
     mov = replacesuffix(b1map_dir/b1map, '_phase'+ext)
     outf = replacesuffix(reg_dir/b1map, '_phase_' + reg_dir_name + ext)
     # need to point to reg dir
@@ -171,128 +170,4 @@ for b1map, spgr05, spgr10, spgr15, spgr20, spgr30 in zip(b1map_fnames, spgr5_fa5
                  provenance=dict(**kwargs))
     except Exception as ex:
         print('\n--> Error during fitting: ', ex)
-
-#################  ================= end here for now  ===========================
-
-for b1map, spgr05, spgr10, spgr15, spgr20, spgr30 in zip(b1map_fnames, spgr5_fa5_fnames, spgr5_fa10_fnames, spgr5_fa15_fnames, spgr5_fa20_fnames, spgr5_fa30_fnames):
-
-    b1map_dir = fs/project/b1map.split('_')[0] / b1map.split('_')[1] / 'fmap'
-    spgr_dir = fs/project/spgr30.split('_')[0] / spgr30.split('_')[1] / 'qt1'
-    b1magcmd = ['fslroi', str(b1map_dir/b1map), str(appendposix(b1map_dir/b1map, '_mag')), '0', '1']
-    b1phasecmd = ['fslroi', str(b1map_dir/b1map), str(appendposix(b1map_dir/b1map, '_phase')), '2', '1']
-    b1tospgr30antscmd = [ str(antsRegistrationSyNQuick), '-d 3 -m', str(appendposix(b1map_dir/b1map, '_mag.nii.gz')), '-f',
-                str(spgr_dir/str(spgr30+'.nii')), '-o', str(appendposix(b1map_dir/b1map, '_mag_reg2spgr30_')),
-                '-n 30 -t s -p f -j 1 -s 10 -r 1']
-    spgr05tospgr30antscmd = [ str(antsRegistrationSyNQuick), '-d 3 -m', str(spgr_dir/str(spgr05+'.nii')), '-f',
-                str(spgr_dir/str(spgr30+'.nii')), '-o', str(spgr_dir/str(spgr05 + '_reg2spgr30_')),
-                '-n 30 -t s -p f -j 1 -s 10 -r 1']
-    spgr15tospgr30antscmd = [ str(antsRegistrationSyNQuick), '-d 3 -m', str(spgr_dir/str(spgr15+'.nii')), '-f',
-                str(spgr_dir/str(spgr30+'.nii')), '-o', str(spgr_dir/str(spgr15 + '_reg2spgr30_')),
-                '-n 30 -t s -p f -j 1 -s 10 -r 1']
-    if flip5:
-        spgr10tospgr30antscmd = [str(antsRegistrationSyNQuick), '-d 3 -m', str(spgr_dir / str(spgr10 + '.nii')), '-f',
-                                 str(spgr_dir / str(spgr30 + '.nii')), '-o',
-                                 str(spgr_dir / str(spgr10 + '_reg2spgr30_')),
-                                 '-n 30 -t s -p f -j 1 -s 10 -r 1']
-        spgr20tospgr30antscmd = [str(antsRegistrationSyNQuick), '-d 3 -m', str(spgr_dir / str(spgr20 + '.nii')), '-f',
-                                 str(spgr_dir / str(spgr30 + '.nii')), '-o',
-                                 str(spgr_dir / str(spgr20 + '_reg2spgr30_')),
-                                 '-n 30 -t s -p f -j 1 -s 10 -r 1']
-        #spgr20tospgr30antscmd = ' '.join(spgr15tospgr30antscmd)
-    results = ()
-    # make b1 corr function
-    if overwrite or not Path(appendposix(b1map_dir / b1map, '_phase_reg2spgr30_mf.nii.gz')).is_file():
-        with WorkingContext(str(b1map_dir)):
-            results += run_subprocess(b1magcmd)
-            results += run_subprocess(b1phasecmd)
-            results += run_subprocess(b1tospgr30antscmd)
-            mov = appendposix(b1map_dir/b1map, '_phase.nii.gz')
-            ref = spgr_dir/str(spgr30+'.nii')
-            outf = appendposix(b1map_dir/b1map, '_phase_reg2spgr30.nii.gz')
-            warpf = [str(appendposix(b1map_dir/b1map, '_mag_reg2spgr30_1Warp.nii.gz'))]
-            affine_xform = [str(replacesuffix(b1map_dir/b1map, '_mag_reg2spgr30_0GenericAffine.mat'))]
-            execwd = b1map_dir
-            subj2templ_applywarp(str(mov), str(ref), str(outf), warpf, str(execwd), affine_xform=affine_xform)
-            phase_data = nib.load(str(outf)).get_data().astype('float32')
-            phase_data_mf = medianf(phase_data, size=7)
-            savenii(phase_data_mf, nib.load(str(outf)).affine, str(appendposix(b1map_dir/b1map, '_phase_reg2spgr30_mf.nii.gz')))
-            prov.log(str(appendposix(b1map_dir/b1map, '_phase_reg2spgr30_mf.nii.gz')), 'median filtered b1 phase map reg to spgr 30', str(b1map_dir/b1map)+'.nii', script=__file__,
-                     provenance={'filter': 'numpy median filter', 'filter size': '7', 'results': results})
-
-    with WorkingContext(str(spgr_dir)):
-        if overwrite or not (spgr_dir/str(spgr05 + '_reg2spgr30_Warped.nii.gz')).is_file():
-            results += run_subprocess(spgr05tospgr30antscmd)
-            extract_brain(spgr_dir / str(spgr05 + '_reg2spgr30_Warped.nii.gz'))
-        if overwrite or not (spgr_dir / str(spgr15 + '_reg2spgr30_Warped.nii.gz')).is_file():
-            results += run_subprocess(spgr15tospgr30antscmd)
-        if flip5:
-            if overwrite or not (spgr_dir / str(spgr10 + '_reg2spgr30_Warped.nii.gz')).is_file():
-                results += run_subprocess(spgr10tospgr30antscmd)
-            if overwrite or not (spgr_dir / str(spgr20 + '_reg2spgr30_Warped.nii.gz')).is_file():
-                results += run_subprocess(spgr20tospgr30antscmd)
-
-        maskfile = spgr_dir/str(spgr05 + '_reg2spgr30_Warped_brain_mask.nii.gz')
-
-        if flip5:
-            for f in [appendposix(b1map_dir/b1map, '_phase_reg2spgr30_mf.nii.gz'), spgr_dir/str(spgr15 + '_reg2spgr30_Warped.nii.gz'), spgr_dir/str(spgr30+'.nii')]:
-                results += run_subprocess(['fslmaths', str(f), '-mas', str(maskfile), str(appendposix(f, '_brain'))])
-
-
-        rootdir = str(fs/project)
-        if flip5:
-            files = [str(spgr_dir/str(spgr05 + '_reg2spgr30_Warped_brain.nii.gz')),
-                     str(spgr_dir / str(spgr10 + '_reg2spgr30_Warped_brain.nii.gz')),
-                 str(spgr_dir/str(spgr15 + '_reg2spgr30_Warped_brain.nii.gz')),
-                     str(spgr_dir / str(spgr20 + '_reg2spgr30_Warped_brain.nii.gz')),
-                 str(spgr_dir/str(spgr30+'_brain.nii.gz'))]
-        else:
-            files = [str(spgr_dir/str(spgr05 + '_reg2spgr30_Warped_brain.nii.gz')),
-                 str(spgr_dir/str(spgr15 + '_reg2spgr30_Warped_brain.nii.gz')),
-                 str(spgr_dir/str(spgr30+'_brain.nii.gz'))]
-
-        subject = spgr05.split('_')[0]
-        ses = spgr05.split('_')[1]
-        b1file = appendposix(b1map_dir/b1map, '_phase_reg2spgr30_mf_brain.nii.gz')
-        X = []
-        if flip5:
-            for a in [spgr05, spgr10, spgr15, spgr20, spgr30]:
-                X.append(int(a.split('_')[3].split('-')[1]))
-                outfile = str(spgr_dir / '{sub}_{ses}_qT1_b1corr_5flip.nii.gz'.format(sub=subject, ses=ses))
-        else:
-            for a in [spgr05, spgr15, spgr30]:
-                X.append(int(a.split('_')[3].split('-')[1]))
-                outfile = str(spgr_dir/ '{sub}_{ses}_qT1_b1corr_3flip.nii.gz'.format(sub=subject, ses=ses))
-        print(files)
-        print(b1file)
-        kwargs = {}
-        kwargs['scantype'] = spgr05.split('_')[2].upper()
-        kwargs['TR'] = TR
-        kwargs['t1filename'] = outfile
-        if b1file.is_file():
-            kwargs['b1file'] = str(b1file)
-        else:
-            print('No B1 file for subject {0}'.format(subject))
-        if maskfile.is_file():
-            kwargs['maskfile'] = str(maskfile)
-        if async:
-            kwargs['mute'] = True
-            pool.apply_async(t1fit, [files, X], kwargs)
-        else:
-            try:
-                t1fit(files, X, **kwargs)
-                kwargs['clamp'] = '1 to 6000'
-                kwargs['erosion structure'] = (2,2,2)
-                kwargs['flips'] = X
-                kwargs['files'] = files
-                qt1_data = nib.load(kwargs['t1filename']).get_data()
-                mask_data = nib.load(kwargs['maskfile']).get_data().astype(int)
-                qt1_data[qt1_data < 1] = 0
-                qt1_data[qt1_data > 6000] = 6000
-                mask_data = ero(mask_data, structure=np.ones((2,2,2))).astype(mask_data.dtype)
-                qt1_clamped = applymask(qt1_data, mask_data)
-                savenii(qt1_clamped, nib.load(kwargs['t1filename']).affine, str(appendposix(kwargs['t1filename'], '_clamped')))
-                prov.log(str(appendposix(kwargs['t1filename'], '_clamped')), 'clamping qT1 fit', kwargs['t1filename'],
-                         script=__file__, provenance=dict(**kwargs))
-            except Exception as ex:
-                print('\n--> Error during fitting: ', ex)
 
