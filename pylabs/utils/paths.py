ddd6b4444f094e6872f0adc9ef0ef917fa7be52b
mrjeffs <mrjeffs@uw.edu>
2018-01-10 08:21:18 -0800

    meg head mask extended

--- pylabs/utils/paths.py
+++ pylabs/utils/paths.py
@@ -24,6 +24,8 @@ MNI1mm_T1_brain_mask = pylabs_atlasdir/'MNI152_T1_1mm_brain_mask.nii.gz'
 #MNI1mm_T2 = pylabs_atlasdir/'MNI152_T2_1mm.nii.gz'
 MNI1mm_T2_brain = pylabs_atlasdir/'MNI152_T2_1mm_brain.nii.gz'
 MNI1mm_T1_qa_mask = pylabs_atlasdir/'MNI152_T1_1mm_qa_mask.nii.gz'
+meg_head_mask = pylabs_atlasdir/'MNI152_T1_1mm_meg_mask.nii'
+
 
 def getlocaldataroot():
     hostname = socket.gethostname()
