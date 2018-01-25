7b184a0799e700306670cb60f35a50188d17fa48
mrjeffs@uw.edu <M@ttiss14>
2018-01-25 15:32:57 -0800

    fs bem error retry with new mne-python pull

--- pylabs/projects/genz/mrs/voi_tissue_fractions.py
+++ pylabs/projects/genz/mrs/voi_tissue_fractions.py
@@ -52,7 +52,7 @@ picks = [
          #{'subj': 'sub-genz996', 'session': 'ses-1', 'run': '1',},
          #{'subj': 'sub-genz996', 'session': 'ses-2', 'run': '1',},
          #{'subj': 'sub-genz997', 'session': 'ses-1', 'run': '1',},
-         {'subj': 'sub-genz997', 'session': 'ses-2', 'run': '1',}
+         {'subj': 'sub-genz997', 'session': 'ses-4', 'run': '1',}
          ]
 
 setattr(subjids_picks, 'subjids', picks)
