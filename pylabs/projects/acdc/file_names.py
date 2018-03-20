70ecdc607adc401786ffb007f33a2526d2b84cb4
mrjeffs <mrjeffs@uw.edu>
2018-03-20 10:09:35 -0700

    wip Optsd class as dict

--- pylabs/projects/acdc/file_names.py
+++ pylabs/projects/acdc/file_names.py
@@ -1,12 +1,7 @@
-# orig_dwi are the original dwi source files output from conversion -> inputs to topup
-# set as a triple tuple (topup_dwi_6S0 6 vol S0 1st, topdn_dwi_6S0 6 vol S0 2nd, dwi-topup_64dir-3sh-800-2000 multishell 64 dir dwi 3rd)
-# topup_dwi_6S0 and topdn_dwi_6S0 are inputs to topup along with dwell time
-# topup_unwarped is the output from topup and input to eddy
-
 # here there are no subj ids, those are called out by the picks list inside SubjIdPicks object.
 
 # class object to pass list of subject ids and passed to functions here using the SubjIdPicks class object.
-# defaults for session and run numbers are set here as  well as exceptions to those defaults called out by subject and modality.
+# defaults for project set in class Optsd used in all pipelines
 import pylabs
 from pathlib import *
 import pandas as pd
